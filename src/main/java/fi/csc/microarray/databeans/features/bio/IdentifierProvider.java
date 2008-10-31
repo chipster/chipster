a4a3166771f516f2d57f13dedbb3108fd32e4f69
akallio <devnull@localhost>
2008-10-31 11:42:09 +0000

    made "identifier" also supported as a name for the row name column, with some refactoring

---
+++ src/main/java/fi/csc/microarray/databeans/features/bio/IdentifierProvider.java
@@ -0,0 +1,42 @@
+package fi.csc.microarray.databeans.features.bio;
+
+import fi.csc.microarray.databeans.DataBean;
+import fi.csc.microarray.databeans.features.Feature;
+import fi.csc.microarray.databeans.features.FeatureProviderBase;
+import fi.csc.microarray.databeans.features.NonexistingFeature;
+
+/**
+ * <p>
+ * Tool for retrieving phenodata related information. Supports following paths:
+ * <ul>
+ * <li>/ : a valid feature is returned if this bean is identified as phenodata</li>
+ * <li>/is-complete : a valid feature is returned if this phenodata is complete</li>
+ * <li>/sample_to_name/X : if phenodata contains real samples names it is
+ * returned for X, otherwise X is returned</li>
+ * <li>/linked : linked phenodata bean is located and following path is
+ * executed against it</li>
+ * </ul>
+ * </p>
+ * 
+ * @author Aleksi Kallio
+ * 
+ */
+public class IdentifierProvider extends FeatureProviderBase {
+
+
+	private static final String NAMED_IDENTIFIER_COLUMN = "/column/identifier";
+	private static final String ROW_NAME_COLUMN = "/column/ ";
+
+	public Feature createFeature(String namePostfix, DataBean bean) {
+
+		if (bean.queryFeatures(ROW_NAME_COLUMN).exists()) {
+			return bean.queryFeatures(ROW_NAME_COLUMN).asFeature();
+			
+		} else if (bean.queryFeatures(NAMED_IDENTIFIER_COLUMN).exists()) {
+			return bean.queryFeatures(NAMED_IDENTIFIER_COLUMN).asFeature();
+			
+		} else {
+			return new NonexistingFeature(bean, this);
+		}
+	}
+}
