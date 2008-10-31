c1e9bff174431977115992043b84a2abfbdfa28a
akallio <devnull@localhost>
2008-10-31 11:34:40 +0000

    venn diagram selection patching, still some issues prevail

--- src/main/java/fi/csc/microarray/client/visualisation/methods/PositionRecordingRenderer.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/PositionRecordingRenderer.java
@@ -41,16 +41,13 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 		this.selectedItems = selectedItems;
 		this.chartPanel = chartPanel;
 	}
-	
+
 	@Override
-	public void drawItem(Graphics2D g2, XYItemRendererState state, 
-			Rectangle2D dataArea, PlotRenderingInfo info, 
-			XYPlot plot, ValueAxis domainAxis, 
-			ValueAxis rangeAxis, XYDataset dataset, 
-			int series, int item, CrosshairState crosshairState, int pass) {
+	public void drawItem(Graphics2D g2, XYItemRendererState state, Rectangle2D dataArea, PlotRenderingInfo info, XYPlot plot, ValueAxis domainAxis, ValueAxis rangeAxis, XYDataset dataset, int series, int item, CrosshairState crosshairState, int pass) {
 
 		// this method body has been ripped from JFreeChart, and modified to
-		//   work in a derived class. it does not support every feature the original did
+		// work in a derived class. it does not support every feature the
+		// original did
 
 		boolean itemVisible = getItemVisible(series, item);
 
@@ -79,23 +76,22 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 		double transX1 = domainAxis.valueToJava2D(x1, dataArea, xAxisLocation);
 		double transY1 = rangeAxis.valueToJava2D(y1, dataArea, yAxisLocation);
 
-		//logger.debug("x1: " + x1 + "y1: " + y1 + "transX1: " + transX1 + "transY1: " + transY1);			
+		// logger.debug("x1: " + x1 + "y1: " + y1 + "transX1: " + transX1 +
+		// "transY1: " + transY1);
 
 		if (getPlotLines()) {
-			if (true/*this.drawSeriesLineAsPath*/) {
+			if (true/* this.drawSeriesLineAsPath */) {
 				State s = (State) state;
-				if (true/*s.getSeriesIndex() != series*/) {
+				if (true/* s.getSeriesIndex() != series */) {
 					// we are starting a new series path
 					/*
-					s.seriesPath().reset();
-					s.lastPointGood = false;
-					s.setSeriesIndex(series);
+					 * s.seriesPath().reset(); s.lastPointGood = false;
+					 * s.setSeriesIndex(series);
 					 */
 				}
 
 				// update path to reflect latest point
-				if (itemVisible && !Double.isNaN(transX1) 
-						&& !Double.isNaN(transY1)) {
+				if (itemVisible && !Double.isNaN(transX1) && !Double.isNaN(transY1)) {
 					float x = (float) transX1;
 					float y = (float) transY1;
 					if (orientation == PlotOrientation.HORIZONTAL) {
@@ -105,17 +101,15 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 					if (s.isLastPointGood()) {
 						// TODO: check threshold
 						s.seriesPath.lineTo(x, y);
-					}
-					else {
+					} else {
 						s.seriesPath.moveTo(x, y);
 					}
 					s.setLastPointGood(true);
-				}
-				else {
+				} else {
 					s.setLastPointGood(false);
 				}
 				if (item == dataset.getItemCount(series) - 1) {
-					if (true/*s.seriesIndex == series*/) {
+					if (true/* s.seriesIndex == series */) {
 						// draw path
 						g2.setStroke(getSeriesStroke(series));
 						g2.setPaint(getSeriesPaint(series));
@@ -131,38 +125,30 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 				if (!Double.isNaN(x0) && !Double.isNaN(y0)) {
 					boolean drawLine = true;
 					if (getPlotDiscontinuous()) {
-						// only draw a line if the gap between the current and 
+						// only draw a line if the gap between the current and
 						// previous data point is within the threshold
 						int numX = dataset.getItemCount(series);
 						double minX = dataset.getXValue(series, 0);
 						double maxX = dataset.getXValue(series, numX - 1);
 						if (this.getGapThresholdType() == UnitType.ABSOLUTE) {
 							drawLine = Math.abs(x1 - x0) <= this.getGapThreshold();
-						}
-						else {
-							drawLine = Math.abs(x1 - x0) <= ((maxX - minX) 
-									/ numX * getGapThreshold());
+						} else {
+							drawLine = Math.abs(x1 - x0) <= ((maxX - minX) / numX * getGapThreshold());
 						}
 					}
 					if (drawLine) {
-						double transX0 = domainAxis.valueToJava2D(x0, dataArea,
-								xAxisLocation);
-						double transY0 = rangeAxis.valueToJava2D(y0, dataArea, 
-								yAxisLocation);
+						double transX0 = domainAxis.valueToJava2D(x0, dataArea, xAxisLocation);
+						double transY0 = rangeAxis.valueToJava2D(y0, dataArea, yAxisLocation);
 
 						// only draw if we have good values
-						if (Double.isNaN(transX0) || Double.isNaN(transY0) 
-								|| Double.isNaN(transX1) || Double.isNaN(transY1)) {
+						if (Double.isNaN(transX0) || Double.isNaN(transY0) || Double.isNaN(transX1) || Double.isNaN(transY1)) {
 							return;
 						}
 
 						if (orientation == PlotOrientation.HORIZONTAL) {
-							state.workingLine.setLine(transY0, transX0, 
-									transY1, transX1);
-						}
-						else if (orientation == PlotOrientation.VERTICAL) {
-							state.workingLine.setLine(transX0, transY0, 
-									transX1, transY1);
+							state.workingLine.setLine(transY0, transX0, transY1, transX1);
+						} else if (orientation == PlotOrientation.VERTICAL) {
+							state.workingLine.setLine(transX0, transY0, transX1, transY1);
 						}
 
 						if (state.workingLine.intersects(dataArea)) {
@@ -171,8 +157,7 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 					}
 				}
 			}
-		}		
-
+		}
 
 		// we needed to get this far even for invisible items, to ensure that
 		// seriesPath updates happened, but now there is nothing more we need
@@ -185,45 +170,46 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 
 			Shape shape = getItemShape(series, item);
 			if (orientation == PlotOrientation.HORIZONTAL) {
-				shape = ShapeUtilities.createTranslatedShape(shape, transY1, 
-						transX1);
-			}
-			else if (orientation == PlotOrientation.VERTICAL) {
-				shape = ShapeUtilities.createTranslatedShape(shape, transX1, 
-						transY1);
+				shape = ShapeUtilities.createTranslatedShape(shape, transY1, transX1);
+			} else if (orientation == PlotOrientation.VERTICAL) {
+				shape = ShapeUtilities.createTranslatedShape(shape, transX1, transY1);
 			}
 			if (shape.intersects(dataArea)) {
 				if (getItemShapeFilled(series, item)) {
 					g2.fill(shape);
-				}
-				else {
+				} else {
 					g2.draw(shape);
 				}
-													
+
 				boolean selected = false;
-				for (DataItem2D dItem : selectedItems){
-					//if(allItems.indexOf(dItem) == item){
-					if(dItem.getIndex() == item){
+				for (DataItem2D selectedItem : selectedItems) {
+//					System.out.println("drawing " + item + " " + series);
+//					System.out.println("comparing to currently selected " + selectedItem.getName() + " " + selectedItem.getIndex() + " " + selectedItem.getSeries());
+					if (selectedItem.getIndex() == item && selectedItem.getSeries() == series) {
 						selected = true;
 						break;
 					}
 				}
-				
-				if(selected){
+
+				if (selected) {
 					Rectangle rect = shape.getBounds();
-					rect.grow(2,2);
+					rect.grow(2, 2);
 					g2.draw(rect);
 				}
-				
-				//store names and coordinates for point selection
+
+				// store names and coordinates for point selection
 				Rectangle rect = new Rectangle();
-				rect.setSize(shape.getBounds().getSize());//TODO This should be scaled too
+				rect.setSize(shape.getBounds().getSize());// TODO This should
+				// be scaled too
 				rect.setLocation(chartPanel.translateJava2DToScreen(shape.getBounds().getLocation()));
-									
-				allItems.get(item).setBounds(rect);																	
-				
-				//logger.debug(chartPanel.translateJava2DToScreen(shape.getBounds().getLocation()));
-				//logger.debug("drew item " + itemNames.get(item) + " to " + shape.getBounds().x + "," + shape.getBounds().y + ", with h=" + shape.getBounds().height + ", w=" + shape.getBounds().width);  
+
+				allItems.get(item).setBounds(rect);
+
+				// logger.debug(chartPanel.translateJava2DToScreen(shape.getBounds().getLocation()));
+				// logger.debug("drew item " + itemNames.get(item) + " to " +
+				// shape.getBounds().x + "," + shape.getBounds().y + ", with h="
+				// + shape.getBounds().height + ", w=" +
+				// shape.getBounds().width);
 			}
 			entityArea = shape;
 
@@ -232,13 +218,9 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 		if (getPlotImages()) {
 			Image image = getImage(plot, series, item, transX1, transY1);
 			if (image != null) {
-				Point hotspot = getImageHotspot(plot, series, item, transX1, 
-						transY1, image);
-				g2.drawImage(image, (int) (transX1 - hotspot.getX()), 
-						(int) (transY1 - hotspot.getY()), null);
-				entityArea = new Rectangle2D.Double(transX1 - hotspot.getX(), 
-						transY1 - hotspot.getY(), image.getWidth(null), 
-						image.getHeight(null));
+				Point hotspot = getImageHotspot(plot, series, item, transX1, transY1, image);
+				g2.drawImage(image, (int) (transX1 - hotspot.getX()), (int) (transY1 - hotspot.getY()), null);
+				entityArea = new Rectangle2D.Double(transX1 - hotspot.getX(), transY1 - hotspot.getY(), image.getWidth(null), image.getHeight(null));
 			}
 
 		}
@@ -250,24 +232,21 @@ public class PositionRecordingRenderer extends StandardXYItemRenderer {
 			if (orientation == PlotOrientation.HORIZONTAL) {
 				xx = transY1;
 				yy = transX1;
-			}          
-			drawItemLabel(g2, orientation, dataset, series, item, xx, yy, 
-					(y1 < 0.0));
+			}
+			drawItemLabel(g2, orientation, dataset, series, item, xx, yy, (y1 < 0.0));
 		}
 
 		/*
-		int domainAxisIndex = plot.getDomainAxisIndex(domainAxis);
-		int rangeAxisIndex = plot.getRangeAxisIndex(rangeAxis);
-		updateCrosshairValues(crosshairState, x1, y1, domainAxisIndex, 
-				rangeAxisIndex, transX1, transY1, orientation);
+		 * int domainAxisIndex = plot.getDomainAxisIndex(domainAxis); int
+		 * rangeAxisIndex = plot.getRangeAxisIndex(rangeAxis);
+		 * updateCrosshairValues(crosshairState, x1, y1, domainAxisIndex,
+		 * rangeAxisIndex, transX1, transY1, orientation);
 		 */
 
 		// add an entity for the item...
 		if (entities != null) {
-			addEntity(entities, entityArea, dataset, series, item, 
-					transX1, transY1);
+			addEntity(entities, entityArea, dataset, series, item, transX1, transY1);
 		}
 	}
 
-	
-}		
+}
--- src/main/java/fi/csc/microarray/client/visualisation/methods/VennDiagram.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/VennDiagram.java
@@ -1,4 +1,5 @@
 package fi.csc.microarray.client.visualisation.methods;
+
 import java.awt.BorderLayout;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -26,7 +27,9 @@ import fi.csc.microarray.client.visualisation.VisualisationMethod;
 import fi.csc.microarray.client.visualisation.methods.VenndiPlot.AREAS;
 import fi.csc.microarray.databeans.DataBean;
 
-public class VennDiagram extends Visualisation implements PropertyChangeListener{
+public class VennDiagram extends Visualisation implements PropertyChangeListener {
+
+	private static final String IDENTIFIER_COLUMN = "/identifier";
 
 	public VennDiagram(VisualisationFrame frame) {
 		super(frame);
@@ -40,163 +43,157 @@ public class VennDiagram extends Visualisation implements PropertyChangeListener
 
 	@Override
 	public JPanel getParameterPanel() {
-		if (paramPanel == null){
+		if (paramPanel == null) {
 			paramPanel = new JPanel();
 			paramPanel.setPreferredSize(Visualisation.PARAMETER_SIZE);
 			paramPanel.setLayout(new BorderLayout());
 
-			list = new AnnotateListPanel();				
+			list = new AnnotateListPanel();
 
 			JTabbedPane tabPane = new JTabbedPane();
 			tabPane.addTab("Selected", list);
-			
+
 			paramPanel.add(tabPane, BorderLayout.CENTER);
-		}		
+		}
 		return paramPanel;
 	}
-	
-	
+
 	@Override
-	public JComponent getVisualisation(List<DataBean> datas) throws Exception {				
-		
-		if(datas.size() < 2 || datas.size() > 3){
+	public JComponent getVisualisation(List<DataBean> datas) throws Exception {
+
+		if (datas.size() < 2 || datas.size() > 3) {
 			throw new IllegalArgumentException("Venn Diagram can be used only with two or three datasets");
 		}
-			
+
 		Map<String, Integer> A = new HashMap<String, Integer>();
 		Map<String, Integer> B = new HashMap<String, Integer>();
 		Map<String, Integer> C = new HashMap<String, Integer>();
-		
-		
+
 		int i = 0;
-		for (String name : datas.get(0).queryFeatures("/column/ ").asStrings()) {
+		for (String name : datas.get(0).queryFeatures(IDENTIFIER_COLUMN).asStrings()) {
 			A.put(name, i++);
-			
+
 		}
-		
+
 		i = 0;
-		for (String name : datas.get(1).queryFeatures("/column/ ").asStrings()) {
+		for (String name : datas.get(1).queryFeatures(IDENTIFIER_COLUMN).asStrings()) {
 			B.put(name, i++);
 		}
-		
+
 		i = 0;
-		if(datas.size() == 3){
-			for (String name : datas.get(2).queryFeatures("/column/ ").asStrings()) {
+		if (datas.size() == 3) {
+			for (String name : datas.get(2).queryFeatures(IDENTIFIER_COLUMN).asStrings()) {
 				C.put(name, i++);
 			}
 		}
-				
-		//Every AREA is mapped to the set of String containing the identifiers of each area.
+
+		// Every AREA is mapped to the set of String containing the identifiers
+		// of each area.
 		Map<AREAS, Set<String>> sortedIds = new HashMap<AREAS, Set<String>>(VenndiPlot.AREAS.values().length);
-		
-		for (AREAS area: AREAS.values()){
+
+		for (AREAS area : AREAS.values()) {
 			sortedIds.put(area, new HashSet<String>());
 		}
-		
+
 		sortedIds.get(AREAS.ABC).addAll(A.keySet());
 		sortedIds.get(AREAS.ABC).retainAll(B.keySet());
 		sortedIds.get(AREAS.ABC).retainAll(C.keySet());
-		
+
 		sortedIds.get(AREAS.AB).addAll(A.keySet());
 		sortedIds.get(AREAS.AB).retainAll(B.keySet());
 		sortedIds.get(AREAS.AB).removeAll(C.keySet());
-		
+
 		sortedIds.get(AREAS.BC).addAll(B.keySet());
 		sortedIds.get(AREAS.BC).retainAll(C.keySet());
 		sortedIds.get(AREAS.BC).removeAll(A.keySet());
-		
+
 		sortedIds.get(AREAS.AC).addAll(A.keySet());
 		sortedIds.get(AREAS.AC).retainAll(C.keySet());
 		sortedIds.get(AREAS.AC).removeAll(B.keySet());
-		
+
 		sortedIds.get(AREAS.A).addAll(A.keySet());
 		sortedIds.get(AREAS.A).removeAll(B.keySet());
 		sortedIds.get(AREAS.A).removeAll(C.keySet());
-		
+
 		sortedIds.get(AREAS.B).addAll(B.keySet());
 		sortedIds.get(AREAS.B).removeAll(A.keySet());
 		sortedIds.get(AREAS.B).removeAll(C.keySet());
-		
+
 		sortedIds.get(AREAS.C).addAll(C.keySet());
 		sortedIds.get(AREAS.C).removeAll(A.keySet());
 		sortedIds.get(AREAS.C).removeAll(B.keySet());
-		
-		String[][] idTable= new String[AREAS.values().length][];				
-		
-		for (AREAS area : AREAS.values()){
+
+		String[][] idTable = new String[AREAS.values().length][];
+
+		for (AREAS area : AREAS.values()) {
 			idTable[area.ordinal()] = sortedIds.get(area).toArray(new String[0]);
-		}		
-		
+		}
+
 		Map<DataBean, Map<String, Integer>> indexMaps = new HashMap<DataBean, Map<String, Integer>>();
-		
+
 		indexMaps.put(datas.get(0), A);
 		indexMaps.put(datas.get(1), B);
-		if(datas.size() > 2){
+		if (datas.size() > 2) {
 			indexMaps.put(datas.get(2), C);
 		}
-		
+
 		plot = new VenndiPlot(idTable, datas, indexMaps, this);
-		
+
 		chartPanel = makePanel(new JFreeChart("Venn-diagram", plot));
-		
+
 		chartPanel.addChartMouseListener(plot);
 
 		this.updateSelectionsFromApplication(false);
-	
+
 		application.addPropertyChangeListener(this);
-		
+
 		return chartPanel;
 	}
-	
+
 	@Override
 	public boolean canVisualise(DataBean bean) throws MicroarrayException {
-		//throw new NotImplementedException();
 		return false;
 	}
-	
+
 	@Override
 	public boolean canVisualise(List<DataBean> beans) throws MicroarrayException {
-		if(beans.size() < 2 || beans.size() > 3){
+		if (beans.size() < 2 || beans.size() > 3) {
 			return false;
 		}
-		
-		for(DataBean data: beans){
+
+		for (DataBean data : beans) {
 			boolean isTabular = VisualisationMethod.SPREADSHEET.getHeadlessVisualiser().canVisualise(data);
-			
-			//FIXME Image is passed through wrongly
-			if(!(isTabular && data.queryFeatures("/column/chip.*").exists())){
+
+			if (!(isTabular && data.queryFeatures(IDENTIFIER_COLUMN).exists())) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	@Override 
-	public boolean isForSingleData(){
+
+	@Override
+	public boolean isForSingleData() {
 		return false;
 	}
-	
-	@Override 
-	public boolean isForMultipleDatas(){
+
+	@Override
+	public boolean isForMultipleDatas() {
 		return true;
 	}
 
 	public void propertyChange(PropertyChangeEvent evt) {
-		if (evt instanceof RowChoiceEvent &&
-				evt.getSource() != this){
-			
+		if (evt instanceof RowChoiceEvent && evt.getSource() != this) {
+
 			updateSelectionsFromApplication(false);
 		}
 	}
-	
-
 
 	private void updateSelectionsFromApplication(boolean dispatchEvent) {
-		
+
 		List<String> selected = new ArrayList<String>();
 		try {
 
-			for(DataBean data: plot.getDataset().getDataBeans()){
+			for (DataBean data : plot.getDataset().getDataBeans()) {
 				RowSelectionManager manager = application.getSelectionManager().getRowSelectionManager(data);
 				selected.addAll(manager.getSelectedIdentifiers());
 			}
@@ -204,34 +201,29 @@ public class VennDiagram extends Visualisation implements PropertyChangeListener
 		} catch (MicroarrayException e) {
 			application.reportException(new MicroarrayException("Unable to get selected identifiers", e));
 		}
-		
+
 		plot.setSelected(selected);
 		plot.setSelectedForList(false);
-		//To show new selections
+		// To show new selections
 		chartPanel.repaint();
 	}
-
 
 	@Override
 	public JComponent getVisualisation(DataBean data) throws Exception {
 		throw new MicroarrayException("Venn Diagram can be used only with two or three datasets");
 	}
 
-
 	/**
-	 * AnnotateListPanel.setSelectedListContentMultipleDatas can't be seen from the VenndiPlot, so we
-	 * need to forward this information.
+	 * AnnotateListPanel.setSelectedListContentMultipleDatas can't be seen from
+	 * the VenndiPlot, so we need to forward this information.
 	 * 
 	 * @param ids
 	 * @param indexes
 	 * @param venndiPlot
 	 * @param dispatchEvent
 	 */
-	public void setSelectedListContent(List<String> ids,
-			Map<DataBean, Set<Integer>> indexes, VenndiPlot venndiPlot,
-			boolean dispatchEvent) {
-		
-		list.setSelectedListContentMultipleDatas(ids,
-				 indexes, this, dispatchEvent);
+	public void setSelectedListContent(List<String> ids, Map<DataBean, Set<Integer>> indexes, VenndiPlot venndiPlot, boolean dispatchEvent) {
+
+		list.setSelectedListContentMultipleDatas(ids, indexes, this, dispatchEvent);
 	}
 }
