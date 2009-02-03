c16b4dcb6d222b213ead1f8f1f83c992bfc86c68
akallio <devnull@localhost>
2009-02-03 11:44:26 +0000

    refactored status UI code and made status bar show file transfer progress

---
+++ src/main/java/fi/csc/microarray/client/StatusBar.java
@@ -0,0 +1,253 @@
+package fi.csc.microarray.client;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.Timer;
+
+import fi.csc.microarray.MicroarrayConfiguration;
+import fi.csc.microarray.util.MemUtil;
+
+public class StatusBar {
+
+	private SwingClientApplication application;
+	private JPanel statusPanel = null;
+	private JLabel statusLabel = null;
+	private JProgressBar jobStatusIndicator = null;
+	private Color normalStatusIndicatorColor;
+	private JProgressBar memoryIndicator;
+	private JButton jobListButton;
+
+	private int oldTaskCount = 0;
+	private Timer blinker;
+
+	
+	public StatusBar(SwingClientApplication application) {
+		this.application = application;
+	}
+	
+	public JPanel getStatusPanel() {
+		if (statusPanel == null) {
+			// add status bar
+			jobStatusIndicator = new JProgressBar();
+
+			jobStatusIndicator.setName("jobStatusIndicator");
+			jobStatusIndicator.setStringPainted(true);
+			normalStatusIndicatorColor = jobStatusIndicator.getForeground();
+			
+			memoryIndicator = new JProgressBar(0, 100);
+			memoryIndicator.setStringPainted(true);
+			memoryIndicator.setToolTipText("Shows how much of the available memory is allocated. Click the indicator to release unused memory (garbage collection).");
+			memoryIndicator.addMouseListener(new MouseListener() {
+				public void mouseClicked(MouseEvent e) {
+					application.garbageCollect();
+				}
+
+				public void mouseEntered(MouseEvent e) { /* do nothing */
+				}
+
+				public void mouseExited(MouseEvent e) { /* do nothing */
+				}
+
+				public void mousePressed(MouseEvent e) { /* do nothing */
+				}
+
+				public void mouseReleased(MouseEvent e) { /* do nothing */
+				}
+			});
+
+			statusLabel = new JLabel();
+			String labelText = "Connected to " + MicroarrayConfiguration.getValue("messaging", "broker_host");
+			statusLabel.setText(labelText);
+			statusLabel.setBorder(jobStatusIndicator.getBorder());
+
+			jobListButton = new JButton(VisualConstants.ARROW_UP_ICON);
+			jobListButton.setName("jobListButton");
+			jobListButton.setToolTipText("View tasks");
+
+			jobListButton.addMouseListener(new MouseListener() {
+
+				public void mouseClicked(MouseEvent e) {
+					flipTaskListVisibility(true);
+				}
+
+				public void mouseEntered(MouseEvent e) {
+				}
+
+				public void mouseExited(MouseEvent e) {
+				}
+
+				public void mousePressed(MouseEvent e) {
+				}
+
+				public void mouseReleased(MouseEvent e) {
+				}
+			});
+
+			this.taskCountChanged(0, 0, false);
+
+			statusPanel = new JPanel(new GridBagLayout());
+			GridBagConstraints c = new GridBagConstraints();
+			c.weightx = 1.0;
+			c.weighty = 1.0;
+			c.fill = GridBagConstraints.BOTH;
+			c.insets.set(0, 0, 0, 5);
+			statusPanel.add(statusLabel, c);
+			c.gridx = 1;
+			c.weightx = 0.0;
+			statusPanel.add(jobListButton, c);
+			c.gridx = 2;
+			statusPanel.add(jobStatusIndicator, c);
+			c.insets.set(0, 0, 0, 0);
+			c.gridx = 3;
+			statusPanel.add(memoryIndicator, c);
+		}
+		return statusPanel;
+	}
+
+	public void flipTaskListVisibility(boolean closeIfVisible) {
+		application.refreshTaskList();
+		boolean wasVisible = application.getTaskListScreen().getFrame().isVisible();
+		// boolean wasActive =
+		// childScreens.get("TaskList").getFrame().isActive();
+		boolean wasShowing = application.getTaskListScreen().getFrame().isShowing();
+		boolean wasNotIconified = true;
+		if ((application.getTaskListScreen().getFrame().getExtendedState() & JFrame.ICONIFIED) == 1) {
+			wasNotIconified = false;
+		}
+
+		if (wasVisible && wasShowing && wasNotIconified && closeIfVisible) {
+			// hide
+			application.getTaskListScreen().getFrame().setVisible(false);
+			jobListButton.setIcon(VisualConstants.ARROW_UP_ICON);
+			jobListButton.setToolTipText("View Task manager");
+		} else {
+			// show
+			application.getTaskListScreen().getFrame().setVisible(true);
+			application.getTaskListScreen().getFrame().setExtendedState(JFrame.NORMAL);
+			application.getTaskListScreen().getFrame().toFront();
+			application.getTaskListScreen().getFrame().setFocusable(true);
+			application.getTaskListScreen().getFrame().requestFocus();
+			application.getTaskListScreen().getFrame().toFront();
+			jobListButton.setIcon(VisualConstants.ARROW_DOWN_ICON);
+			jobListButton.setToolTipText("Hide Task manager");
+		}
+	}
+	
+	private void setProgressBar(String indicatorText, boolean indeterminate, int value) {
+		jobStatusIndicator.setString(indicatorText);
+		jobStatusIndicator.setIndeterminate(indeterminate);
+		jobStatusIndicator.setValue(value);
+		if (value > 0) {
+			jobStatusIndicator.setForeground(normalStatusIndicatorColor.brighter());
+		} else {
+			jobStatusIndicator.setForeground(normalStatusIndicatorColor);
+		}
+	}
+	
+	protected void taskCountChanged(int taskCount, int completion, boolean useBlinking) {
+
+		// update status bar
+		boolean jobStarted;
+		if (oldTaskCount < taskCount) {
+			jobStarted = true;
+		} else {
+			jobStarted = false;
+		}
+
+		oldTaskCount = taskCount;
+
+		// update progress bar state
+		if (taskCount == 0) {
+			setProgressBar("Ready", false, 0);
+		} else {
+			if (taskCount == 1) {
+				if (completion != -1) {
+					setProgressBar("Transferring data", false, completion);
+				} else {
+					setProgressBar(taskCount + " job running", true, 0);
+				}
+			} else {
+				setProgressBar(taskCount + " jobs running", true, 0);
+			}
+			
+		}		
+
+		if (useBlinking && jobStarted) {
+			blinkProgressBar();
+		}
+
+		// update title
+		application.updateWindowTitle(taskCount);
+
+		// update task list
+		application.refreshTaskList();
+	}
+
+	/**
+	 * Adds a fancy blinking effect to progress bar after job is started
+	 * 
+	 */
+	private void blinkProgressBar() {
+
+		class Blinker implements ActionListener {
+
+			private boolean isBold;
+			private int repeats;
+			private int repeated;
+
+			public Blinker(int repeats) {
+				this.isBold = false;
+				this.repeats = repeats;
+				this.repeated = 0;
+			}
+
+			public void actionPerformed(ActionEvent e) {
+				if (repeated == repeats) {
+					jobStatusIndicator.setFont(jobStatusIndicator.getFont().deriveFont(Font.PLAIN));
+					((Timer) e.getSource()).stop();
+					return;
+				}
+
+				if (isBold) {
+					jobStatusIndicator.setFont(jobStatusIndicator.getFont().deriveFont(Font.PLAIN));
+					isBold = false;
+				} else {
+					jobStatusIndicator.setFont(jobStatusIndicator.getFont().deriveFont(Font.BOLD));
+					isBold = true;
+				}
+				repeated++;
+			}
+		}
+
+		if (blinker != null) {
+			blinker.stop();
+			jobStatusIndicator.setFont(jobStatusIndicator.getFont().deriveFont(Font.PLAIN));
+		}
+
+		blinker = new Timer(350, new Blinker(6));
+		blinker.start();
+
+	}
+
+	public void updateMemoryIndicator() {
+		memoryIndicator.setString(MemUtil.getMemInfo());
+		memoryIndicator.setValue((int) (((float) MemUtil.getUsed()) / ((float) Runtime.getRuntime().maxMemory()) * 100f));
+	}
+
+	public void setFontSize(float fontSize) {
+		jobStatusIndicator.setFont(jobStatusIndicator.getFont().deriveFont(fontSize));
+	}
+
+}
