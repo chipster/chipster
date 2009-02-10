de858ac0e11d98a3fbf8ef5d7eb127891463f876
akallio <devnull@localhost>
2009-02-10 12:22:43 +0000

    fix from jarno

--- src/main/R/R-2.6.1/norm-prenormalized.R
+++ src/main/R/R-2.6.1/norm-prenormalized.R
@@ -3,7 +3,7 @@
 # expression values need to be specifies as Sample in the Import tool. If you want to be able to use annotation,
 # you need to SPECIFY THE CHIPTYPE.)
 # INPUT CDNA microarray[...].tsv OUTPUT normalized.tsv, phenodata.tsv
-# PARAMETER chiptype [empty] DEFAULT empty (chiptype)
+# PARAMETER chiptype STRING DEFAULT empty (chiptype)
 
 
 # Process prenormalized
@@ -19,7 +19,8 @@ columns.other<-c("flag", "annotation")
 
 files<-dir()
 files<-files[files!="phenodata.tsv"]
-dat<-read.maimages(files=files, columns=columns, annotation=annotation, other.columns=columns.other) 
+dat<-read.maimages(files=files, columns=columns, annotation=annotation,
+other.columns=columns.other)
 
 # Mock normalization
 dat2<-normalizeBetweenArrays(dat$R, method="none")
@@ -31,22 +32,25 @@ training<-c(rep("", length(sample)))
 time<-c(rep("", length(sample)))
 random<-c(rep("", length(sample)))
 if(chiptype=="empty") {
-   chiptype<-c("cDNA")
+    chiptype<-c("cDNA")
 }
-write.table(data.frame(sample=sample, chiptype=chiptype, group=group), file="phenodata.tsv", sep="\t", row.names=F, col.names=T, quote=F)
+write.table(data.frame(sample=sample, chiptype=chiptype, group=group),
+file="phenodata.tsv", sep="\t", row.names=F, col.names=T, quote=F)
 
-#Preparing data for export
+# Prepare data for export
 M<-data.frame(dat2)
 colnames(M)<-paste("chip.", colnames(M), sep="")
-rownames(M)<-as.matrix(dat$genes)
+M2<-aggregate(M, as.list(dat$genes), mean)
+rownames(M2)<-M2$identifier
+M2<-M2[,-1]
 
 if(chiptype!="cDNA") {
-   # Including gene names to data
-   library(chiptype, character.only=T)
-   symbol<-gsub("\'", "", data.frame(unlist(as.list(get(paste(chiptype, "SYMBOL", sep="")))))[rownames(M),])
-   genename<-gsub("\'", "", data.frame(unlist(as.list(get(paste(chiptype, "GENENAME", sep="")))))[rownames(M),])
+    # Including gene names to data
+    library(chiptype, character.only=T)
+    symbol<-gsub("\'", "", data.frame(unlist(as.list(get(paste(chiptype, "SYMBOL", sep="")))))[rownames(M),])
+    genename<-gsub("\'", "", data.frame(unlist(as.list(get(paste(chiptype, "GENENAME", sep="")))))[rownames(M),])
 }
 
 if(chiptype=="cDNA") {
-   write.table(data.frame(round(M, digits=2)), file="normalized.tsv", col.names=T, quote=F, sep="\t", row.names=T)
+    write.table(data.frame(round(M2, digits=2)), file="normalized.tsv", col.names=T, quote=F, sep="\t", row.names=T)
 }
