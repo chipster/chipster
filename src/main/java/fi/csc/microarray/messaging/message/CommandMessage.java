ece0f71c15d6a2ec53b6fa904076d427bae4ce8c
uivelo <devnull@localhost>
2009-02-13 14:06:28 +0000

    Added url request command

--- src/main/java/fi/csc/microarray/messaging/message/CommandMessage.java
+++ src/main/java/fi/csc/microarray/messaging/message/CommandMessage.java
@@ -20,6 +20,7 @@ public class CommandMessage extends ParameterMessage {
 	public final static String COMMAND_OFFER = "offer";
 	public final static String COMMAND_ACCEPT_OFFER = "choose";
 
+	public final static String COMMAND_URL_REQUEST ="url-request";
 	
 	private String command;
 	
