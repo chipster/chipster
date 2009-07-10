01a343a4c698621b2179ec9c04c7c3da32113864
petriklemela <devnull@localhost>
2009-07-10 11:47:51 +0000

    Imports cleaned

--- src/main/java/fi/csc/microarray/client/QuickLinkPanel.java
+++ src/main/java/fi/csc/microarray/client/QuickLinkPanel.java
@@ -3,7 +3,6 @@ package fi.csc.microarray.client;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
