ca9e3e10b9627de1bd1b538c7d0bf9395ee23cd9
uivelo <devnull@localhost>
2009-11-10 10:35:36 +0000

    Renamed R-2.9.0 to R-2.9

--- src/main/tools/R-2.9.0/input-output.R
+++
@@ -1,5 +0,0 @@
-# ANALYSIS Test/InputOutput (Empty analysis for testing system wide data transfer.)
-# INPUT CDNA input.tsv OUTPUT output.tsv
-
-input<-read.table(c("input.tsv"), header=T, sep="\t")
-write.table(c(1, 2, 3), file="output.tsv", quote=FALSE, col.names=FALSE, row.names=FALSE)
--- src/main/tools/R-2.9.0/input-sleep-output.R
+++
@@ -1,4 +0,0 @@
-# ANALYSIS Test/InputSleepOutput (Just sleep with a few inputs and outputs.)
-# INPUT AFFY input[...].cel OUTPUT input1.cel, input2.cel, input3.cel, input4.cel
-
-system("sleep 10")
