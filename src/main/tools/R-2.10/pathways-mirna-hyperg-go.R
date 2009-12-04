e412c3c82b42251eb28c7741281ab0ffff13245b
mgentile <devnull@localhost>
2009-12-04 14:26:18 +0000

    changed name and default p value cutoff

--- src/main/tools/R-2.10/pathways-mirna-hyperg-go.R
+++ src/main/tools/R-2.10/pathways-mirna-hyperg-go.R
@@ -1,13 +1,14 @@
-# ANALYSIS Pathways/"miRNA, enriched GO" (Performs a statistical test for enrichments of GO terms in the predicted gene targets of a list of miRNA ID:s.)
-# INPUT GENE_EXPRS normalized.tsv OUTPUT hyperg_go_mf.tsv, hyperg_go_bp.tsv, hyperg_go_cc.tsv
-# PARAMETER p.value.threshold DECIMAL FROM 0 TO 1 DEFAULT 0.05 (P-value threshold)
-# PARAMETER species [human, mouse, rat] DEFAULT human (the species for which the miRNA:s have been analyzed)
+# ANALYSIS Pathways/"GO enrichment for miRNA targets" (Performs a statistical test for enrichments of GO terms in the predicted gene targets of a list of miRNA ID:s.)
+# INPUT GENE_EXPRS normalized.tsv OUTPUT hyperg_go.tsv
+# PARAMETER ontology [biological_process, molecular_function, cellular_component] DEFAULT biological_process (the ontology to be analyzed)
+# PARAMETER p.value.threshold DECIMAL FROM 0 TO 1 DEFAULT 0.01 (P-value threshold)
+# PARAMETER species [human] DEFAULT human (the species for which the miRNA:s have been analyzed)
 
 # POSSIBLE summary.feature [gene, transcript] DEFAULT gene (should the targets for the miRNA:s be transcripts or genes?)
 
 
 # miRNA hypergeometric test for GO
-# MG, 3.10.2009
+# MG, 4.11.2009
 
 # force "transcript" mode
 summary.feature <- "transcript"
@@ -115,7 +116,7 @@ if (summary.feature=="transcript") {
 #go2term <- GO2df.fun(url="ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2go.gz")
 
 # Fetch links between GO id and term from locally installed files
-go2term <- read.table(.path(path.mappings, "go_id_to_go_term.txt"), sep="\t")
+go2term <- read.table(file=file.path(path.mappings, "go_id_to_go_term.txt"), sep="\t")
 
 # Define the reference list to compare against, i.e. all transcripts in tran2gomf
 if (summary.feature=="gene") {
@@ -143,34 +144,49 @@ sample.bp <- intersect(targets.list[,2], reference.list.bp)
 sample.cc <- intersect(targets.list[,2], reference.list.cc)
 
 # Run the hypergeometric test for over-representation
-test.mf <- corna.test.fun(sample.mf,
+if (ontology=="molecular_function") {
+	test.mf <- corna.test.fun(sample.mf,
                 reference.list.mf,
                 tran2gomf,
                 fisher=T,
                 sort="fisher",
                 min.pop=5, 
                 desc=go2term) 
-test.bp <- corna.test.fun(sample.bp,
+	significant.mf <- test.mf[test.mf$fisher <= p.value.threshold, ]
+	write.table(significant.mf, file="hyperg_go.tsv", sep="\t", quote=F)	
+}
+	
+if (ontology=="biological_process") {
+	test.bp <- corna.test.fun(sample.bp,
                 reference.list.bp,
                 tran2gobp,
                 fisher=T,
                 sort="fisher",
                 min.pop=5, 
                 desc=go2term) 
-test.cc <- corna.test.fun(sample.cc,
+	significant.bp <- test.bp[test.bp$fisher <= p.value.threshold, ]
+	write.table(significant.bp, file="hyperg_go.tsv", sep="\t", quote=F)
+}
+
+if (ontology=="cellular_component") {
+	test.cc <- corna.test.fun(sample.cc,
                 reference.list.cc,
                 tran2gocc,
                 fisher=T,
                 sort="fisher",
                 min.pop=5, 
                 desc=go2term) 
-
+	significant.cc <- test.cc[test.cc$fisher <= p.value.threshold, ]
+	write.table(significant.cc, file="hyperg_go.tsv", sep="\t", quote=F)	
+}
+	
 # Fetch the list of significant terms
-significant.mf <- test.mf[test.mf$fisher <= p.value.threshold, ]
-significant.bp <- test.bp[test.bp$fisher <= p.value.threshold, ]
-significant.cc <- test.cc[test.cc$fisher <= p.value.threshold, ]
+#significant.mf <- test.mf[test.mf$fisher <= p.value.threshold, ]
+#significant.bp <- test.bp[test.bp$fisher <= p.value.threshold, ]
+#significant.cc <- test.cc[test.cc$fisher <= p.value.threshold, ]
 
 # Write the results in three different tables
-write.table(significant.mf, file="hyperg_go_mf.tsv", sep="\t")
-write.table(significant.bp, file="hyperg_go_bp.tsv", sep="\t")
-write.table(significant.cc, file="hyperg_go_cc.tsv", sep="\t")
+#write.table(significant.mf, file="hyperg_go_mf.tsv", sep="\t", quote=F)
+#write.table(significant.bp, file="hyperg_go_bp.tsv", sep="\t", quote=F)
+#write.table(significant.cc, file="hyperg_go_cc.tsv", sep="\t", quote=F)
+
