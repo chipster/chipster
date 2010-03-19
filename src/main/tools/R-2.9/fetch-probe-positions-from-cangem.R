ca5fef4cee1cef72051220a195f1c6495cb6656d
gentile <devnull@localhost>
2010-03-19 12:05:09 +0200

    Some fixes by ilari

--- src/main/tools/R-2.9/fetch-probe-positions-from-cangem.R
+++ src/main/tools/R-2.9/fetch-probe-positions-from-cangem.R
@@ -6,12 +6,12 @@
 
 # fetch-probe-positions-from-cangem.R
 # Ilari Scheinin <firstname.lastname@helsinki.fi>
-# 2010-03-11
+# 2010-03-18
 
 # check for valid platform accession
-dataset <- toupper(platform.accession)
-if (length(grep('^CG-PLM-[0-9]+$', dataset))==0)
-  stop('Not a valid platform accession: ', dataset)
+platform.accession <- toupper(platform.accession)
+if (length(grep('^CG-PLM-[0-9]+$', platform.accession))==0)
+	stop('Not a valid platform accession: ', platform.accession)
 
 dat <- read.table('normalized.tsv', header=TRUE, sep='\t', as.is=TRUE, row.names=1)
 
@@ -28,9 +28,8 @@ colnames(platform)[colnames(platform)=='chr'] <- 'chromosome'
 rownames(platform) <- platform[,1]
 platform$chromosome <- factor(platform$chromosome, levels=c(1:22, "X", "Y", "MT"), ordered=TRUE)
 
-dat2 <- cbind(platform[rownames(dat), c('chromosome', 'start', 'end')], dat)
+dat2 <- cbind(platform[rownames(dat), c('chromosome', 'start', 'end')], dat, row.names=rownames(dat))
 dat2 <- dat2[order(dat2$chromosome, dat2$start),]
-
 
 write.table(dat2, file='normalized.tsv', quote=FALSE, sep='\t', col.names=TRUE, row.names=TRUE)
 
