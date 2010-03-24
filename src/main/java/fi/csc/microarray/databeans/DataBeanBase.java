8253832206be5e1dfc4967e849312fd50da87754
taavi <devnull@localhost>
2010-03-24 10:04:40 +0200

    Refactored stuff from FSDataBean to DataBean.

--- src/main/java/fi/csc/microarray/databeans/DataBeanBase.java
+++ src/main/java/fi/csc/microarray/databeans/DataBeanBase.java
@@ -3,6 +3,7 @@ package fi.csc.microarray.databeans;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -12,6 +13,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.log4j.Logger;
 
+import fi.csc.microarray.client.operation.Operation;
 import fi.csc.microarray.databeans.features.Feature;
 import fi.csc.microarray.databeans.features.QueryResult;
 import fi.csc.microarray.databeans.features.RequestExecuter;
@@ -21,6 +23,18 @@ import fi.csc.microarray.util.StreamStartCache;
 import fi.csc.microarray.util.Strings;
 
 public abstract class DataBeanBase extends DataItemBase implements DataBean {
+	
+	private static class LinkedBean {
+		
+		LinkedBean(Link link, DataBean bean) {
+			this.link = link;
+			this.bean = bean;			
+		}
+		
+		Link link;
+		DataBean bean;
+	}
+	
 	
 	/**
 	 * Logger for this class
@@ -34,7 +48,16 @@ public abstract class DataBeanBase extends DataItemBase implements DataBean {
 	private URL url = null;
 	private boolean contentChanged = true;
 	private Lock contentLock = new ReentrantLock();
+
+	private LinkedList<LinkedBean> outgoingLinks = new LinkedList<LinkedBean>();
+	private LinkedList<LinkedBean> incomingLinks = new LinkedList<LinkedBean>();
 	
+	protected Date date;
+	private Operation sourceOperation;
+	private String notes;
+
+	protected ContentType contentType;
+
 	
 	protected DataBeanBase(DataManagerBase dataManager) {
 		this.dataManager = dataManager;
@@ -190,6 +213,130 @@ public abstract class DataBeanBase extends DataItemBase implements DataBean {
 	public void setUrl(URL url) {
 		this.url = url;
 	}
+
+	public Date getDate() {
+		return this.date;
+	}
+	
+	public void setName(String newName) {
+		super.setName(newName);
+
+		ContentChangedEvent cce = new ContentChangedEvent(this);		
+		dataManager.dispatchEventIfVisible(cce);
+	}
+
+	public void removeLink(Link type, DataBean target) {
+		for (LinkedBean outgoingLink : outgoingLinks) {
+			if (outgoingLink.bean == target && outgoingLink.link == type) {
+				
+				outgoingLinks.remove(outgoingLink);
+				
+				// FIXME don't reference incoming directly
+				DataBeanBase fsBean = ((DataBeanBase)outgoingLink.bean);
+				
+				for (LinkedBean incomingLink : fsBean.incomingLinks) {
+					if (incomingLink.bean == this && incomingLink.link == type) {
+						fsBean.incomingLinks.remove(incomingLink);
+						
+						// both links were found
+						LinksChangedEvent lce = new LinksChangedEvent(this, target, type, false);
+						dataManager.dispatchEventIfVisible(lce);
+						return; 
+					}
+				}
+			}
+		}
+		throw new RuntimeException("internal error: failed locate links for: " + this.getName()  + " <" + type + "> " + target.getName());
+	}
+	
+
+
+	public void addLink(Link type, DataBean target) {
+		if (target == null) {
+			return;
+		}		
+		
+		// FIXME add more internal state validation to FSDataBean
+//		for (LinkedBean linkedBean : outgoingLinks) {
+//			if (linkedBean.bean == target && linkedBean.link == type) {
+//				throw new RuntimeException("duplicate link");
+//			}
+//		}
+
+		// make both parties aware of the link
+		// FIXME don't reference incomingLinks directly
+		((DataBeanBase)target).incomingLinks.add(new LinkedBean(type, this));
+		outgoingLinks.add(new LinkedBean(type, target));
+
+		// dispatch events only if both visible
+		if (this.getParent() != null && target.getParent() != null) {
+			LinksChangedEvent lce = new LinksChangedEvent(this, target, type, true);
+			dataManager.dispatchEvent(lce);
+		}
+
+	}
+	
+	
+	/**
+	 * @return The parent dataset of this dataset.
+	 */
+	public List<DataBean> getLinkTargets(Link... types) {
+		return getLinkedBeans(types, outgoingLinks);
+	}
+
+	public List<DataBean> getLinkSources(Link... types) {
+		return getLinkedBeans(types, incomingLinks);
+	}
+	
+	private List<DataBean> getLinkedBeans(Link[] types, LinkedList<LinkedBean> links) {
+		LinkedList<DataBean> targets = new LinkedList<DataBean>();
+		for (LinkedBean linkTarget : links) {
+			for (Link type : types) {
+				if (linkTarget.link == type) {
+					targets.add(linkTarget.bean);
+					break;
+				}
+			}
+		}
+		return targets;
+	}
+
+	/**
+	 * @return The operation that has been selected for this dataset (and which
+	 * 		   may have already been done at least once to produce another
+	 * 		   dataset, or which has not yet been conducted, and maybe never
+	 * 		   will, depending on the user).
+	 */
+	public Operation getOperation() {
+		return sourceOperation;
+	}
+
+	/**
+	 * Associates the given operation with this DataBean.
+	 * 
+	 * @param operation to associate
+	 */
+	public void setOperation(Operation operation) {
+		this.sourceOperation = operation;
+	}
+
+	
+	public void setNotes(String notes) {
+		this.notes = notes;		
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public ContentType getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(ContentType contentType) {
+		this.contentType = contentType;
+	}
+
 	
 	
 }
--- src/main/java/fi/csc/microarray/databeans/fs/FSDataBean.java
+++ src/main/java/fi/csc/microarray/databeans/fs/FSDataBean.java
@@ -8,42 +8,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
 
-import fi.csc.microarray.client.operation.Operation;
 import fi.csc.microarray.databeans.ContentChangedEvent;
 import fi.csc.microarray.databeans.ContentType;
 import fi.csc.microarray.databeans.DataBean;
 import fi.csc.microarray.databeans.DataBeanBase;
 import fi.csc.microarray.databeans.DataFolder;
 import fi.csc.microarray.databeans.DataManagerBase;
-import fi.csc.microarray.databeans.LinksChangedEvent;
 import fi.csc.microarray.exception.MicroarrayException;
 import fi.csc.microarray.util.Files;
 
 public class FSDataBean extends DataBeanBase {
 	
-	private static class LinkedBean {
-		
-		LinkedBean(Link link, DataBean bean) {
-			this.link = link;
-			this.bean = bean;			
-		}
-		
-		Link link;
-		DataBean bean;
-	}
 	
-	private ContentType contentType;
-	private String name;
-	private Operation sourceOperation;
-	private Date date;
-	private String notes;
-	
-	private LinkedList<LinkedBean> outgoingLinks = new LinkedList<LinkedBean>();
-	private LinkedList<LinkedBean> incomingLinks = new LinkedList<LinkedBean>();
-
 	private File contentFile;
 	
 	
@@ -66,131 +43,8 @@ public class FSDataBean extends DataBeanBase {
 		this.contentFile = contentFile;
 		this.contentType = contentType;
 	}
-
-	public Date getDate() {
-		return this.date;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-	
-	public void removeLink(Link type, DataBean target) {
-		for (LinkedBean outgoingLink : outgoingLinks) {
-			if (outgoingLink.bean == target && outgoingLink.link == type) {
-				
-				outgoingLinks.remove(outgoingLink);
-				FSDataBean fsBean = ((FSDataBean)outgoingLink.bean);
-				
-				for (LinkedBean incomingLink : fsBean.incomingLinks) {
-					if (incomingLink.bean == this && incomingLink.link == type) {
-						fsBean.incomingLinks.remove(incomingLink);
-						
-						// both links were found
-						LinksChangedEvent lce = new LinksChangedEvent(this, target, type, false);
-						dataManager.dispatchEventIfVisible(lce);
-						return; 
-					}
-				}
-			}
-		}
-		throw new RuntimeException("internal error: failed locate links for: " + this.getName()  + " <" + type + "> " + target.getName());
-	}
-	
-	public DataFolder getParent() {
-		return this.parent;
-	}
-
-	public void setName(String newName) {
-		this.name = newName;
-
-		ContentChangedEvent cce = new ContentChangedEvent(this);		
-		dataManager.dispatchEventIfVisible(cce);
-	}
-
-	public void addLink(Link type, DataBean target) {
-		if (target == null) {
-			return;
-		}		
 		
-		// FIXME add more internal state validation to FSDataBean
-//		for (LinkedBean linkedBean : outgoingLinks) {
-//			if (linkedBean.bean == target && linkedBean.link == type) {
-//				throw new RuntimeException("duplicate link");
-//			}
-//		}
-
-		// make both parties aware of the link
-		((FSDataBean)target).incomingLinks.add(new LinkedBean(type, this));
-		outgoingLinks.add(new LinkedBean(type, target));
-
-		// dispatch events only if both visible
-		if (this.getParent() != null && target.getParent() != null) {
-			LinksChangedEvent lce = new LinksChangedEvent(this, target, type, true);
-			dataManager.dispatchEvent(lce);
-		}
-
-	}
 	
-		
-	/**
-	 * @return The parent dataset of this dataset.
-	 */
-	public List<DataBean> getLinkTargets(Link... types) {
-		return getLinkedBeans(types, outgoingLinks);
-	}
-
-	public List<DataBean> getLinkSources(Link... types) {
-		return getLinkedBeans(types, incomingLinks);
-	}
-	
-	private List<DataBean> getLinkedBeans(Link[] types, LinkedList<LinkedBean> links) {
-		LinkedList<DataBean> targets = new LinkedList<DataBean>();
-		for (LinkedBean linkTarget : links) {
-			for (Link type : types) {
-				if (linkTarget.link == type) {
-					targets.add(linkTarget.bean);
-					break;
-				}
-			}
-		}
-		return targets;
-	}
-	
-	/**
-	 * @return The operation that has been selected for this dataset (and which
-	 * 		   may have already been done at least once to produce another
-	 * 		   dataset, or which has not yet been conducted, and maybe never
-	 * 		   will, depending on the user).
-	 */
-	public Operation getOperation() {
-		return sourceOperation;
-	}
-
-	/**
-	 * Associates the given operation with this DataBean.
-	 * 
-	 * @param operation to associate
-	 */
-	public void setOperation(Operation operation) {
-		this.sourceOperation = operation;
-	}
-	
-	public ContentType getContentType() {
-		return contentType;
-	}
-	
-	public void setContentType(ContentType contentType) {
-		this.contentType = contentType;
-	}
-
-	public void setNotes(String notes) {
-		this.notes = notes;		
-	}
-
-	public String getNotes() {
-		return notes;
-	}
 	
 	public InputStream getRawContentByteStream() throws MicroarrayException {
 		InputStream is;
