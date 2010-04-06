6d008d588b26a15a69e6f6a56421768ab7e6ddac
gentile <devnull@localhost>
2010-04-06 11:12:31 +0300

    Changed name of script slightly

--- src/main/tools/R-2.9/norm-prenormalized-affy.R
+++ src/main/tools/R-2.9/norm-prenormalized-affy.R
@@ -1,4 +1,4 @@
-# ANALYSIS Normalisation/"Process prenormalized affy" (If you import prenormalized Affymetrix data that is not in Chipster format, you
+# ANALYSIS Normalisation/"Process prenormalized Affymetrix" (If you import prenormalized Affymetrix data that is not in Chipster format, you
 # need to import it through the Import Wizard, and then use this tool for preprocessing it. During data import, make sure to mark every column containing normalized
 # expression values as "Sample" and the column containing the Affymetrix probe ID:s as "Identifier". If you want to be able to use annotation,
 # you need to SPECIFY THE CHIPTYPE, e.g. "hgu133a2.db".)
