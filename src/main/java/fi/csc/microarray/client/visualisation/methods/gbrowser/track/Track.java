264f8f78b555eae4397756cb86b7a5b0b50104f6
akallio <devnull@localhost>
2010-06-02 10:28:49 +0300

    view sized AreaRequests used again

--- src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/track/Track.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/track/Track.java
@@ -17,7 +17,6 @@ import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.FilePa
 import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.Strand;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.message.AreaRequest;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.message.BpCoord;
-import fi.csc.microarray.client.visualisation.methods.gbrowser.message.BpCoordRegion;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.message.Chromosome;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.message.FsfStatus;
 
@@ -62,16 +61,8 @@ public abstract class Track implements AreaResultListener {
 			status.concise = isConcised();
 
 			Collection<ColumnType> defCont = getDefaultContents();
-			
-			BpCoordRegion requestRegion = new BpCoordRegion(view.getBpRegion());
-			requestRegion.start.bp -= 1000;
-			requestRegion.end.bp += 1000;
-			
-			if (requestRegion.start.bp < 0) {
-				requestRegion.start.bp = 0l;
-			}
 
-			view.getQueueManager().addAreaRequest(file, new AreaRequest(requestRegion, defCont, status), true);
+			view.getQueueManager().addAreaRequest(file, new AreaRequest(view.getBpRegion(), defCont, status), true);
 		}
 	}
 
