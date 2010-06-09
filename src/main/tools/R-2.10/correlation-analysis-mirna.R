787b90faee2e4966324f91f0a1a2b02b5968d632
akallio <devnull@localhost>
2010-06-09 10:21:37 +0300

    bug fix from Ilari

--- src/main/tools/R-2.10/correlation-analysis-mirna.R
+++ src/main/tools/R-2.10/correlation-analysis-mirna.R
@@ -28,6 +28,7 @@
 
 # Correlation analysis of miRNA targets
 # MG, 11.2.2010
+# IS, 8.6.2010 bugfix
 
 # Loads the libraries
 library(RmiR)
@@ -90,12 +91,8 @@ mirna.data.3 <- mirna.data.2[,order(mirna.order)]
 gene.data.3 <- gene.data.2[,order(gene.order)]
 
 # Create data set appropriate for correlation testing
-mirna.data.4 <- cbind(rownames(mirna.data.3), as.numeric(mirna.data.3[,1]))
-gene.data.4 <- cbind(rownames(gene.data.3), as.numeric(gene.data.3[,1]))
-mirna.data.4 <- as.data.frame(mirna.data.4)
-gene.data.4 <- as.data.frame(gene.data.4)
-mirna.data.4[,2] <- as.numeric(mirna.data.4[,2])
-gene.data.4[,2] <- as.numeric(gene.data.4[,2])
+mirna.data.4 <- data.frame(mirna=rownames(mirna.data.3), exprs=mirna.data.3[,1])
+gene.data.4 <- data.frame(gene=rownames(gene.data.3), exprs=gene.data.3[,1])
 # check that the gene list actually contain at least one miRNA target
 try(merged.table <- read.mir(gene=gene.data.4, mirna=mirna.data.4,
 				annotation=chip.type), silent=TRUE)
@@ -108,12 +105,8 @@ merged.table <- read.mir(gene=gene.data.4, mirna=mirna.data.4,
 		annotation=chip.type, verbose=TRUE)
 
 for (count in 2:number.conditions) {
-	mirna.data.4 <- cbind(rownames(mirna.data.3), as.numeric(mirna.data.3[,count]))
-	gene.data.4 <- cbind(rownames(gene.data.3), as.numeric(gene.data.3[,count]))
-	mirna.data.4 <- as.data.frame(mirna.data.4)
-	gene.data.4 <- as.data.frame(gene.data.4)
-	mirna.data.4[,2] <- as.numeric(mirna.data.4[,2])
-	gene.data.4[,2] <- as.numeric(gene.data.4[,2])
+	mirna.data.4 <- data.frame(mirna=rownames(mirna.data.3), exprs=mirna.data.3[,count])
+	gene.data.4 <- data.frame(gene=rownames(gene.data.3), exprs=gene.data.3[,count])
 	temp.table <- read.mir(gene=gene.data.4, mirna=mirna.data.4,
 			annotation=chip.type, verbose=TRUE)
 	temp.table
@@ -137,11 +130,11 @@ for (mirna.count in 1:number.mirna) {
 }
 
 # Find genes with statistically significant positive correlation
-results.positive <- results.table[results.table[,4]<0,]
+results.positive <- results.table[results.table[,4]>0,]
 results.positive.significant <- results.positive[results.positive[,5]<=p.value.threshold,]
 
 # Find genes with statistically significant negative correlation
-results.negative <- results.table[results.table[,4]>=0,]
+results.negative <- results.table[results.table[,4]<=0,]
 results.negative.significant <- results.negative[results.negative[,5]<=p.value.threshold,]
 
 # Write the results to tables to be read into Chipster
