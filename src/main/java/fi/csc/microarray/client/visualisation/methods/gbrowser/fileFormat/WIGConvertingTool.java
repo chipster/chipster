f10819e5a5879a4ea06c86642d705ae510f1288f
zukauska <devnull@localhost>
2010-07-29 10:13:53 +0300

    simple wig format converting

--- src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/fileFormat/WIGConvertingTool.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/fileFormat/WIGConvertingTool.java
@@ -1,9 +1,17 @@
 package fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+
+/**
+ * 
+ * simple wig format converting tool
+ *  
+ */
 
 public class WIGConvertingTool {
 	
@@ -14,31 +22,40 @@ public class WIGConvertingTool {
 		Long span;
 		Long startPosition;
 		Long step;
-		Long headerPosition;
 	}
 	
 	HeaderDefinition header;
 	
-	public void convert(File file){
+	public void convert(File file) {
 		
-		String line;
+		String line = "";
 		int linesCount = 0;
 		
 		FileReader fReader;
+		BufferedWriter writer;
 		try {
 			fReader = new FileReader(file);
 			BufferedReader reader = new BufferedReader(fReader);
 			
+			writer = new BufferedWriter(new FileWriter(file.getAbsolutePath()+".out"));
+			
+			while (!line.contains("track")) {
+				line = reader.readLine();
+			}
+			
 			while (reader.ready()) {
 				line = reader.readLine();
-				if (line.contains("Step")){
+				if (line.contains("Step")) {
 					header = setHeader(line);
+					linesCount = 0;
 				} else {
 					line = formLine(line, linesCount);
-					//write line
+					writer.write(line);
+					linesCount++;
 				}
 			}
 			
+			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -59,24 +76,24 @@ public class WIGConvertingTool {
 			//TODO configure depending on converting
 			switch (cols.length) {
 				case 2:
-					header.type = cols[0];//variable step
+					header.type = cols[0]; //variable step
 					header.chr = cols[1].replace("chrom=chr", "");
 					
 					break;
 				case 3:
-					header.type = cols[0];//variable step
+					header.type = cols[0]; //variable step
 					header.chr = cols[1].replace("chrom=chr", "");
 					header.span = Long.parseLong(cols[2].replace("span=", ""));
 					break;
 				case 4:
-					header.type = cols[0];//fixed step
-					header.chr = cols[1];//.replace("chrom=chr", "");
+					header.type = cols[0]; //fixed step
+					header.chr = cols[1].replace("chrom=", "");
 					header.startPosition = Long.parseLong(cols[2].replace("start=", ""));
 					header.step = Long.parseLong(cols[3].replace("step=", ""));
 					break;
 				case 5:
-					header.type = cols[0];//fixed step
-					header.chr = cols[1];//.replace("chrom=chr", "");
+					header.type = cols[0]; //fixed step
+					header.chr = cols[1].replace("chrom=", "");
 					header.startPosition = Long.parseLong(cols[2].replace("start=", ""));
 					header.step = Long.parseLong(cols[3].replace("step=", ""));
 					header.span = Long.parseLong(cols[4].replace("span=", ""));
@@ -94,7 +111,16 @@ public class WIGConvertingTool {
 	}
 	
 	public String formLine(String line, int linesCount) {
-		//TODO formLine
+		
+		if (header.type.startsWith("variable")) {
+			String[] splitted = line.split("\t");
+			return header.chr + "\t" + splitted[0] + "\t" + 
+			Long.parseLong(splitted[0]) + header.span + "\t" + splitted[1] + "\n";  
+		} else {
+			return header.chr + "\t" + (header.startPosition + linesCount * header.step) +
+			"\t" + (header.startPosition + linesCount * header.step + header.span - 1) + "\t" +
+			line + "\n";
+		}
 	}
 
 }
