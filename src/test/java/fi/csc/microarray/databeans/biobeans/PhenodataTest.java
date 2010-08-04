246b1c6ba8b21b4d6e0fe8b88437e757bb902aca
akallio <devnull@localhost>
2010-08-04 12:45:12 +0300

    added util for output set search

--- src/test/java/fi/csc/microarray/databeans/biobeans/PhenodataTest.java
+++ src/test/java/fi/csc/microarray/databeans/biobeans/PhenodataTest.java
@@ -33,13 +33,16 @@ public class PhenodataTest {
 	public void testPhenodataRetrieval() throws MicroarrayException, IOException {
 		DataBean normalised = manager.createDataBean("normalised.tsv");
 		DataBean filtered = manager.createDataBean("filtered.tsv");
+		DataBean filtered2 = manager.createDataBean("filtered2.tsv");
 		DataBean phenodata = manager.createDataBean("phenodata.tsv");
 		
 		filtered.addLink(Link.DERIVATION, normalised);
+		filtered2.addLink(Link.DERIVATION, normalised);
 		phenodata.addLink(Link.ANNOTATION, normalised);
 		
 		Assert.assertEquals(phenodata, LinkUtils.retrieveInherited(normalised, Link.ANNOTATION));
 		Assert.assertEquals(phenodata, LinkUtils.retrieveInherited(filtered, Link.ANNOTATION));
+		Assert.assertEquals(2, LinkUtils.retrieveOutputSet(filtered).length);
 	}
 	
 	
