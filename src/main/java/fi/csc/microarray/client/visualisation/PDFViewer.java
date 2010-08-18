0328b6e252098ce8baf885a77ef0f2c796608d13
akallio <devnull@localhost>
2010-08-18 10:13:33 +0300

    separated pdf viewer component into library (not part of chipster codebase any more)

--- src/main/java/fi/csc/microarray/client/visualisation/PDFViewer.java
+++ src/main/java/fi/csc/microarray/client/visualisation/PDFViewer.java
@@ -1,9 +1,15 @@
 package fi.csc.microarray.client.visualisation;
 
-import javax.swing.JComponent;
+import java.io.File;
+import java.io.IOException;
 
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+import fi.csc.microarray.client.Session;
 import fi.csc.microarray.databeans.DataBean;
 import fi.csc.microarray.exception.MicroarrayException;
+import fi.csc.microarray.util.SimplePDFViewer;
 
 public class PDFViewer extends Visualisation {
 
@@ -13,14 +19,28 @@ public class PDFViewer extends Visualisation {
 
 	@Override
 	public boolean canVisualise(DataBean bean) throws MicroarrayException {
-		// TODO Auto-generated method stub
-		return false;
+		return bean.isContentTypeCompatitible("application/pdf");
+		
 	}
 
 	@Override
 	public JComponent getVisualisation(DataBean data) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+        SimplePDFViewer viewer = new SimplePDFViewer(true);
+        File file = Session.getSession().getDataManager().getLocalFile(data);
+        viewer.openFile(file);
+		return viewer;
 	}
 
+	
+	public static void main(String[] args) throws IOException {
+        SimplePDFViewer viewer = new SimplePDFViewer(true);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(viewer);
+        frame.pack();
+        frame.setVisible(true);
+        File file = new File("document.pdf");
+        viewer.openFile(file);
+
+	}
 }
