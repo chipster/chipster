7fcc2c9e8e7dd572bd48249b06ed32cf89a4f42a
gentile <devnull@localhost>
2010-09-21 16:24:06 +0300

    updtated test settings

--- src/main/tools/R-2.12/Test-multiple-file-output.R
+++ src/main/tools/R-2.12/Test-multiple-file-output.R
@@ -1,6 +1,6 @@
 # TOOL "Statistics" / Test-multiple-file-output.R: "Takes a table of data, extracts the first 3 rows and returns them as individual 1-row tables" (Takes a table of data, extracts the first 3 rows and returns them as individual 1-row tables)
 # INPUT table.tsv: "Data file" TYPE GENERIC
-# OUTPUT result_{...}.tsv: "testing multiple file output"
+# OUTPUT result-test-{...}.tsv: "testing multiple file output"
 # OUTPUT table_1.tsv: "also one at a time, first row"
 # OUTPUT table_2.tsv: "also one at a time, second row"
 # OUTPUT table_3.tsv: "also one at a time, thirs row"
@@ -22,9 +22,9 @@ second_row <- data_table[2,]
 third_row <- data_table[3,]
 
 # Create the multiple files output
-write.table(first_row, "result_1.tsv", sep="\t", row.names=T, col.names=T, quote=F)
-write.table(second_row, "result_2.tsv", sep="\t", row.names=T, col.names=T, quote=F)
-write.table(third_row, "result_3.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+write.table(first_row, "result-test-1.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+write.table(second_row, "result-test-2.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+write.table(third_row, "result-test-3.tsv", sep="\t", row.names=T, col.names=T, quote=F)
 
 # Create the individual files output
 write.table(first_row, "table_1.tsv", sep="\t", row.names=T, col.names=T, quote=F)
