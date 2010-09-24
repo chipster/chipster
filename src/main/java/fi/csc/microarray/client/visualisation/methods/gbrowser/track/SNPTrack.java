b04768d392eb62a18f3ac4c3dcfbe147b6460871
zukauska <devnull@localhost>
2010-09-24 16:00:42 +0300

    improved snp track, added colors for consequence view, prioritized by importance

--- src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/track/SNPTrack.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/track/SNPTrack.java
@@ -13,45 +13,44 @@ import java.util.Set;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.DataSource;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.View;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.dataFetcher.AreaRequestHandler;
-import fi.csc.microarray.client.visualisation.methods.gbrowser.dataFetcher.ChunkTreeHandlerThread;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.drawable.Drawable;
-import fi.csc.microarray.client.visualisation.methods.gbrowser.drawable.LineDrawable;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.drawable.RectDrawable;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.ColumnType;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.message.AreaResult;
-import fi.csc.microarray.client.visualisation.methods.gbrowser.message.BpCoord;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.message.RegionContent;
 
 public class SNPTrack extends Track {
 	
-	// - is black
-	// frameshift is pink
-	// stop is red
-	// 
 	// prioritize (1. stop, 2. frameshift, synonymous, non_synonymous)
-	 
 	
-	private static int width = 6;
+	private static int width = 8;
 	Collection<RegionContent> values = new LinkedList<RegionContent>();
+	Long lastPosition;
 	Color a = new Color(64, 192, 64, 128);
 	Color c = new Color(64, 64, 192, 128);
 	Color g = new Color(128, 128, 128, 128);
 	Color t = new Color(192, 64, 64, 128);
 	
-	Color frameShift = new Color(255, 255, 255, 255);
-	Color none = new Color(64, 192, 64, 128);
-	Color upstream = new Color(64, 192, 64, 128);
-	Color intronic = new Color(0, 0, 0, 0);
-	Color intergenetic = new Color(0, 0, 0, 0);
-	Color non_synonymous_coding = new Color(0, 0, 0, 0);
-	Color synonymous_coding = new Color(0, 0, 0, 0);
-	Color frameshift_coding = new Color(0, 0, 0, 0);
+	//ordered by prioritiazation
+	private Color[] colors = new Color[] {
+			new Color(255, 0, 0, 128),//red
+			new Color(255, 0, 0, 128),//red
+			new Color(255, 105, 180, 128),//pink
+			new Color(255, 215, 0, 128),//gold
+			new Color(50, 205, 50, 128),//lemon green
+			new Color(190, 190, 190, 128),//gray
+	};
+	
+	private int lastColorIndex = colors.length-1;
 	
 	boolean changeView = false;
 	
+	//ordered according colors
 	enum ConsequenceName {
-		NONE, UPSTREAM, INTRONIC, INTERGENETIC, NON_SYNONYMOUS_CODING, 
-		SYNONYMOUS_CODING, FRAMESHIFT_CODING,
+		STOP_GAINED, STOP_LOST, FRAMESHIFT_CODING, SYNONYMOUS_CODING,
+		NON_SYNONYMOUS_CODING, UPSTREAM, INTRONIC, INTERGENETIC,  
+		COMPLEX_INDEL, PARTIAL_CODON, REGULATORY_REGION, WITHIN_MATURE_mIRNA,
+		PRIME5_UTR, PRIME3_UTR, NONE,  
 	}
 
 	public SNPTrack(View view, DataSource file, Class<? extends AreaRequestHandler> handler) {
@@ -74,52 +73,94 @@ public class SNPTrack extends Track {
 	                iter.remove();
 	                continue;
 	            }
+	            
+	            if (lastPosition == null) {
+					lastPosition = (long)getView().bpToTrack(value.region.start);
+				}
 	            String allele = (String)value.values.get(ColumnType.ALLELE);
 	            long position = getView().bpToTrack(value.region.start);
 	            if (changeView) {
 	            	String consequence = (String)value.values.get(ColumnType.CONSEQUENCE_TO_TRANSCRIPT);
-	            	if (consequence.equals(ConsequenceName.NONE.toString())) {
+	            	
+	            	//if we have multiple lines with same location
+	            	if (lastPosition == position) {
+	            		Iterator<Drawable> i = drawables.iterator();
+	            		int j = 0;
+	            		Drawable lastDrawable = null;
+	            		
+	            		//get the last drawable
+	            		while (i.hasNext()) {
+	            			lastDrawable = i.next();
+	            			j++;
+	            		}
+	            		if (lastDrawable == null) {
+	            			
+	            		} else {
+	            			int old = getColorNumber(lastDrawable.color);
+	            			int now = ConsequenceName.valueOf(consequence).ordinal();
+	            			
+	            			//if importance is less than the old one, then just skip it
+	            			if (now >= old) {
+	            			} else {
+	            				System.out.println(consequence);
+	            				//change the color
+	            				drawables.remove(lastDrawable);
+	            				drawables.add(new RectDrawable((int)position, 1, width,
+	            						getHeight(), colors[now], colors[now]));
+	            			}
+	            		}
+	            	} else {
 	            		drawables.add(new RectDrawable((int)position, 1, 
-	            				width, getHeight(), none, none));
-	            	} else if (consequence.equals(ConsequenceName.UPSTREAM.toString())) {
-	            		drawables.add(new RectDrawable((int)position, 1, 
-	            				width, getHeight(), upstream, upstream));
-	            	} else if (consequence.equals(ConsequenceName.INTRONIC.toString())) {
-	            		drawables.add(new RectDrawable((int)position, 1, 
-	            				width, getHeight(), intronic, intronic));
-	            	} else if (consequence.equals(ConsequenceName.INTERGENETIC.toString())) {
-	            		drawables.add(new RectDrawable((int)position, 1, 
-	            				width, getHeight(), intergenetic, intergenetic));
-	            	} else if (consequence.equals(ConsequenceName.NON_SYNONYMOUS_CODING.toString())) {
-	            		drawables.add(new RectDrawable((int)position, 1, 
-	            				width, getHeight(), non_synonymous_coding, non_synonymous_coding));
-	            	} else if (consequence.equals(ConsequenceName.SYNONYMOUS_CODING.toString())) {
-	            		drawables.add(new RectDrawable((int)position, 1, 
-	            				width, getHeight(), synonymous_coding, synonymous_coding));
-	            	} else if (consequence.equals(ConsequenceName.FRAMESHIFT_CODING)) {
-	            		drawables.add(new RectDrawable((int)position, 1, 
-	            				width, getHeight(), frameshift_coding, frameshift_coding));
+            				width, getHeight(), colors[getColorNumber(consequence)],
+            				colors[getColorNumber(consequence)]));
 	            	}
 	            } else {
 	            	//frameshift
-	            	if (allele.matches("[A-Z]/[A-Z-]/[A-Z-]")) {
-	            		
-	            	} else if (allele.startsWith("A")) {
+            		if (allele.matches("[A-Z]/[A-Z]/-") || 
+	            			allele.matches("[A-Z]/-") || allele.matches("[A-Z]/-/[A-Z]")) {
+	            		drawables.add(new RectDrawable((int)position, 1, width, getHeight(), 
+	            				colors[getColorNumber("FRAMESHIFT_CODING")], 
+	            				colors[getColorNumber("FRAMESHIFT_CODING")]));
+	            		//Y/X change from Y to X 
+	            	} else if (allele.matches("[A-Z]/A")) {
 		            	drawables.add(new RectDrawable((int)position, 1, width, getHeight(), a, a));
-		            } else if (allele.startsWith("C")) {
+		            } else if (allele.matches("[A-Z]/C")) {
 		            	drawables.add(new RectDrawable((int)position, 1, width, getHeight(), c, c));
-		            } else if (allele.startsWith("G")) {
+		            } else if (allele.matches("[A-Z]/G")) {
 		            	drawables.add(new RectDrawable((int)position, 1, width, getHeight(), g, g));
-		            } else if (allele.startsWith("T")) {
+		            } else if (allele.matches("[A-Z]/T")) {
 		            	drawables.add(new RectDrawable((int)position, 1, width, getHeight(), t, t));
 		            }
 	            }
+	            lastPosition = position;
 			}
 		}
 		
 		return drawables;
 	}
-
+	
+	private int getColorNumber(Color co) {
+		int index = -1;
+		for (Color c : colors) {
+			index++;
+			if (c.equals(co)) {
+				break;
+			}
+		}
+		if(index == -1) {
+			index = lastColorIndex;
+		}
+		return index;
+	}
+	
+	private int getColorNumber(String cn) {
+		int index = ConsequenceName.valueOf(cn).ordinal();
+		if (index > lastColorIndex) {
+			index = lastColorIndex;
+		}
+		return index;
+	}
+	
 	@Override
 	public void processAreaResult(AreaResult<RegionContent> areaResult) {
 		if (areaResult.content.values.get(ColumnType.STRAND) == getStrand()) {
@@ -152,7 +193,7 @@ public class SNPTrack extends Track {
 	@Override
     public Integer getHeight() {
         if (isVisible()) {
-            return 5;
+            return 12;
         } else {
             return 0;
         }
