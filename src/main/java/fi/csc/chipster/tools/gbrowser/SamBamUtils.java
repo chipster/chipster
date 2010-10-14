98f7ea37a5f3e6d4fbc172ab5a4af5e01e68587c
akallio <devnull@localhost>
2010-10-14 15:26:21 +0300

    first version of SAM/BAM utils

--- src/main/java/fi/csc/chipster/tools/gbrowser/SamBamUtils.java
+++ src/main/java/fi/csc/chipster/tools/gbrowser/SamBamUtils.java
@@ -1,28 +1,89 @@
 package fi.csc.chipster.tools.gbrowser;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import net.sf.picard.io.IoUtil;
+import net.sf.picard.sam.BuildBamIndex;
+import net.sf.picard.sam.ViewSam;
+import net.sf.samtools.SAMFileHeader;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMFileWriterFactory;
+import net.sf.samtools.SAMRecord;
+import net.sf.samtools.SAMSequenceDictionary;
+import net.sf.samtools.SAMSequenceRecord;
 
 public class SamBamUtils {
 	
+	private static final String CHROMOSOME_NAME_PREFIX = "chr";
+
 	public static void convertElandToBam(File elandFile, File bamFile) {
 		
 	}
-
-	public static void convertSamToBam(File samFile, File bamFile) {
-		
-	}
 	
-	public static void sortBam(File bamFile) {
+	public static void sortSamBam(File samBamFile, File sortedBamFile) {
 		
+		SAMFileReader reader = new SAMFileReader(IoUtil.openFileForReading(samBamFile));
+		reader.getFileHeader().setSortOrder(SAMFileHeader.SortOrder.coordinate);
+		final SAMFileWriter writer = new SAMFileWriterFactory().makeBAMWriter(reader.getFileHeader(), false, sortedBamFile);
+		final Iterator<SAMRecord> iterator = reader.iterator();
+		while (iterator.hasNext()) {
+			writer.addAlignment(iterator.next());
+		}
+		reader.close();
+		writer.close();
 	}
 
-	public static void normaliseBam(File bamFile) {
+	public static void normaliseBam(File bamFile, File normalisedBamFile) {
+		SAMFileReader reader = new SAMFileReader(IoUtil.openFileForReading(bamFile));
 		
+		SAMSequenceDictionary normalisedDictionary = new SAMSequenceDictionary();
+		for (SAMSequenceRecord sequenceRecord : reader.getFileHeader().getSequenceDictionary().getSequences()) {
+			String sequenceName = sequenceRecord.getSequenceName();
+			
+			// Strip prefix
+			if (sequenceName.startsWith(CHROMOSOME_NAME_PREFIX)) {
+				sequenceName = sequenceName.substring(CHROMOSOME_NAME_PREFIX.length());
+			}
+			
+			normalisedDictionary.addSequence(new SAMSequenceRecord(sequenceName, sequenceRecord.getSequenceLength()));
+		}
+		reader.getFileHeader().setSequenceDictionary(normalisedDictionary);
+
+		SAMFileWriter writer = new SAMFileWriterFactory().makeBAMWriter(reader.getFileHeader(), true, normalisedBamFile);
+
+		Iterator<SAMRecord> iterator = reader.iterator();
+		while (iterator.hasNext()) {
+			writer.addAlignment(iterator.next());
+		}
+		reader.close();
+		writer.close();
 	}
 	
 	public static void indexBam(File bamFile, File baiFile) {
+		BuildBamIndex.createIndex(new SAMFileReader(bamFile), baiFile); 
+	}
+	
+	public static void preprocessSamBam(File samBamFile, File preprocessedBamFile, File baiFile) throws IOException {
 		
+		// Sort
+		File sortedTempBamFile = File.createTempFile("sorted", "bam");
+		sortSamBam(samBamFile, sortedTempBamFile);
+		
+		// Normalise
+		normaliseBam(sortedTempBamFile, preprocessedBamFile);
+		sortedTempBamFile.delete();
+
+		// Index
+		indexBam(preprocessedBamFile, baiFile);
 	}
 
-
+	public static void main(String[] args) throws IOException {
+		File input = new File("/home/akallio/Desktop/test.sam");
+		File temp = File.createTempFile("temp", "bam");
+		normaliseBam(input, temp);
+		ViewSam.main(new String[] {"I=" + temp.getAbsolutePath()});
+	}
 }
