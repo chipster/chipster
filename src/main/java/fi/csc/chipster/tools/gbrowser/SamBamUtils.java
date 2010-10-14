e7046ec6b482819ff3a34dc17b803c8efd072997
akallio <devnull@localhost>
2010-10-14 12:43:17 +0300

    skeleton for SAM/BAM utils

---
+++ src/main/java/fi/csc/chipster/tools/gbrowser/SamBamUtils.java
@@ -0,0 +1,28 @@
+package fi.csc.chipster.tools.gbrowser;
+
+import java.io.File;
+
+public class SamBamUtils {
+	
+	public static void convertElandToBam(File elandFile, File bamFile) {
+		
+	}
+
+	public static void convertSamToBam(File samFile, File bamFile) {
+		
+	}
+	
+	public static void sortBam(File bamFile) {
+		
+	}
+
+	public static void normaliseBam(File bamFile) {
+		
+	}
+	
+	public static void indexBam(File bamFile, File baiFile) {
+		
+	}
+
+
+}
