def93d7ff9bd256f4eb20773cb4da569fccf804b
hupponen <devnull@localhost>
2010-10-15 13:06:26 +0300

    Organized imports.

--- src/main/java/fi/csc/microarray/client/ClientApplication.java
+++ src/main/java/fi/csc/microarray/client/ClientApplication.java
@@ -14,7 +14,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
