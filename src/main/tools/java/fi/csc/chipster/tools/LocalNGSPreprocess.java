deb48d42749433c9ce4ffe4cde4465aa5ef4f6b1
akallio <devnull@localhost>
2010-11-30 11:00:39 +0200

    bed preprocessing partly functional

--- src/main/tools/java/fi/csc/chipster/tools/LocalNGSPreprocess.java
+++ src/main/tools/java/fi/csc/chipster/tools/LocalNGSPreprocess.java
@@ -1,18 +1,24 @@
 package fi.csc.chipster.tools;
 
 import java.io.File;
+import java.io.IOException;
 
 import fi.csc.chipster.tools.gbrowser.SamBamUtils;
+import fi.csc.chipster.tools.gbrowser.TsvSorter;
 import fi.csc.chipster.tools.gbrowser.SamBamUtils.SamBamUtilState;
 import fi.csc.chipster.tools.gbrowser.SamBamUtils.SamBamUtilStateListener;
 import fi.csc.chipster.tools.gbrowser.SamBamUtils.ChromosomeNormaliser;
 import fi.csc.microarray.client.Session;
 import fi.csc.microarray.client.operation.Operation;
 import fi.csc.microarray.client.tasks.Task;
+import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.BEDParser;
+import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.ColumnType;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.ElandParser;
+import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.FileDefinition;
 import fi.csc.microarray.client.visualisation.methods.gbrowser.fileFormat.TsvParser;
 import fi.csc.microarray.databeans.DataBean;
 import fi.csc.microarray.databeans.DataManager;
+import fi.csc.microarray.exception.MicroarrayException;
 
 public class LocalNGSPreprocess implements Runnable {
 
@@ -52,74 +58,110 @@ public class LocalNGSPreprocess implements Runnable {
 
 			for (DataBean inputDataBean : task.getInputs()) {
 				File inputFile = dataManager.getLocalFile(inputDataBean);
+				String extension = inputFile.getName().substring(inputFile.getName().lastIndexOf("."));
 				
-				String outputName;
-				String indexOutputName; 
-				String extension = "";
-				int fileExtensionStartPosition = inputFile.getName().lastIndexOf(".");
-				if (fileExtensionStartPosition != -1) {
-					outputName = inputFile.getName().substring(0, fileExtensionStartPosition) + "-preprocessed";
-					extension = inputFile.getName().substring(fileExtensionStartPosition + 1);
-				} else {
-					outputName = inputFile.getName() + "-preprocessed";
-				}
-				
-				outputName = outputName + ".bam";
-				indexOutputName = outputName + ".bai";
-				
-				File outputFile = dataManager.createNewRepositoryFile(outputName);		
-				File indexOutputFile = dataManager.createNewRepositoryFile(indexOutputName);
-
-				// Run preprocessing
-				SamBamUtils samBamUtil= new SamBamUtils(new SamBamUtilStateListener() {
-
-					@Override
-					public void stateChanged(SamBamUtilState newState) {
-						System.out.println(newState.getState() + " " + newState.getPercentage());
-						task.setStateDetail(newState.getState() + " " + newState.getPercentage());
-					}
-					 
-				}, new ChromosomeNormaliser() {
-
-					public String normaliseChromosome(String chromosomeName) {
-						
-						// Add prefix, if it is missing
-						String CHROMOSOME_NAME_PREFIX = "chr";
-						if (!chromosomeName.startsWith(CHROMOSOME_NAME_PREFIX)) {
-							chromosomeName = CHROMOSOME_NAME_PREFIX + chromosomeName;
-						}
-						
-						// Remove postfix, if present
-						String SEPARATOR = ".";
-						if (chromosomeName.contains(SEPARATOR)) {
-							chromosomeName = chromosomeName.substring(0, chromosomeName.indexOf(SEPARATOR));
-						}
-						
-						return chromosomeName;
-					}
-				});
-
-				if (SamBamUtils.isSamBamExtension(extension)) {
-					samBamUtil.preprocessSamBam(inputFile, outputFile, indexOutputFile);
+				if (".bed".equals(extension)) {
+					preprocessBed(dataManager, inputFile);
 					
 				} else {
-					// Assume ELAND format
-					samBamUtil.preprocessEland(inputFile, outputFile, indexOutputFile);
+					preprocessReads(dataManager, inputFile, extension);
 				}
-
-				// create outputs in the client
-				DataBean outputBean = dataManager.createDataBean(outputName, outputFile);
-				DataBean indexOutputBean = dataManager.createDataBean(indexOutputName, indexOutputFile);
-				
-				// create new operation instance, without any inputs FIXME parameters are lost, sucks
-				outputBean.setOperation(new Operation(Session.getSession().getApplication().getOperationDefinition(task.getOperationID()), new DataBean[] {}));
-				indexOutputBean.setOperation(new Operation(Session.getSession().getApplication().getOperationDefinition(task.getOperationID()), new DataBean[] {}));
-				dataManager.getRootFolder().addChild(outputBean);
-				dataManager.getRootFolder().addChild(indexOutputBean);
+					
 				
 			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
+
+	private String generateFilename(File inputFile, String extension) {
+		
+		// Strip extension and add "-preprocessed"
+		String outputName;
+		int fileExtensionStartPosition = inputFile.getName().lastIndexOf(".");
+		if (fileExtensionStartPosition != -1) {
+			outputName = inputFile.getName().substring(0, fileExtensionStartPosition) + "-preprocessed";
+		} else {
+			outputName = inputFile.getName() + "-preprocessed";
+		}
+		
+		// Replace new extension
+		outputName = outputName + "." + extension;
+
+		return outputName;
+	}
+
+	private void preprocessReads(DataManager dataManager, File inputFile, String extension) throws IOException, MicroarrayException {
+		
+		String outputName = generateFilename(inputFile, "bam");
+		String indexOutputName = outputName + ".bai";
+
+		File outputFile = dataManager.createNewRepositoryFile(outputName);		
+		File indexOutputFile = dataManager.createNewRepositoryFile(indexOutputName);
+
+		// Run preprocessing
+		SamBamUtils samBamUtil= new SamBamUtils(new SamBamUtilStateListener() {
+
+			@Override
+			public void stateChanged(SamBamUtilState newState) {
+				System.out.println(newState.getState() + " " + newState.getPercentage());
+				task.setStateDetail(newState.getState() + " " + newState.getPercentage());
+			}
+			 
+		}, new ChromosomeNormaliser() {
+
+			public String normaliseChromosome(String chromosomeName) {
+				
+				// Add prefix, if it is missing
+				String CHROMOSOME_NAME_PREFIX = "chr";
+				if (!chromosomeName.startsWith(CHROMOSOME_NAME_PREFIX)) {
+					chromosomeName = CHROMOSOME_NAME_PREFIX + chromosomeName;
+				}
+				
+				// Remove postfix, if present
+				String SEPARATOR = ".";
+				if (chromosomeName.contains(SEPARATOR)) {
+					chromosomeName = chromosomeName.substring(0, chromosomeName.indexOf(SEPARATOR));
+				}
+				
+				return chromosomeName;
+			}
+		});
+		
+		if (SamBamUtils.isSamBamExtension(extension)) {
+			samBamUtil.preprocessSamBam(inputFile, outputFile, indexOutputFile);
+			
+		} else {
+			// Assume ELAND format
+			samBamUtil.preprocessEland(inputFile, outputFile, indexOutputFile);
+		}
+
+		// create outputs in the client
+		DataBean outputBean = dataManager.createDataBean(outputName, outputFile);
+		DataBean indexOutputBean = dataManager.createDataBean(indexOutputName, indexOutputFile);
+		
+		// create new operation instance, without any inputs FIXME parameters are lost, sucks
+		outputBean.setOperation(new Operation(Session.getSession().getApplication().getOperationDefinition(task.getOperationID()), new DataBean[] {}));
+		indexOutputBean.setOperation(new Operation(Session.getSession().getApplication().getOperationDefinition(task.getOperationID()), new DataBean[] {}));
+		dataManager.getRootFolder().addChild(outputBean);
+		dataManager.getRootFolder().addChild(indexOutputBean);
+	}
+	
+	private void preprocessBed(DataManager dataManager, File inputFile) throws Exception {
+		
+		String outputName = generateFilename(inputFile, "bed");
+		File outputFile = dataManager.createNewRepositoryFile(outputName);		
+
+		// Sort
+		FileDefinition def = new BEDParser().getFileDefinition();
+		new TsvSorter().sort(inputFile, outputFile, def.indexOf(ColumnType.CHROMOSOME), def.indexOf(ColumnType.BP_START));
+		
+		// Create outputs in the client
+		DataBean outputBean = dataManager.createDataBean(outputName, outputFile);
+		
+		// Create new operation instance, without any inputs FIXME parameters are lost, sucks
+		outputBean.setOperation(new Operation(Session.getSession().getApplication().getOperationDefinition(task.getOperationID()), new DataBean[] {}));
+		dataManager.getRootFolder().addChild(outputBean);
+	}
+
 }
