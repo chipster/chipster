0629e010af1e7482c1b51c4e74dc18653b325925
hupponen <devnull@localhost>
2011-01-19 14:15:46 +0200

    Added first updates from Jarno.

---
+++ src/main/tools/R-2.12/filter-by-column.R
@@ -0,0 +1,58 @@
+# ANALYSIS Utilities/"Filter using a column" (Allows the user to filter the genes on the basis of one numerical column. Altered by Mandy to include discrete values)
+# INPUT GENERIC normalized.tsv OUTPUT column-filter3.tsv
+# PARAMETER column COLUMN_SEL (Data column to filter by)
+# PARAMETER Type [Discrete, Continuous] DEFAULT Continuous (Choose Discrete or Continuous)
+# PARAMETER cutoff DECIMAL FROM -100000 TO 100000 DEFAULT 1 (Cut-off for filtering)
+# PARAMETER discrete.match STRING DEFAULT empty (String to search for)
+# PARAMETER smaller.or.larger [equal-to, smaller-than, larger-than, within, outside] DEFAULT smaller-than (Smaller or larger than the cutoff is filtered.
+# Use the "within" or "outside" options to filter symmmetrically around two cut-offs, useful for example when searching for 2-fold up- and down-regulated genes.)
+
+# Filtering using a column
+# JTT 31.1.2008
+# Amanda Miotto 16.1.2009 added the discrete option
+#
+# MG, 2.3.2010 added the option to filter "within" and "outside" a symmetrical
+# range of values
+
+# Parameter settings (default) for testing purposes
+#column<-c("chip.microarray001.cel")
+#Type<-c("Continuous")
+#cutoff<-c(7)
+#discrete.match<-c("empty")
+#smaller.or.larger<-c("larger-than")
+
+
+# Loads the normalized data
+file<-c("normalized.tsv")
+dat<-read.table(file, header=T, sep="\t", row.names=1)
+
+if(Type=="Continuous") {
+	# Extract the data to a vector
+	f<-dat[,grep(column, colnames(dat))]
+	# Filters the data
+	if(smaller.or.larger=="equal-to") {
+		dat2<-dat[which(f==cutoff),]
+	}
+	if(smaller.or.larger=="smaller-than") {
+		dat2<-dat[which(f<=cutoff),]
+	}
+	if(smaller.or.larger=="larger-than") {
+		dat2<-dat[which(f>=cutoff),]
+	}
+	if(smaller.or.larger=="outside") {
+		cutoff_2 <- -cutoff
+		dat2<-dat[which(f>=cutoff | f<=cutoff_2),]
+	}
+	if(smaller.or.larger=="within") {
+		cutoff_2 <- -cutoff
+		dat2<-dat[-which(f>=cutoff | f <=cutoff_2),]
+	}
+	
+	# Writing the data to disk
+	write.table(dat2, "column-filter3.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+}
+
+if (Type=="Discrete"){
+	dat2<-dat[grep(discrete.match, dat[,(as.vector(grep(column,names(dat))))]),]
+	write.table(dat2, "column-filter3.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+}
