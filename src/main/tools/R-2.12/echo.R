fada5748bb3400cdef4f5dcb039cd80c76d09ac9
hupponen <devnull@localhost>
2011-03-22 13:50:32 +0200

    Added test scripts to R-2.12.

---
+++ src/main/tools/R-2.12/echo.R
@@ -0,0 +1,7 @@
+# ANALYSIS Test/Echo (Prints the given parameter.)
+# PARAMETER echoParameter STRING (String which is printed to stdout.)
+
+print(echoParameter)
+
+
+
---
+++ src/main/tools/R-2.12/sleep.R
@@ -0,0 +1,6 @@
+# ANALYSIS Test/Sleep (Sleep for awhile.)
+
+print("Kohta nukutaan.")
+system("sleep 10")
+print("Nukuttu.")
+
