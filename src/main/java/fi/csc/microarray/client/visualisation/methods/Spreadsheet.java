576af6f114b78910a01a4a8b4b6b34ae656a5a04
akallio <devnull@localhost>
2011-04-04 12:57:25 +0300

    reverted changes from merge

--- src/main/java/fi/csc/microarray/client/visualisation/methods/Spreadsheet.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/Spreadsheet.java
@@ -15,8 +15,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.table.DefaultTableModel;
 
-import org.h2.command.ddl.CreateSequence;
-
 import fi.csc.microarray.client.visualisation.MicroarrayTable;
 import fi.csc.microarray.client.visualisation.Visualisation;
 import fi.csc.microarray.client.visualisation.VisualisationFrame;
@@ -27,95 +25,88 @@ import fi.csc.microarray.databeans.features.Table;
 import fi.csc.microarray.exception.MicroarrayException;
 
 /**
- * A GUI component for showing (and some day maybe even editing!) the microarray
- * data - that is, the channels and their intensities. It basically takes the
- * data from a dataset's microarray and puts it to a JTable. Once gene names are
- * implemented in microarrays, this could be used to annotate them (write some
- * describing names and notes for different genes).
+ * A GUI component for showing (and some day maybe even editing!) the
+ * microarray data - that is, the channels and their intensities.
+ * It basically takes the data from a dataset's microarray and puts it
+ * to a JTable. Once gene names are implemented in microarrays, this
+ * could be used to annotate them (write some describing names and notes
+ * for different genes).
  * 
  * @author Janne KÃ¤ki, Mikko Koski, Aleksi Kallio
- * 
+ *
  */
 
-public class Spreadsheet extends Visualisation {
 
+public class Spreadsheet extends Visualisation {
+	
 	public Spreadsheet(VisualisationFrame frame) {
 		super(frame);
-	}
+	}	
 
 	/**
-	 * PopupMenu for spread sheet view. Allows copy operation and annotating
+	 * PopupMenu for spread sheet view. Allows copy operation and annotating 
 	 * using Bioconductor
-	 * 
+	 *
 	 */
 	public class SpreadsheetPopupMenu extends JPopupMenu implements ActionListener {
 
 		private MicroarrayTable table;
 		private JMenuItem annotateMenuItem;
 		private JMenuItem copyMenuItem;
-		private JMenuItem createFromSelectionMenuItem;
-		private JMenuItem createExcludingSelectionMenuItem;
-
+		private JMenuItem filterMenuItem;
+		
 		public SpreadsheetPopupMenu(MicroarrayTable table) {
 			this.table = table;
-
-			copyMenuItem = new JMenuItem("Copy");
+			
+			copyMenuItem = new JMenuItem("Copy");			
 			annotateMenuItem = new JMenuItem("Create dataset and annotate with Bioconductor");
-			createFromSelectionMenuItem = new JMenuItem("Create dataset");
-			createExcludingSelectionMenuItem = new JMenuItem("Create dataset excluding selection");
-
-			copyMenuItem.addActionListener(this);
+			filterMenuItem = new JMenuItem("Create dataset");
+			
+			copyMenuItem.addActionListener(this);			
 			annotateMenuItem.addActionListener(this);
-			createFromSelectionMenuItem.addActionListener(this);
-			createExcludingSelectionMenuItem.addActionListener(this);
-
+			filterMenuItem.addActionListener(this);
+			
 			this.add(copyMenuItem);
 			this.add(new JSeparator());
-			this.add(createFromSelectionMenuItem);
 			this.add(annotateMenuItem);
-			this.add(createExcludingSelectionMenuItem);
+			this.add(filterMenuItem);
 		}
-
+		
 		public void actionPerformed(ActionEvent e) {
-
+						
 			if (e.getSource() == annotateMenuItem) {
 				VisualisationUtilities.annotateBySelection(getFrame().getDatas());
-			} else if (e.getSource() == copyMenuItem) {
-				table.copy();
-			} else if (e.getSource() == createFromSelectionMenuItem) {
-				VisualisationUtilities.filterBySelection(getFrame().getDatas());
-			} else if (e.getSource() == createExcludingSelectionMenuItem) {
-				DataBean data = getFrame().getDatas().get(0);
-				if (data != null) {
-					VisualisationUtilities.createDatasetExcludingSelection(data);
-				}
 			}
-
+			if (e.getSource() == copyMenuItem) {
+				table.copy();
+			}
+			if(e.getSource() == filterMenuItem) {
+				VisualisationUtilities.filterBySelection(getFrame().getDatas());
+			}
 		}
 	}
 
 	private final int COLUMNS_REQUIRES_SCROLLING = 8;
 
 	private MicroarrayTable table;
-
+	
 	/**
 	 * Creates a new TablePanel, which (for now) is dataset specific.
 	 * 
-	 * @param datas
-	 *            The dataset for which to create the table.
-	 * @throws Exception
-	 * @throws MicroarrayException
+	 * @param datas The dataset for which to create the table.
+	 * @throws Exception 
+	 * @throws MicroarrayException 
 	 */
 	@Override
 	public JComponent getVisualisation(DataBean data) throws Exception {
-		JPanel panel = new JPanel(new BorderLayout());
-
+		JPanel panel = new JPanel(new BorderLayout());				
+	
 		Table rowCounter = data.queryFeatures("/column/*").asTable();
 		int rowCount = 0;
 		while (rowCounter.nextRow()) {
 			rowCount++;
 		}
-
+		
 		Table columns = data.queryFeatures("restrict(/column/*)").asTable();
 		String[] columnTitles = new String[columns.getColumnCount()];
 		int counter = 0;
@@ -123,77 +114,78 @@ public class Spreadsheet extends Visualisation {
 			columnTitles[counter] = column;
 			counter++;
 		}
-
-		Object[][] rowData = new Object[RestrictModifier.RESTRICT_TO_ROWS < rowCount ? RestrictModifier.RESTRICT_TO_ROWS : rowCount][columns
-				.getColumnCount()];
+		
+		Object[][] rowData =
+			new Object[RestrictModifier.RESTRICT_TO_ROWS < rowCount ? RestrictModifier.RESTRICT_TO_ROWS : rowCount][columns.getColumnCount()];
 		int row = 0;
 		while (columns.nextRow()) {
 			int column = 0;
 			for (String columnName : columns.getColumnNames()) {
 				Object value = columns.getValue(columnName);
 				if (value instanceof Float) {
-					value = new NicelyShowingFloat((Float) value);
+					value = new NicelyShowingFloat((Float)value);
 				}
 				rowData[row][column] = value;
 				column++;
 			}
 			row++;
 		}
-
+		
 		table = new MicroarrayTable(data);
-		DefaultTableModel tableModel = new DefaultTableModel(rowData, columnTitles) {
+		DefaultTableModel tableModel = new DefaultTableModel(rowData, columnTitles) {			
 			@Override
-			public boolean isCellEditable(int row, int column) {
+			public boolean isCellEditable(int row, int column){
 				return false;
-			}
+			}			
 		};
 		table.setModel(tableModel);
 		table.setColumnControlVisible(true);
 		JScrollPane tableScroller = new JScrollPane(table);
-		table.setBackground(java.awt.Color.white);
+        table.setBackground(java.awt.Color.white);
 		table.setHorizontalScrollEnabled(columns.getColumnCount() > COLUMNS_REQUIRES_SCROLLING);
-
+		
 		// Adds popupmenu listener
-		table.addMouseListener(new MouseAdapter() {
-
+		table.addMouseListener(new MouseAdapter(){
+			
 			@Override
-			public void mousePressed(MouseEvent e) {
-				maybeShowPopup(e);
-			}
-
+	        public void mousePressed(MouseEvent e) {
+	        	maybeShowPopup(e);        
+	        }
+	        
 			@Override
-			public void mouseReleased(MouseEvent e) {
-				maybeShowPopup(e);
-			}
-
-			private void maybeShowPopup(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					JPopupMenu popup = new SpreadsheetPopupMenu(table);
-					popup.show(e.getComponent(), e.getX(), e.getY());
-				}
-			}
+	        public void mouseReleased(MouseEvent e) {
+	        	maybeShowPopup(e);        
+	        }
+	            		    				
+		    private void maybeShowPopup(MouseEvent e) {
+		        if (e.isPopupTrigger()) {
+		        	JPopupMenu popup = new SpreadsheetPopupMenu(table);
+	                popup.show(e.getComponent(), e.getX(), e.getY());
+		        }
+		    }
 		});
-
+		
 		panel.add(new JLabel("Showing " + rowData.length + " rows of " + rowCount), BorderLayout.NORTH);
 		panel.add(tableScroller, BorderLayout.CENTER);
 		table.updateSelectionsFromApplication();
-
+	
 		table.sendEvents(true);
-		return panel;
+		return panel; 
 	}
 
+	
 	public static class NicelyShowingFloat implements Comparable {
 
 		private Float floatValue;
-
+		
 		public NicelyShowingFloat(Float floatValue) {
 			this.floatValue = floatValue;
 		}
-
+		
 		@Override
 		public boolean equals(Object obj) {
 			if (obj instanceof NicelyShowingFloat) {
-				return floatValue.equals(((NicelyShowingFloat) obj).floatValue);
+				return floatValue.equals(((NicelyShowingFloat)obj).floatValue);
 			}
 			throw new IllegalArgumentException("cannot compare to instance of " + obj.getClass().getSimpleName());
 		}
@@ -201,49 +193,45 @@ public class Spreadsheet extends Visualisation {
 		@Override
 		public int hashCode() {
 			return floatValue.hashCode();
-
+			
 		}
-
+		
 		@Override
 		public String toString() {
 			String s = floatValue.toString();
-
+			
 			if (s.endsWith(".0")) {
-				s = s.substring(0, s.length() - 2);
+				s = s.substring(0, s.length()-2);
 			}
-
+			
 			return s;
 		}
-
+		
 		public int compareTo(Object obj) {
 			if (obj instanceof NicelyShowingFloat) {
-				return floatValue.compareTo(((NicelyShowingFloat) obj).floatValue);
+				return floatValue.compareTo(((NicelyShowingFloat)obj).floatValue);
 			}
 			throw new IllegalArgumentException("cannot compare to instance of " + obj.getClass().getSimpleName());
 		}
-
+		
 	}
-
+	
 	@Override
 	public boolean canVisualise(DataBean bean) throws MicroarrayException {
 
 		if (bean.isContentTypeCompatitible("text/tab", "text/csv")) {
 			return true; // clearly tabular
-
+			
 		} else if (bean.isContentTypeCompatitible("application/cel")) {
-			return !bean.queryFeatures("/embedded-binary-content/").exists(); // might
-																				// have
-																				// embedded
-																				// binary
-																				// content
-
+			return !bean.queryFeatures("/embedded-binary-content/").exists(); // might have embedded binary content
+			
 		} else {
 			return false;
 		}
 	}
-
+	
 	@Override
-	public void removeVisualisation() {
+	public void removeVisualisation(){
 		application.removePropertyChangeListener(table);
 	}
 }
