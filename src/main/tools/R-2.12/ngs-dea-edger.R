7e0fec28c549b6cc227d672dfa997ba03f0d1370
gentile <devnull@localhost>
2011-05-06 16:08:49 +0300

    Updated to produce PDF file output

--- src/main/tools/R-2.12/ngs-dea-edger.R
+++ src/main/tools/R-2.12/ngs-dea-edger.R
@@ -2,10 +2,10 @@
 # INPUT data.tsv TYPE GENERIC
 # INPUT phenodata.tsv TYPE GENERIC
 # OUTPUT de-list.tsv
-# OUTPUT OPTIONAL ma-plot-raw-counts.png
-# OUTPUT OPTIONAL ma-plot-normalized-counts.png
-# OUTPUT OPTIONAL ma-plot-significant-counts.png
-# OUTPUT OPTIONAL mds-plot.png
+# OUTPUT OPTIONAL ma-plot-raw-counts.pdf
+# OUTPUT OPTIONAL ma-plot-normalized-counts.pdf
+# OUTPUT OPTIONAL ma-plot-significant-counts.pdf
+# OUTPUT OPTIONAL mds-plot.pdf
 # PARAMETER column: "Column describing groups" TYPE METACOLUMN_SEL DEFAULT group (Phenodata column describing the groups to test)
 # PARAMETER normalization: "Apply normalization" TYPE [yes, no] DEFAULT yes (If enabled, a normalization factor based on the trimmed mean of M-values \(TMM\) is performed to reduce the effect from sequencing biases.)
 # PARAMETER dispersion_estimate: "Dispersion estimate" TYPE [common, tagwise] DEFAULT tagwise (The dispersion of counts for any given sequence can either be estimated based on the actual counts in the sample data set or be moderated across a selection of sequences with similar count numbers. The latter option, which is set by default, typically yields higher sensitivity and specificity.)
@@ -64,14 +64,14 @@ if (normalization == "yes") {
 }
 
 # Produce MDS plot of normazied data
-bitmap(file="mds-plot.png", width=w/72, height=h/72)
+pdf(file="mds-plot.pdf", width=w/72, height=h/72)
 sample_colors <-  ifelse (dge_list$samples$group==group_levels[1], 1, 2)
 plotMDS.dge(dge_list, main="MDS Plot", xlim=c(-2,1), col=sample_colors)
 legend(x="topleft", legend = group_levels,col=c(1,2), pch=19)
 dev.off()
 
 # MA-plot comparison before and after normalization
-bitmap(file="ma-plot-raw-counts.png", width=w/72, height=h/72)
+pdf(file="ma-plot-raw-counts.pdf", width=w/72, height=h/72)
 maPlot(dge_list$counts[, 1], dge_list$counts[, 2], normalize = TRUE, pch = 19,
 		cex = 0.4, ylim = c(-8, 8))
 grid(col = "blue")
@@ -81,7 +81,7 @@ abline(h = log2(dge_list$samples$norm.factors[2]/dge_list$samples$norm.factors[1
 dev.off()
 
 if (normalization == "yes") {
-	bitmap(file="ma-plot-normalized-counts.png", width=w/72, height=h/72)
+	pdf(file="ma-plot-normalized-counts.pdf", width=w/72, height=h/72)
 	eff.libsize <- dge_list$samples$lib.size * dge_list$samples$norm.factors
 	maPlot(dge_list$counts[, 1]/eff.libsize[1], dge_list$counts[, 2]/eff.libsize[2],
 			normalize = FALSE, pch = 19, cex = 0.4, ylim = c(-8, 8))
@@ -111,7 +111,7 @@ if (dispersion_estimate == "common") {
 	significant_results <- results_table[results_table$FDR<cutoff,]
 	
 	# Make an MA-plot displaying the significant reads
-	bitmap(file="ma-plot-significant-counts.png", width=w/72, height=h/72)	
+	pdf(file="ma-plot-significant-counts.pdf", width=w/72, height=h/72)	
 	significant_indices <- rownames (significant_results)
 	plotSmear(dge_list, de.tags = significant_indices, main = "MA plot for significantly\ndifferentially expressed sequence tags")
 	abline(h = c(-2, 2), col = "dodgerblue", lwd = 2)
@@ -141,7 +141,7 @@ if (dispersion_estimate == "tagwise") {
 	significant_results <- results_table[results_table$FDR<cutoff,]
 	
 	# Make an MA-plot displaying the significant reads
-	bitmap(file="ma-plot-significant-counts.png", width=w/72, height=h/72)	
+	pdf(file="ma-plot-significant-counts.pdf", width=w/72, height=h/72)	
 	significant_indices <- rownames (significant_results)
 	plotSmear(dge_list, de.tags = significant_indices, main = "MA plot for significantly\ndifferentially expressed sequence tags")
 	abline(h = c(-2, 2), col = "dodgerblue", lwd = 2)
