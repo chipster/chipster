93be18b3ee4d14bc6119e34927fac2582e5dd001
hupponen <devnull@localhost>
2011-05-10 14:05:18 +0300

    Fixed urls being messed up inside sessions.

--- src/main/java/fi/csc/microarray/client/session/SessionSaver.java
+++ src/main/java/fi/csc/microarray/client/session/SessionSaver.java
@@ -6,8 +6,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map.Entry;
