3e1b43c5c1aed29f8f827d9dfd818664fce30d57
gentile <devnull@localhost>
2011-05-19 14:10:42 +0300

    changed output to png

--- src/main/modules/microarray/R-2.12/plot-boxplot.R
+++ src/main/modules/microarray/R-2.12/plot-boxplot.R
@@ -1,9 +1,8 @@
-# TOOL plot-boxplot.R: Boxplot (Creates a boxplot of normalized data. One box per chip is plotted.)
-# INPUT normalized.tsv: normalized.tsv TYPE GENE_EXPRS 
-# INPUT META phenodata.tsv: phenodata.tsv TYPE GENERIC 
-# OUTPUT boxplot.pdf: boxplot.pdf 
-# PARAMETER image.width: image.width TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
-# PARAMETER image.height: image.height TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
+# ANALYSIS Visualisation/"Boxplot" (Creates a boxplot of normalized data. One box per chip is plotted.)
+# INPUT GENE_EXPRS normalized.tsv, GENERIC phenodata.tsv 
+# OUTPUT boxplot.png
+# PARAMETER image.width INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
+# PARAMETER image.height INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
 
 
 # Boxplot
@@ -30,12 +29,12 @@ phenodata<-read.table("phenodata.tsv", header=T, sep="\t")
 
 # Plotting
 if(nrow(phenodata)==ncol(dat2)) {
-   pdf(file="boxplot.pdf", width=w/72, height=h/72)
+   bitmap(file="boxplot.png", width=w/72, height=h/72)
    par(mar=c(12,5,5,5))
    boxplot(as.data.frame(dat2), las=2, names=phenodata$description)
    dev.off()
 } else {
-   pdf(file="boxplot.pdf", width=w/72, height=h/72)
+   bitmap(file="boxplot.png", width=w/72, height=h/72)
    par(mar=c(12,5,5,5))
    boxplot(as.data.frame(dat2), las=2)
    dev.off()
