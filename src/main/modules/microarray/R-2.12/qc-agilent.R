88a4e5330c83045c41f58c0746bd3125e39fd68c
hupponen <devnull@localhost>
2011-05-25 14:41:45 +0300

    Pdf to png outputs.

--- src/main/modules/microarray/R-2.12/qc-agilent.R
+++ src/main/modules/microarray/R-2.12/qc-agilent.R
@@ -1,9 +1,10 @@
-# ANALYSIS "Quality control"/"Agilent 2-color" (Agilent quality control using boxplots, density plots, and MA plots. 
-# This tool should be run on normalized data.)
-# INPUT GENE_EXPRS normalized.tsv 
-# OUTPUT boxplot.png, densityplot.pdf, MA-plot.png
-# PARAMETER image.width INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
-# PARAMETER image.height INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
+# TOOL qc-agilent.R: "Agilent 2-color" (Agilent quality control using boxplots, density plots, and MA plots. This tool should be run on normalized data.)
+# INPUT normalized.tsv: normalized.tsv TYPE GENE_EXPRS 
+# OUTPUT boxplot.png: boxplot.png 
+# OUTPUT densityplot.pdf: densityplot.pdf 
+# OUTPUT MA-plot.png: MA-plot.png 
+# PARAMETER image.width: image.width TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
+# PARAMETER image.height: image.height TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
 
 
 # Quality control of Agilent chips
