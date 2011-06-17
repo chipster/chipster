e228b040a072f2d484e453baf4ed96e286eadd38
gentile <devnull@localhost>
2011-06-17 13:58:19 +0300

    modified to output bed

--- src/main/modules/ngs/R-2.12/ngs-find-peaks-macs-one.R
+++ src/main/modules/ngs/R-2.12/ngs-find-peaks-macs-one.R
@@ -1,6 +1,7 @@
 # TOOL ngs-find-peaks-macs-one.R: "Find peaks using MACS, treatment only" (This tool will search for statistically significantly enriched genomic regions in sequencing data from a ChIP-seq experiment. The analysis is performed on one or more treatment samples alone, without taking into account control control samples.)
 # INPUT treatment.bam: "Treatment data file" TYPE GENERIC 
 # OUTPUT positive-peaks.tsv: "True enriched peaks" 
+# OUTPUT positive-peaks.bed: "True enriched peaks in a format compatible with the Genome Browser"
 # OUTPUT OPTIONAL model-plot.png: "A plot of the fitted peak model" 
 # OUTPUT OPTIONAL negative-peaks.tsv: "The false enriched peaks" 
 # OUTPUT analysis-log.txt: "Summary of analysis settings and run" 
