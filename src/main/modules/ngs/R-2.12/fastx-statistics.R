6fe6bc8ee342201495de6b8a87c5a46c88af3b75
ekorpela <devnull@localhost>
2011-06-20 16:05:24 +0300

    Added quality score graph to fatx statistics tool.

--- src/main/modules/ngs/R-2.12/fastx-statistics.R
+++ src/main/modules/ngs/R-2.12/fastx-statistics.R
@@ -1,16 +1,26 @@
 # TOOL fastx-statistics.R: "FASTX quality statistics" (FASTX quality statistics.)
 # INPUT reads.fastq TYPE GENERIC 
 # OUTPUT quality-stats.tsv 
+# OUTPUT qualities.png
 
 
 # EK 17.6.2011
 
 # binary
-binary <- c(file.path(chipster.tools.path, "fastx", "bin", "fastx_quality_stats"))
+binary.stats <- c(file.path(chipster.tools.path, "fastx", "bin", "fastx_quality_stats"))
 
 # command
-command <- paste(binary, "-i reads.fastq -o quality-stats.tsv")
+command.stats <- paste(binary.stats, "-i reads.fastq -o quality-stats.tsv")
 
 # run
-system(command)
+system(command.stats)
+
+# binary
+binary.distribution <- c(file.path(chipster.tools.path, "fastx", "bin", "fastq_quality_boxplot_graph.sh"))
+
+# command
+command.distribution <- paste(binary.distribution, "-i quality-stats.tsv", "-o qualities.png")
+
+# run
+system(command.distribution)
 
