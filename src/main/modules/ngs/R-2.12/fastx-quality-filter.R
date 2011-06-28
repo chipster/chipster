e2f6aab5e099ca7bff8ea8c08caf3f1de586a72c
ekorpela <devnull@localhost>
2011-06-28 12:43:43 +0300

    Created quality filter tool.

---
+++ src/main/modules/ngs/R-2.12/fastx-quality-filter.R
@@ -0,0 +1,22 @@
+# TOOL fastx-quality-filter.R: "Filter out low quality reads" (Filters out reads which contain quality scores below the user-specified criteria.
+# This tool is based on the FASTQ Quality Filter tool of the FASTX package.)
+# INPUT reads.fastq TYPE GENERIC
+# OUTPUT quality-filtered.fastq
+# OUTPUT quality-filtered.log
+# PARAMETER quality: "Minimum quality score to keep" TYPE INTEGER FROM 1 TO 100 DEFAULT 5 (What is the minimum quality score to keep.)
+# PARAMETER percentage: "Minimum percent of bases that must have that quality" TYPE INTEGER FROM 1 TO 100 DEFAULT 90 (What is the minimum percentage of bases that must have the specified.)
+
+
+
+# EK 28.6.2011
+
+# binary
+binary <- c(file.path(chipster.tools.path, "fastx", "bin", "fastq_quality_filter"))
+
+# command
+command <- paste(binary, "-v", "-q", quality, "-p", percentage, "-i reads.fastq -o quality-filtered.fastq > quality-filtered.log")
+
+# run
+system(command)
+
+
