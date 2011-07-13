d5b39242ec54dcf1cc3bac9e5a81ffcc8f647bb4
gentile <devnull@localhost>
2011-07-13 16:18:42 +0300

    Added options to filter within and outside of a range of values

--- src/main/modules/ngs/R-2.12/ngs-filter-results-column.R
+++ src/main/modules/ngs/R-2.12/ngs-filter-results-column.R
@@ -3,7 +3,8 @@
 # OUTPUT filtered-NGS-results.tsv: filtered-NGS-results.tsv 
 # PARAMETER column: column TYPE COLUMN_SEL (Data column to filter by)
 # PARAMETER cutoff: cutoff TYPE DECIMAL FROM -10000000 TO 10000000 DEFAULT 1 (Cut-off for filtering)
-# PARAMETER smaller.or.larger: smaller.or.larger TYPE [equal-to: equal-to, smaller-than: smaller-than, larger-than: larger-than] DEFAULT smaller-than (Smaller, equal or larger than the cutoff is filtered.)
+# PARAMETER smaller.or.larger: smaller.or.larger TYPE [equal-to: equal-to, smaller-than: smaller-than, larger-than: larger-than, within: within, outside: outside] DEFAULT smaller-than (Smaller or larger than the cutoff is filtered.
+# Use the "within" or "outside" options to filter symmmetrically around two cut-offs, useful for example when searching for 2-fold up- and down-regulated genes.)
 
 # Filter NGS results on the basis of a continuous parameter in a column
 # MG 26.5.2010
@@ -16,14 +17,22 @@ dat <- read.table(file, header=T, sep="\t")
 f <- dat[,grep(column, colnames(dat))]
 
 # Filters the data
-if (smaller.or.larger == "equal-to") {
-	dat2 <- dat[which(f == cutoff),]
+if(smaller.or.larger=="equal-to") {
+	dat2<-dat[which(f==cutoff),]
 }
-if (smaller.or.larger == "smaller-than") {
-	dat2 <- dat[which(f <= cutoff),]
+if(smaller.or.larger=="smaller-than") {
+	dat2<-dat[which(f<=cutoff),]
 }
-if (smaller.or.larger == "larger-than") {
-	dat2 <- dat[which(f >= cutoff),]
+if(smaller.or.larger=="larger-than") {
+	dat2<-dat[which(f>=cutoff),]
+}
+if(smaller.or.larger=="outside") {
+	cutoff_2 <- -cutoff
+	dat2<-dat[which(f>=cutoff | f<=cutoff_2),]
+}
+if(smaller.or.larger=="within") {
+	cutoff_2 <- -cutoff
+	dat2<-dat[-which(f>=cutoff | f <=cutoff_2),]
 }
 
 # Writing the data to disk
