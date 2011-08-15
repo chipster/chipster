2e34b2399375137186262656140b0f479a1da94f
arimatti saren <arimatti.saren@gmail.com>
2011-08-15 18:15:11 +0300

    bamtobed, intersectbed, pairtopair

--- src/main/modules/ngs/R-2.12/bedtools-bamtobed.R
+++ src/main/modules/ngs/R-2.12/bedtools-bamtobed.R
@@ -1,6 +1,6 @@
 # TOOL bedtools-bamtobed.R: "BEDTools bamToBed" (Converts BAM alignments to BED6, BED12 or BEDPE format.)
 # INPUT file.a: "BAM file" TYPE GENERIC
-# OUTPUT bamtobed.bam
+# OUTPUT bamtobed.bed
 # PARAMETER output.type: "Output type" TYPE [BED6, BED12, BEDPE] DEFAULT "BED6" (Select the output type (BED6, BED12 or BEDPE\).)
 # PARAMETER OPTIONAL split: "Report split BAM alignments as separate BED entries" TYPE [yes,no] DEFAULT no (Report each portion of a split BAM (i.e., having an N CIGAR operation\) alignment as a distinct BED intervals.)
 # PARAMETER OPTIONAL ed: "Use BAM edit distance for BED score" TYPE [yes,no] DEFAULT no (Use BAM edit distance (NM tag\) for BED score. Default for BED is to use mapping quality. Default for BEDPE is to use the minimum of the two mapping qualities for the pair.)
@@ -10,7 +10,20 @@
 # binary
 binary <- c(file.path(chipster.tools.path, "bedtools", "bin", "bamToBed"))
 
+# options
+options <- paste("")
+if (output.type == "BEDPE") {options <- paste(optons, "-bedpe")}
+if (output.type == "BED12") {
+	options <- paste(options, "-bed12")
+	options <- paste(options, "-color", color)
+}
+if (split == "yes") {options <- paste(options, "-split")}
+if (ed == "yes") {options <- paste(options, "-ed")}
+if (cigar == 'yes') {options <- paste(options,"-cigar")}
+options <- paste(options, "-i", "file.a")
 
+# command
+command <- paste(binary, options, ">bamtobed.bed")
 
 # run
-system(binary)
+system(command)
--- src/main/modules/ngs/R-2.12/bedtools-intersectbed.R
+++ src/main/modules/ngs/R-2.12/bedtools-intersectbed.R
@@ -1,7 +1,7 @@
 # TOOL bedtools-intersectbed.R: "BEDTools intersectBed" (Report overlaps between two feature files.)
 # INPUT file.a: "Input file A" TYPE GENERIC
 # INPUT file.b: "Input file B" TYPE GENERIC
-# OUTPUT result.txt 
+# OUTPUT intersectbed.bed 
 # PARAMETER abam: "File A is BAM format" TYPE [yes, no] DEFAULT no (Select yes if file A is BAM format.)
 # PARAMETER OPTIONAL ubam: "Write uncompressed BAM output" TYPE [yes, no] DEFAULT no (Write uncompressed BAM output. Default is to write compressed BAM.)
 # PARAMETER OPTIONAL bed: "Write output as BED" TYPE [yes, no] DEFAULT no (When using BAM input, write output as BED. The default is to write output in BAM when using BAM input.)
@@ -21,24 +21,25 @@
 binary <- c(file.path(chipster.tools.path, "bedtools", "bin", "intersectBed"))
 
 # options
-if (ubam == "yes") {options <- paste("-ubam")}
-if (bed == "yes") {options <- paste("-bed")}
-if (wa == "yes") {options <- paste("-wa")}
-if (wb == "yes") {options <- paste("-wb")}
-if (wo == "yes") {options <- paste("-wo")}
-if (wao == "yes") {options <- paste("-wao")}
-if (u == "yes") {options <- paste("-u")}
-if (c == "yes") {options <- paste("-c")}
-if (v == "yes") {options <- paste("-v")}
-if (f == "yes") {options <- paste("-f", f)}
-if (r == "yes") {options <- paste("-r")}
-if (s == "yes") {options <- paste("-s")}
-if (split == "yes") {options <- paste("-split")}
-if (abam == "yes") {options <- paste("-abam file.a -b file.b")}
-if (abam == "no") {options <- paste("-a file.a -b file.b")}
+options <- paste("")
+if (ubam == "yes") {options <- paste(options, "-ubam")}
+if (bed == "yes") {options <- paste(options,"-bed")}
+if (wa == "yes") {options <- paste(options,"-wa")}
+if (wb == "yes") {options <- paste(options,"-wb")}
+if (wo == "yes") {options <- paste(options,"-wo")}
+if (wao == "yes") {options <- paste(options,"-wao")}
+if (u == "yes") {options <- paste(options, "-u")}
+if (c == "yes") {options <- paste(options, "-c")}
+if (v == "yes") {options <- paste(options, "-v")}
+if (f == "yes") {options <- paste(options, "-f", f)}
+if (r == "yes") {options <- paste(options, "-r")}
+if (s == "yes") {options <- paste(options, "-s")}
+if (split == "yes") {options <- paste(options, "-split")}
+if (abam == "yes") {options <- paste(options, "-abam file.a -b file.b")}
+if (abam == "no") {options <- paste(options, "-a file.a -b file.b")}
 
 # command
-command <- paste(binary, options, ">result.txt")
+command <- paste(binary, options, ">intersectbed.bed")
 
 # run
 system(command)
