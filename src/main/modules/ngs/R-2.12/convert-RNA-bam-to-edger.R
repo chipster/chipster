d8abd3146722800dd9a92e2f69c0fe003d399cf7
gentile <devnull@localhost>
2011-08-22 14:26:57 +0300

    removed unneccesary parameters

--- src/main/modules/ngs/R-2.12/convert-RNA-bam-to-edger.R
+++ src/main/modules/ngs/R-2.12/convert-RNA-bam-to-edger.R
@@ -4,14 +4,14 @@
 # INPUT file.b: "BED file" TYPE GENERIC
 # OUTPUT edgeR-input.tsv
 # PARAMETER OPTIONAL s: "Force strandedness" TYPE [yes, no] DEFAULT no (Force strandedness. That is, only include hits in A that overlap B on the same strand. By default, hits are included without respect to strand.)
-# PARAMETER OPTIONAL hist: "Report a histogram of coverage" TYPE [yes, no] DEFAULT no (Report a histogram of coverage for each feature in B as well as a summary histogram for all features in B. Output (tab delimited\) after each feature in B: 1\) depth, 2\) \# bases at depth, 3\) size of B, 4\) % of B at depth.)
-# PARAMETER OPTIONAL d: "Report the depth at each position" TYPE [yes, no] DEFAULT no (Report the depth at each position in each B feature. Positions reported are one based.  Each position and depth follow the complete B feature.)
-# PARAMETER OPTIONAL split: "Treat split BAM or BED12 entries as distinct BED intervals" TYPE [yes, no] DEFAULT no (Treat "split" BAM or BED12 entries as distinct BED intervals when computing coverage. For BAM files, this uses the CIGAR N and D operations to infer the blocks for computing coverage. For BED12 files, this uses the BlockCount, BlockStarts, and BlockEnds fields (i.e., columns 10,11,12\).)
 
 # MG 22.8.2011
 # modified from the coveragebed tool by AMS
 
 # Not needed for use with RNA-seq data
+# PARAMETER OPTIONAL hist: "Report a histogram of coverage" TYPE [yes, no] DEFAULT no (Report a histogram of coverage for each feature in B as well as a summary histogram for all features in B. Output (tab delimited\) after each feature in B: 1\) depth, 2\) \# bases at depth, 3\) size of B, 4\) % of B at depth.)
+# PARAMETER OPTIONAL d: "Report the depth at each position" TYPE [yes, no] DEFAULT no (Report the depth at each position in each B feature. Positions reported are one based.  Each position and depth follow the complete B feature.)
+# PARAMETER OPTIONAL split: "Treat split BAM or BED12 entries as distinct BED intervals" TYPE [yes, no] DEFAULT no (Treat "split" BAM or BED12 entries as distinct BED intervals when computing coverage. For BAM files, this uses the CIGAR N and D operations to infer the blocks for computing coverage. For BED12 files, this uses the BlockCount, BlockStarts, and BlockEnds fields (i.e., columns 10,11,12\).)
 # PARAMETER abam: "File A is BAM format" TYPE [yes, no] DEFAULT no (Select yes if file A is BAM format.)
 
 # setup parameters
@@ -23,9 +23,9 @@ binary <- c(file.path(chipster.tools.path, "bedtools", "bin", "coverageBed"))
 # optional options
 options <- paste("")
 if (s == "yes") {options <- paste(options, "-s")}
-if (hist == "yes") {options <- paste(options, "-hist")}
-if (d == "yes") {options <- paste(options, "-d")}
-if (split == "yes") {options <- paste(options, "-split")}
+# if (hist == "yes") {options <- paste(options, "-hist")}
+# if (d == "yes") {options <- paste(options, "-d")}
+# if (split == "yes") {options <- paste(options, "-split")}
 
 # input files
 if (abam == "yes") {options <- paste(options, "-abam file.a -b file.b")}
