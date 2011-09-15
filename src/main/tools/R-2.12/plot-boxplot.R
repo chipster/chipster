a28a09af9a40802d8216bedfe6210496cf8f1220
gentile <devnull@localhost>
2011-09-15 14:40:44 +0300

    fixed issue when no group info is available

--- src/main/tools/R-2.12/plot-boxplot.R
+++ src/main/tools/R-2.12/plot-boxplot.R
@@ -4,7 +4,6 @@
 # PARAMETER image.width INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
 # PARAMETER image.height INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
 
-
 # Boxplot
 # JTT 2.10.2007
 # MG, 15.9.2011
@@ -29,23 +28,37 @@ dat2<-dat[,grep("chip", names(dat))]
 # Loads phenodata
 phenodata<-read.table("phenodata.tsv", header=T, sep="\t")
 
+# Setup sample colors according to group
+if (length(levels(as.factor(phenodata$group))) > 0) {
+	sample_colors <- numeric(length(phenodata$group))
+	group_levels <- levels(as.factor(phenodata$group))
+	group_identity <- as.character(phenodata$group)
+	for (count_levels in 1:length(group_levels)) {
+		for (count_samples in 1:length(phenodata$group)) {
+			if(group_identity[count_samples]==group_levels[count_levels]) sample_colors[count_samples] <- 1+count_levels
+		}
+	}
+	level_colors <- levels(as.factor(sample_colors))
+}
+if (length(levels(as.factor(phenodata$group))) == 0) {
+	sample_colors <- rep(2,length(phenodata$group))
+}
+
 # Plotting
 if(nrow(phenodata)==ncol(dat2)) {
 	bitmap(file="boxplot.png", width=w/72, height=h/72)
 	par(mar=c(12,5,5,5))
-	boxplot(as.data.frame(dat2), las=2, names=phenodata$description, col=phenodata$group+2)
+	boxplot(as.data.frame(dat2), las=2, names=phenodata$description, col=sample_colors)
 	if (length(levels(as.factor(phenodata$group))) > 0) {
-		legend (x="topleft", legend=levels(as.factor(phenodata$group)), col=1+1:length(levels(as.factor(phenodata$group))), cex=0.5, pch=19)
+		legend (x="topleft", legend=group_levels, col=level_colors, cex=0.5, pch=19)
 	}
-		dev.off()
+	dev.off()
 } else {
 	bitmap(file="boxplot.png", width=w/72, height=h/72)
 	par(mar=c(12,5,5,5))
-	boxplot(as.data.frame(dat2), las=2, col=phenodata$group+2)
+	boxplot(as.data.frame(dat2), las=2, col=sample_colots)
 	if (length(levels(as.factor(phenodata$group))) > 0) {
-		legend (x="topleft", legend=levels(as.factor(phenodata$group)), col=1+1:length(levels(as.factor(phenodata$group))), cex=0.5, pch=19)
+		legend (x="topleft", legend=group_levels, col=level_colors, cex=0.5, pch=19)
 	}
 	dev.off()
 }
-
-
