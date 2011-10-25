3b622de59a002f0663231173e5b9798d6bd2f659
ekorpelainen <ekorpelainen@gmail.com>
2011-10-24 18:55:41 -0700

    Polished the description of FASTX renamer and added it to ngs-module.xml.

--- src/main/modules/ngs/R-2.12/fastx-renamer.R
+++ src/main/modules/ngs/R-2.12/fastx-renamer.R
@@ -1,7 +1,7 @@
-# TOOL fastx-renamer.R: "Rename reads" (Renames read identifiers in a FASTQ file. The original names can be replaced by a running number or by the sequence itself.) 
+# TOOL fastx-renamer.R: "Rename reads" (Renames read identifiers in a FASTQ file. The original names can be replaced by a running number or by the sequence itself. This tool is based on the Renamer tool of the FASTX package.) 
 # INPUT reads.fastq TYPE GENERIC 
 # OUTPUT renamed-reads.fastq
-# PARAMETER rename.type: "Rename read identifiers to" TYPE [COUNT: "Running number", SEQ: "Sequence itself"] DEFAULT COUNT (Should the reads be named with running number or the srequence itself.)
+# PARAMETER rename.type: "Rename read identifiers to" TYPE [COUNT: "Running number", SEQ: "Sequence itself"] DEFAULT COUNT (Should the reads be named with running number or the sequence itself.)
 
 
 # EK 24.10.2011
