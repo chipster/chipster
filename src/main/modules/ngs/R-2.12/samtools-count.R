aab785c6ca7f51c30a5f1dfb304b88d61fa0c8c1
ekorpelainen <ekorpelainen@gmail.com>
2011-10-27 14:22:51 -0700

    Renamed tools and polished descriptions.

--- src/main/modules/ngs/R-2.12/samtools-count.R
+++ src/main/modules/ngs/R-2.12/samtools-count.R
@@ -1,4 +1,4 @@
-# TOOL samtools-count.R: "Count alignments in BAM file" (Counts alignments in a BAM file, taking the mapping quality into account if needed. This tool is based on the SAMtools package.)
+# TOOL samtools-count.R: "Count alignments in BAM" (Counts alignments in a BAM file, taking the mapping quality into account if needed. This tool is based on the SAMtools package.)
 # INPUT alignment.bam TYPE GENERIC 
 # OUTPUT alignment-counts.txt 
 # PARAMETER OPTIONAL mapping.quality: "Minimum mapping quality required for counting" TYPE INTEGER FROM 0 TO 1000 DEFAULT 0 (Counts only alignments which have a mapping quality higher than this. Note that Bowtie doesn't calculate mapping quality, but just inserts 255 to the field 5 of BAM file if the read aligns, and 0 if it doesn't align.)
@@ -10,6 +10,9 @@
 samtools.binary <- c(file.path(chipster.tools.path, "samtools", "samtools"))
 
 system(paste(samtools.binary, "view -bc -q", mapping.quality, "alignment.bam > alignment-counts.txt"))
+
+
+
 
 
 
