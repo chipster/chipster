26b6981c5c965081dae8555f4a848139094996ec
ekorpelainen <ekorpelainen@gmail.com>
2011-10-31 09:45:23 -0700

    Added strandedness parameter to Tophats.

--- src/main/modules/ngs/R-2.12/tophat.R
+++ src/main/modules/ngs/R-2.12/tophat.R
@@ -29,7 +29,8 @@ path.bowtie.index <- c(file.path(path.bowtie, "indexes", genome))
 command.start <- paste("bash -c '", set.path, tophat.binary)
 
 # parameters
-command.parameters <- paste("-r", mate.inner.distance, "-a", min.anchor.length, "-m", splice.mismatches, "-i", min.intron.length, "-I", max.intron.length, "-F", min.isoform.fraction)
+command.parameters <- paste("-r", mate.inner.distance, "-a", min.anchor.length, "-m", splice.mismatches, "-i", min.intron.length, "-I", max.intron.length, "-F", min.isoform.fraction, "--library-type fr-unstranded")
+
 
 # command ending
 command.end <- paste(path.bowtie.index, "reads1.fq reads2.fq'")
