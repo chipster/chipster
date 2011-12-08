d45f7cce5d9670195842af4d8bcc629204f353cd
taavi hupponen <taavi.hupponen@gmail.com>
2011-12-08 14:58:20 +0200

    Improved config tool.

--- src/main/applications/wrapper/configure.sh
+++ src/main/applications/wrapper/configure.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
+#!/bin/bash                                                                                                     
+if [ "$#" -eq 0 ]
+then
+    java -jar shared/lib/chipster-config-tool.jar configure
+elif [ "$#" -eq 1 ]
+then
+    if [ "$1" = "auto" ]
+    then
+        java -jar shared/lib/chipster-config-tool.jar auto-configure
+    else
+        java -jar shared/lib/chipster-config-tool.jar simple-configure $1
+    fi
+else
+    echo "Usage: ./configure.sh <hostname or address> | auto"
+fi
 
-java -jar shared/lib/chipster-config-tool.jar configure
+
