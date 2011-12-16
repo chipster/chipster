8dcdca54449761ad87080f1bdc8afd800083beac
ekorpelainen <ekorpelainen@gmail.com>
2011-12-15 19:05:43 -0800

    Added more explanations to the header of the Calculate fold change tool.

--- src/main/modules/microarray/R-2.12/calculate-fold-change.R
+++ src/main/modules/microarray/R-2.12/calculate-fold-change.R
@@ -1,17 +1,15 @@
 # TOOL calculate-fold-change.R: "Calculate fold change" (Calculates a geometric or arithmetic average of gene expression for replicate chips and then
 # calculates a difference or ratio between the averages. The output fold change can be represented either in log2 or linear scale. Note that
-# the tool is only applicable if you have exactly two groups of samples.)
+# the tool is only applicable if you have exactly two groups of samples, and the reference group is determined by the order-parameter, not by a smaller group number.)
 # INPUT normalized.tsv: normalized.tsv TYPE GENE_EXPRS 
 # INPUT META phenodata.tsv: phenodata.tsv TYPE GENERIC 
 # OUTPUT fold-change.tsv: fold-change.tsv 
 # PARAMETER column: column TYPE METACOLUMN_SEL DEFAULT group (Phenodata column describing the groups to average.)
-# PARAMETER geometric TYPE [yes, no] DEFAULT yes (Should the geometric or arithmetic mean used in the calculation of
-# average expression for the sample groups?)
-# PARAMETER scale TYPE [log2, linear] DEFAULT log2 (What scale to use for expressing the results. Log2 yields a symmetric distribution around zero
-# with no change being equal to 0, up-regulation taking positive values and down-regulation negative values. Conversely, in linear scale 
+# PARAMETER geometric TYPE [yes, no] DEFAULT yes (Should the geometric or arithmetic mean be used in the calculation of average expression for the sample groups?)
+# PARAMETER scale TYPE [log2, linear] DEFAULT log2 (What scale to use for expressing the results. Log2 yields a symmetric distribution around zero with no change being equal to 0, up-regulation taking positive values and down-regulation negative values. Conversely, in linear scale 
 # up-regulation is represented by values greater than 1 and down-regulation values being between 0 and 1.)
-# PARAMETER order TYPE [normal, reversed] DEFAULT normal (Defines which samples to treat as reference group. In the normal mode the group belonging
-# of the fist sample is assumed to be the reference, whereas in the reversed mode the situation is reversed. Note that this is different to how
+# PARAMETER order TYPE [normal, reversed] DEFAULT normal (Defines which samples to treat as the reference group. In the normal mode the group belonging
+# of the first sample is assumed to be the reference, whereas in the reversed mode the situation is reversed. Note that this is different to how
 # FC values are calculated in the various statistical testing tool, where the reference group is defined by the lower group number or alphabetical order.)
 
 # Calculate fold changes between groups of samples
@@ -19,7 +17,7 @@
 # MG, 3.5.2011, added parameters for choosing aritmetic or geometric mean
 # and for choosing linear or log scale
 # OH, 30.11.2011, support for names as group categories
-# MG, 30.11.2011, added parameter do define refernce group
+# MG, 30.11.2011, added parameter do define reference group
 
 # Loads the normalized data
 file<-c("normalized.tsv")
