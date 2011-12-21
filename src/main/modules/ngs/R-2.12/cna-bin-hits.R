857acd3aa9ed3cf03f2b340cfdb73fcb8b69f8c0
Ilari Scheinin <ilari.scheinin@gmail.com>
2011-12-21 14:34:50 +0100

    First versions of CNA-seq tools.

---
+++ src/main/modules/ngs/R-2.12/cna-bin-hits.R
@@ -0,0 +1,76 @@
+# TOOL cna-bin-hits.R: "Count reads in fixed bins" (Divide the genome into non-overlapping bins of fixed size, calculate reads in each bin, and correct for GC content.)
+# INPUT alignment.bam: "Input BAM file" TYPE GENERIC
+# OUTPUT binned-hits.tsv: "Binned hits"
+# PARAMETER organism: "Organism" TYPE [human: human] DEFAULT human (Organism.)
+# PARAMETER genome.build: "Genome build" TYPE [GRCh37: GRCh37] DEFAULT GRCh37 (Genome build.)
+# PARAMETER bin.size: "Bin size" TYPE [1: "1 kbp", 5: "5 kbp", 10: "10 kbp", 15: "15 kbp", 30: "30 kbp", 100: "100 kbp"] DEFAULT 30 (Bin size.)
+# Ilari Scheinin <firstname.lastname@gmail.com>
+# 2011-12-21
+
+library(limma)
+bin.size <- as.integer(bin.size)
+binbp <- bin.size * 1000
+binstring <- paste(bin.size, 'kbp', sep='')
+
+# chipster.tools.path <- "/home/ischeini/.opt/samtools"
+system(paste(file.path(chipster.tools.path, 'samtools'), ' view -F 0x0404 -q 1 alignment.bam | cut -f3,4 | tr -d chr > hits.txt', sep=''))
+
+chromosomes <- c(1:22, 'X', 'Y', 'MT')
+genomes <- list(GRCh37=c(249250621, 243199373, 198022430, 191154276, 180915260, 171115067, 159138663, 146364022, 141213431, 135534747, 135006516, 133851895, 115169878, 107349540, 102531392, 90354753, 81195210, 78077248, 59128983, 63025520, 48129895, 51304566, 155270560, 59373566, 16571)) # Y not 59034049
+names(genomes[['GRCh37']]) <- chromosomes
+start <- end <- count <- integer()
+for (chromosome in chromosomes) {
+  chromosome.size <- genomes[['GRCh37']][chromosome]
+  chromosome.starts <- seq(from=1, to=chromosome.size, by=binbp)
+  chromosome.ends <- chromosome.starts + binbp - 1
+  chromosome.ends[length(chromosome.ends)] <- chromosome.size
+  chromosome.breaks <- c(chromosome.starts, chromosome.size)
+  start <- c(start, chromosome.starts)
+  end <- c(end, chromosome.ends)
+}
+bins <- data.frame(chromosome=rep(chromosomes, ceiling(genomes[['GRCh37']] / binbp)), start, end, count=0, stringsAsFactors=FALSE)
+f <- file('hits.txt')
+skip <- 0
+chunk <- 100000000
+while(1) {
+  hits <- as.data.frame(scan('hits.txt', what=list(chromosome=character(), pos=integer()), sep='\t', nmax=chunk, skip=skip, quiet=TRUE))
+  if (nrow(hits) == 0)
+    break
+  for (chromosome in unique(hits$chromosome)) {
+    if (!chromosome %in% chromosomes)
+      next
+    chromosome.size <- genomes[['GRCh37']][chromosome]
+    chromosome.starts <- seq(from=1, to=chromosome.size, by=binbp)
+    chromosome.breaks <- c(chromosome.starts, chromosome.size)
+    count <- hist(hits[hits$chromosome==chromosome, 'pos'], breaks=chromosome.breaks, plot=FALSE)$count
+    bins[bins$chromosome==chromosome, 'count'] <- bins[bins$chromosome==chromosome, 'count'] + count
+  }
+  skip <- skip + chunk
+  rm(hits, count); gc(FALSE)
+}
+close(f)
+unlink('hits.txt') 
+
+gc.correct <- function(x) {
+  x$count[x$count==0] <- NA # ?
+  means <- numeric(length=length(gc.intervals))
+  for (i in 1:length(gc.intervals))
+    means[i] <- mean(x[!is.na(gc$permil) & gc$permil==gc.intervals[i],'count'], na.rm=TRUE)
+  loess.means <- loessFit(means, gc.intervals)$fitted
+  correction <- median(loess.means, na.rm=TRUE) - loess.means
+  for (i in 1:length(gc.intervals))
+    x[!is.na(gc$permil) & gc$permil==gc.intervals[i],'corrected'] <- x[!is.na(gc$permil) & gc$permil==gc.intervals[i],'count'] + correction[i]
+  x$corrected <- round(x$corrected)
+  x$corrected - min(x$corrected, na.rm=TRUE) # to prevent negative values
+}
+
+# gc <- read.table(file.path(chipster.tools.path, '..', '..', '.MPScall', paste('gc.', bin.size, 'kbp.txt.gz', sep='')), header=TRUE, sep='\t', as.is=TRUE, colClasses=c('character', 'integer', 'integer', 'numeric'))
+gc <- read.table(file.path(chipster.tools.path, 'MPScall', paste('gc.', bin.size, 'kbp.txt.gz', sep='')), header=TRUE, sep='\t', as.is=TRUE, colClasses=c('character', 'integer', 'integer', 'numeric'))
+gc$permil <- as.integer(round(gc$gc*1000))
+gc.intervals <<- seq(from=range(gc$permil, finite=TRUE)[1], to=range(gc$permil, finite=TRUE)[2])
+bins$corrected <- gc.correct(bins)
+
+options(scipen=10)
+write.table(bins, 'binned-hits.tsv', quote=FALSE, sep='\t', na='')
+
+# EOF
---
+++ src/main/modules/ngs/R-2.12/cna-plot-profile.R
@@ -0,0 +1,64 @@
+# TOOL cna-plot-profile.R: "Plot copy number profile" (This tool plots a copy number profile from binned hits.)
+# INPUT binned-hits.tsv: "Input file with binned read counts" TYPE GENERIC
+# OUTPUT cna-profile.pdf: "Copy number profile"
+# PARAMETER normalization: "Normalization" TYPE [median: median, mode: mode, none: none] DEFAULT median (Normalization method.)
+# PARAMETER counts: "Counts" TYPE [count: "Original raw counts", corrected: "GC corrected counts"] DEFAULT corrected (Whether to use original raw read counts, or GC corrected ones.)
+# PARAMETER chromosomes: chromosomes TYPE STRING DEFAULT 0 (The numbers of the chromosomes to be plotted, separated by commas. 0 means all chromosomes. Ranges are also supported (e.g. 1,3,7-10\).)
+# PARAMETER resolution: resolution TYPE DECIMAL FROM 0 TO 1 DEFAULT 1 (Proportion of log-ratio data points to draw. Lower values lead to smaller file sizes and faster processing.)
+
+# Ilari Scheinin <firstname.lastname@gmail.com>
+# 2011-12-21
+
+source(file.path(chipster.tools.path, 'MPScall', 'CGHcallPlus-R-2.12.R'))
+
+if (counts == 'count) {
+  colClasses <- c('character', 'character', 'integer', 'integer', 'numeric', 'NULL')
+} else {
+  colClasses <- c('character', 'character', 'integer', 'integer', 'NULL', 'numeric')
+}
+
+dat <- read.table('binned-hits.tsv', header=TRUE, sep='\t', row.names=1, colClasses=colClasses)
+dat[,4] <- log2(dat[,4])
+dat <- data.frame(bin=1:nrow(dat), dat)
+
+dat$chromosome[dat$chromosome=='X'] <- '23'
+dat$chromosome[dat$chromosome=='Y'] <- '24'
+dat$chromosome[dat$chromosome=='MT'] <- '25'
+dat$chromosome <- as.integer(dat$chromosome)
+
+cgh.raw <- make_cghRaw(dat)
+cgh.pre <- preprocess(cgh.raw, nchrom=max(chromosomes(cgh.raw)))
+cgh.nor <- normalize(cgh.pre, method=normalization)
+
+sampleNames(cgh.nor) <- paste('CNA Profile With', nrow(dat), 'Bins')
+
+# parse chromosomes to be plotted
+chromosomes <- gsub('X', '23', chromosomes, ignore.case=TRUE)
+chromosomes <- gsub('Y', '24', chromosomes, ignore.case=TRUE)
+chromosomes <- gsub('MT', '25', chromosomes, ignore.case=TRUE)
+chromosomes <- gsub('[^0-9,-]', ',', chromosomes)
+items <- strsplit(chromosomes, ',')[[1]]
+chrs.to.plot <- integer()
+for (item in items) {
+  item <- item[item!='']
+  if (length(item)==0) next
+  range <- strsplit(item, '-')[[1]]
+  range <- range[range!='']
+  if (length(range)==0) next
+  chrs.to.plot <- c(chrs.to.plot, seq(range[1], range[length(range)]))
+}
+chrs.to.plot <- unique(chrs.to.plot)
+chrs.to.plot <- chrs.to.plot[chrs.to.plot %in% dat$chromosome]
+if (length(chrs.to.plot)==0)
+  chrs.to.plot <- 0
+
+# plot
+pdf(file='cna-profile.pdf', paper='a4r', width=0, height=0)
+if (0 %in% chrs.to.plot) {
+  plot(cgh.nor[,1], dotres=1/resolution)
+} else {
+  plot(cgh.nor[chromosomes(cgh.nor) %in% chrs.to.plot, 1], dotres=1/resolution)
+}
+dev.off()
+
+# EOF
