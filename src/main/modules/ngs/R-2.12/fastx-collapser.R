3a86950670b0f400b0aae7d1ae5133ae1a67a64e
ekorpelainen <ekorpelainen@gmail.com>
2012-01-11 11:45:13 -0800

    Made a tool "Remove duplicates from FASTQ".

---
+++ src/main/modules/ngs/R-2.12/fastx-collapser.R
@@ -0,0 +1,15 @@
+# TOOL fastx-collapser.R: "Remove duplicate reads from FASTQ files" (Collapses identical sequences in a FASTQ file into a single sequence. The sequences are renamed with sequence number and the multiplicity value. This tool is based on the FASTA/Q Collapser tool of the FASTX package.)
+# INPUT reads.fastq TYPE GENERIC
+# OUTPUT duplicates-removed.fastq 
+
+
+# EK 12.1.2012
+
+# binary
+binary <- c(file.path(chipster.tools.path, "fastx", "bin", "fastx_collapser"))
+
+# command
+command <- paste(binary, "-i reads.fastq -o duplicates-removed.fastq")
+
+# run
+system(command)
