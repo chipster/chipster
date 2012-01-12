1132a1c5fdc27feb5c75072c6b662edd1213b01a
ekorpelainen <ekorpelainen@gmail.com>
2012-01-11 19:29:25 -0800

    Polished header of "Sort and index BAM".

--- src/main/modules/ngs/R-2.12/samtools-sort-index-BAM.R
+++ src/main/modules/ngs/R-2.12/samtools-sort-index-BAM.R
@@ -1,4 +1,4 @@
-# TOOL samtools-sort-index-BAM.R: "Sort sort and index BAM" (Sorts BAM by chromosomal location and builds an index for it. This tool is based on the SAMtools package.)
+# TOOL samtools-sort-index-BAM.R: "Sort and index BAM" (Sorts BAM by chromosomal location and builds an index for it. This tool is based on the SAMtools package.)
 # INPUT alignment.bam: "BAM file to sort and index" TYPE GENERIC 
 # OUTPUT alignment-sorted.bam 
 # OUTPUT alignment-sorted.bam.bai 
