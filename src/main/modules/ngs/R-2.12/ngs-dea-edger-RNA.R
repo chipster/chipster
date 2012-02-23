d7c799090ba5983a2fccfd36968b50d46d5f2fe2
massimilianogentile1969 <massimilianogentile1969@gmail.com>
2012-02-23 15:21:32 +0200

    Added p-value plot modified ouput file names

--- src/main/modules/ngs/R-2.12/ngs-dea-edger-RNA.R
+++ src/main/modules/ngs/R-2.12/ngs-dea-edger-RNA.R
@@ -1,13 +1,14 @@
 # TOOL ngs-dea-edger-RNA.R: "Differential expression analysis using edgeR" (This tool will perform an analysis for differentially expressed sequences using the R implementation of the edge algorithm.)
 # INPUT data.tsv TYPE GENERIC
 # INPUT phenodata.tsv TYPE GENERIC
-# OUTPUT OPTIONAL de-list.tsv
-# OUTPUT OPTIONAL de-list.bed
-# OUTPUT OPTIONAL ma-plot-raw-counts.pdf
-# OUTPUT OPTIONAL ma-plot-normalized-counts.pdf
-# OUTPUT OPTIONAL ma-plot-significant-counts.pdf
-# OUTPUT OPTIONAL mds-plot.pdf
-# OUTPUT OPTIONAL edgeR-log.txt
+# OUTPUT OPTIONAL de-list-edger.tsv
+# OUTPUT OPTIONAL de-list-edger.bed
+# OUTPUT OPTIONAL ma-plot-raw-edger.pdf
+# OUTPUT OPTIONAL ma-plot-normalized-edger.pdf
+# OUTPUT OPTIONAL ma-plot-significant-edger.pdf
+# OUTPUT OPTIONAL mds-plot-edger.pdf
+# OUTPUT OPTIONAL edger-log.txt
+# OUTPUT OPTIONAL p-value-plot-edger.pdf
 # PARAMETER column: "Column describing groups" TYPE METACOLUMN_SEL DEFAULT group (Phenodata column describing the groups to test)
 # PARAMETER normalization: "Apply normalization" TYPE [yes, no] DEFAULT yes (If enabled, a normalization factor based on the trimmed mean of M-values \(TMM\) is performed to reduce the effect from sequencing biases.)
 # PARAMETER dispersion_method: "Dispersion method" TYPE [common, tagwise] DEFAULT tagwise (The dispersion of counts for any given sequence can either be estimated based on the actual counts in the sample data set or be moderated across a selection of sequences with similar count numbers. The latter option, which is set by default, typically yields higher sensitivity and specificity. Note that when no biological replicates are available common dispersion is used regardless of the setting.)
@@ -29,6 +30,8 @@
 # phenodata file                                           #
 # updated MG, 30.01.2012 to allow analysis without         #
 # biological replicates                                    #
+# updated MG, 22.02.2012, prettified plots, added p-value  #
+# distribution plot                                        #
 #                                                          #
 ############################################################
 
@@ -85,7 +88,7 @@ if (normalization == "yes") {
 # Produce MDS plot of normazied data
 # NOTE: only possible when there are more than 2 samples in total
 if (number_samples > 2) {
-	pdf(file="mds-plot.pdf", width=w/72, height=h/72)
+	pdf(file="mds-plot-edger.pdf", width=w/72, height=h/72)
 	sample_colors <-  ifelse (dge_list$samples$group==group_levels[1], 1, 2)
 	plotMDS.dge(dge_list, main="MDS Plot", col=sample_colors)
 	legend(x="topleft", legend = group_levels,col=c(1,2), pch=19)
@@ -93,22 +96,27 @@ if (number_samples > 2) {
 }
 
 # MA-plot comparison before and after normalization
-pdf(file="ma-plot-raw-counts.pdf", width=w/72, height=h/72)
+pdf(file="ma-plot-raw-edger.pdf", width=w/72, height=h/72)
 maPlot(dge_list$counts[, 1], dge_list$counts[, 2], normalize = FALSE, pch = 19,
 		cex = 0.4, ylim = c(-8, 8))
 grid(col = "blue")
 title("Raw counts")
 abline(h = log2(dge_list$samples$norm.factors[2]/dge_list$samples$norm.factors[1]),
-		col = "red", lwd = 4)
+		col = "red", lwd = 2)
+abline(h = 0, col = "darkgreen", lwd = 1)
+legend (x="topleft", legend=c("not epressed in one condition","expressed in both conditions"), col=c("orange","black"),
+		cex=1, pch=19)
 dev.off()
 
 if (normalization == "yes") {
-	pdf(file="ma-plot-normalized-counts.pdf", width=w/72, height=h/72)
+	pdf(file="ma-plot-normalized-edger.pdf", width=w/72, height=h/72)
 	eff.libsize <- dge_list$samples$lib.size * dge_list$samples$norm.factors
 	maPlot(dge_list$counts[, 1]/eff.libsize[1], dge_list$counts[, 2]/eff.libsize[2],
 			normalize = TRUE, pch = 19, cex = 0.4, ylim = c(-8, 8))
 	grid(col = "blue")
 	title("Normalized counts")
+	legend (x="topleft", legend=c("not epressed in one condition","expressed in both conditions"), col=c("orange","black"),
+			cex=1, pch=19)
 	dev.off()
 }
 
@@ -135,10 +143,12 @@ if (dispersion_method == "common") {
 	significant_results <- results_table[results_table$FDR<cutoff,]
 	
 	# Make an MA-plot displaying the significant reads
-	pdf(file="ma-plot-significant-counts.pdf", width=w/72, height=h/72)	
+	pdf(file="ma-plot-significant-edger.pdf", width=w/72, height=h/72)	
 	significant_indices <- rownames (significant_results)
 	plotSmear(dge_list, de.tags = significant_indices, main = "MA plot for significantly\ndifferentially expressed sequence tags")
-	abline(h = c(-2, 2), col = "dodgerblue", lwd = 2)
+	abline(h = c(-1, 0, 1), col = "dodgerblue", lwd = 2)
+	legend (x="topleft", legend=c("significant","not significant"), col=c("red","black"),
+			cex=1, pch=19)
 	dev.off()
 }
 
@@ -165,10 +175,12 @@ if (dispersion_method == "tagwise") {
 	significant_results <- results_table[results_table$FDR<cutoff,]
 	
 	# Make an MA-plot displaying the significant reads
-	pdf(file="ma-plot-significant-counts.pdf", width=w/72, height=h/72)	
+	pdf(file="ma-plot-significant-edger.pdf", width=w/72, height=h/72)	
 	significant_indices <- rownames (significant_results)
 	plotSmear(dge_list, de.tags = significant_indices, main = "MA plot for significantly\ndifferentially expressed sequence tags")
-	abline(h = c(-2, 2), col = "dodgerblue", lwd = 2)
+	abline(h = c(-1, 0, 1), col = "dodgerblue", lwd = 2)
+	legend (x="topleft", legend=c("significant features","not significant"), col=c("red","black"),
+			cex=1, pch=19)
 	dev.off()
 }
 
@@ -181,7 +193,7 @@ if (dim(significant_results)[1] > 0) {
 
 # Output the table
 if (dim(significant_results)[1] > 0) {
-	write.table(output_table, file="de-list.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+	write.table(output_table, file="de-list-edger.tsv", sep="\t", row.names=T, col.names=T, quote=F)
 }
 
 # Also output a bed graph file for visualization and region matching tools
@@ -190,12 +202,25 @@ if (dim(significant_results)[1] > 0) {
 	bed_output <- output_table [,c("chr","start","end")]
 	bed_output <- cbind(bed_output,empty_column)
 	bed_output <- cbind(bed_output, output_table[,"logFC"])
-	write.table(bed_output, file="de-list.bed", sep="\t", row.names=F, col.names=F, quote=F)
+	write.table(bed_output, file="de-list-edger.bed", sep="\t", row.names=F, col.names=F, quote=F)
 }
 
 # Output a message if no significant genes are found
 if (dim(significant_results)[1] == 0) {
-	cat("No statistically significantly expressed sequences were found. Try again with a less stringent p-value cut-off or multiple testing correction method.", file="edgeR-log.txt")
+	cat("No statistically significantly expressed sequences were found. Try again with a less stringent p-value cut-off or multiple testing correction method.", file="edger-log.txt")
 }
+
+# Make histogram of p-values with overlaid significance cutoff and uniform distribution
+pdf (file="p-value-plot-edger.pdf")
+hist(results_table$PValue, breaks=100, col="blue",
+		border="slateblue", freq=FALSE,
+		main="P-value distribution", xlab="p-value", ylab="proportion (%)")
+hist(results_table$FDR, breaks=100, col="red",
+		border="slateblue", add=TRUE, freq=FALSE)
+abline(h=1, lwd=2, lty=2, col="black")
+abline(v=p_value_threshold, lwd=2, lty=2, col="green")
+legend (x="topright", legend=c("p-values","adjusted p-values", "uniform distribution", "significance cutoff"), col=c("blue","red","black","green"),
+		cex=1, pch=15)
+dev.off()
 
 # EOF
