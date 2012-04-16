d476ff9aae74ce18f7b9d84bbb5acef886272396
aleksi kallio <aleksi.kallio@helsinki.fi>
2012-04-16 15:44:26 +0300

    work with unit testing

--- src/test/java/fi/csc/microarray/databeans/DataManagerTest.java
+++ src/test/java/fi/csc/microarray/databeans/DataManagerTest.java
@@ -7,8 +7,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import fi.csc.microarray.config.DirectoryLayout;
+import fi.csc.microarray.client.Session;
+import fi.csc.microarray.client.operation.Operation;
+import fi.csc.microarray.client.operation.OperationDefinition;
+import fi.csc.microarray.client.operation.OperationRecord;
+import fi.csc.microarray.client.operation.ToolCategory;
 import fi.csc.microarray.config.ConfigurationLoader.IllegalConfigurationException;
+import fi.csc.microarray.config.DirectoryLayout;
 import fi.csc.microarray.module.ModuleManager;
 
 public class DataManagerTest {
@@ -19,7 +24,9 @@ public class DataManagerTest {
 	public void init() throws IOException, IllegalConfigurationException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		DirectoryLayout.initialiseSimpleLayout().getConfiguration();			
 		this.manager = new DataManager();
-		new ModuleManager("fi.csc.microarray.module.chipster.MicroarrayModule").plugAll(this.manager, null);
+		ModuleManager moduleManager = new ModuleManager("fi.csc.microarray.module.chipster.MicroarrayModule");
+		moduleManager.plugAll(this.manager, null);
+		Session.getSession().setModuleManager(moduleManager);
 	}
 	
 	@Test(groups = {"unit"} )
@@ -27,5 +34,31 @@ public class DataManagerTest {
 		File file = File.createTempFile("test", ".png");
 		Assert.assertEquals(manager.guessContentType(file).getType(), "image/png");
 	}
+
 	
+	@Test(groups = {"unit"} )
+	public void testRemoteSessions() throws Exception {
+		
+		// populate with crap
+		File content = File.createTempFile("content", ".tsv");
+		DataBean data = manager.createDataBean("test-content", content);
+		OperationRecord operationRecord = new OperationRecord(new Operation(new OperationDefinition("", "", new ToolCategory(""), "", false), new DataBean[] { data } ));
+		data.setOperationRecord(operationRecord);
+		operationRecord.setModule("");
+		manager.getRootFolder().addChild(data);
+		
+		// save
+		File session = File.createTempFile("test-remote-session", ".zip");
+		manager.saveLightweightSession(session);
+
+		// clear
+		manager.deleteAllDataItems();
+
+		// load
+		manager.loadSession(session, true);
+		
+		// clean up
+		manager.deleteAllDataItems();
+	}
+
 }
