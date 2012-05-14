9db3abe024f93f2fe5ec44c3dd194dde72c95b66
ekorpelainen <ekorpelainen@gmail.com>
2012-05-14 13:59:01 +0300

    Samtools consensus typo fixed.

--- src/main/modules/ngs/R-2.12/samtools-consensus.R
+++ src/main/modules/ngs/R-2.12/samtools-consensus.R
@@ -1,4 +1,4 @@
-# TOOL samtools-consensus.R: "Create consensus sequence from BAM" (Create consensus sequence from a BAM file. Please note that in addition to BAM file you have to provide an index file for it. You can create the index file using the tool Index BAM. You also need to provide the refeence sequence in fasta format. This tool is based on the SAMtools package.)
+# TOOL samtools-consensus.R: "Create consensus sequence from BAM" (Create consensus sequence from a BAM file. Please note that in addition to BAM file you have to provide an index file for it. You can create the index file using the tool Index BAM. You also need to provide the reference sequence in FASTA format. This tool is based on the SAMtools package.)
 # INPUT aln.bam: "BAM file to us as input" TYPE GENERIC
 # INPUT aln.bam.bai: "BAM index file to us as input" TYPE GENERIC
 # INPUT ref.fa: "Reference genome in FASTA format" TYPE GENERIC
