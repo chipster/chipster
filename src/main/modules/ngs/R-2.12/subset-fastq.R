08aa48266d2431c41285598554403f82e6d82635
ekorpelainen <ekorpelainen@gmail.com>
2012-05-15 16:57:53 +0300

    Added unzipping to Subset FASTQ.

--- src/main/modules/ngs/R-2.12/subset-fastq.R
+++ src/main/modules/ngs/R-2.12/subset-fastq.R
@@ -1,8 +1,14 @@
-# TOOL subset-fastq.R: "Make a subset of FASTQ" (Returns a subset of N reads from a FASTQ file)
+# TOOL subset-fastq.R: "Make a subset of FASTQ" (Returns a subset of N reads from a FASTQ file. If the FASTQ file is zipped, the tool will unzip it automatically.)
 # INPUT reads.fastq: "FASTQ file" TYPE GENERIC
 # OUTPUT subset.fastq 
 # PARAMETER n.seq: "Size of subset" TYPE INTEGER DEFAULT 100000 (Number of sequences to return from the start of the FASTQ file.)
 
+# AM 14.5.2012
+# EK 15.5.2012 added unzipping
+
+# check out if the file is compressed and if so unzip it
+source(file.path(chipster.common.path, "zip-utils.R"))
+unzipIfGZipFile("reads.fastq")
 
 # binary
 binary <- c("head")
