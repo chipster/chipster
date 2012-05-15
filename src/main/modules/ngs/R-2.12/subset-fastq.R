5a5c7fb4c1b01061921a6d9fc96d9c3b390a592b
ekorpelainen <ekorpelainen@gmail.com>
2012-05-15 17:05:03 +0300

    Text change.

--- src/main/modules/ngs/R-2.12/subset-fastq.R
+++ src/main/modules/ngs/R-2.12/subset-fastq.R
@@ -1,7 +1,7 @@
 # TOOL subset-fastq.R: "Make a subset of FASTQ" (Returns a subset of N reads from a FASTQ file. If the FASTQ file is zipped, the tool will unzip it automatically.)
 # INPUT reads.fastq: "FASTQ file" TYPE GENERIC
 # OUTPUT subset.fastq 
-# PARAMETER n.seq: "Size of subset" TYPE INTEGER DEFAULT 100000 (Number of sequences to return from the start of the FASTQ file.)
+# PARAMETER n.seq: "Size of subset" TYPE INTEGER DEFAULT 100000 (Number of reads to return from the start of the FASTQ file.)
 
 # AM 14.5.2012
 # EK 15.5.2012 added unzipping
