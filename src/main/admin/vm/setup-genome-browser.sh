53b1f8fdd830f8da5ae6d5f80d4447b24def4843
klemela <devnull@localhost>
2012-05-22 12:39:44 +0300

    tabix setup

--- src/main/admin/vm/setup-genome-browser.sh
+++ src/main/admin/vm/setup-genome-browser.sh
@@ -9,6 +9,8 @@
 # Species and releases are listed in the end of this script.
 
 
+
+##########################################################################################
 # download and extract .gz file from url unless the extracted file exists already
 # second parameter has to match file name in url (excluding .gz ending)
 
@@ -92,9 +94,32 @@ download_and_rename () # parameters 1:url 2:new-name
 	fi
 }
 
-contents_append_all_chr () # parameters 1:type 2:file-name
+
+process_gtf () # parameters 1:url
 {
-	contents_append "$1" "*" "$2"
+	FILE=$(basename $1 .gz)
+	FILE_BODY=$(basename $1 .gtf.gz)
+
+	if [ ! -e "$FILE_BODY-tabix.gtf.gz" ] # if doesn't exist
+	then	
+		download_and_rename "$1" # no rename needed
+
+		#generate list of chromosomes of genes
+		cat $FILE | cut -f 1,9 --output-delimiter=';' | cut -d ';' -f 1,5 | uniq | sed -e 's/; gene_name "/  /' | sed -e 's/\"//' > $FILE_BODY-gene.tsv
+		contents_append "Gene name" "*" "$FILE_BODY-gene.tsv"
+
+		#example in tabix manual:
+		(grep ^"#" $FILE; grep -v ^"#" $FILE | sort -k1,1 -k4,4n) | bgzip > $FILE_BODY-tabix.gtf.gz;
+		rm $FILE
+		contents_append "Transcript" "*" "$FILE_BODY-tabix.gtf.gz"
+
+		#generate index
+		tabix -p gff $FILE_BODY-tabix.gtf.gz; 
+		contents_append "Transcript index" "*" "$FILE_BODY-tabix.gtf.gz.tbi"	
+	
+	else 
+			echo "   Existing files $FILE_BODY.gtf* skipped"
+	fi
 }
 
 # process file from specified url and optionally rename the file
@@ -104,39 +129,13 @@ ensembl_mysql () # parameters 1:url 2:new-name 3:gene-name-database
 
 	# These files are just downloaded
 	download_and_rename "$1karyotype.txt.gz" "$2cytoband.txt" 
-	contents_append_all_chr "Cytoband" "$2cytoband.txt"
+	contents_append "Cytoband" "*" "$2cytoband.txt"
 
 	download_and_rename "$1seq_region.txt.gz" "$2seq_region.txt"
-	contents_append_all_chr "Cytoband seq_region" "$2seq_region.txt"
+	contents_append "Cytoband seq_region" "*" "$2seq_region.txt"
 
 	download_and_rename "$1coord_system.txt.gz" "$2coord_system.txt"
-	contents_append_all_chr "Cytoband coord_system" "$2coord_system.txt"
-
-	download_and_rename "$1gene.txt.gz" "$2gene.txt"
-	contents_append_all_chr "Gene" "$2gene.txt"
-
-
-	# xref.txt is needed to get the gene names, but it is too big for frequent read through
-	# use information from external_db.txt to filter out everything useless:
-
-	download_and_rename $1"external_db.txt.gz" $2"external_db.txt"
-	download_and_rename $1"xref.txt.gz" $2"xref.txt"
-
-	HGNC_ID=$(cat "$2external_db.txt" |grep "	$3	" |cut -f 1)
-	
-	# Filtering according to second tab-delimited column:
-	# starts with number (one or more), followed by <tab> and value $HGNC_ID and <tab>
-	# '^          [0-9]    \{1,\}                   <tab>           $HGNC_ID     <tab>'
-
-	cat $2xref.txt | grep "^[0-9]\{1,\}	$HGNC_ID	" > "$2xref2.txt"
-
-	rm "$2xref.txt"
-	
-	# external_db.txt is not needed anymore, but keep it avoid further downloads
-
-	mv "$2xref2.txt" "$2xref.txt"
-
-	contents_append_all_chr "Gene xref" "$2xref.txt"
+	contents_append "Cytoband coord_system" "*" "$2coord_system.txt"
 }
 
 
@@ -164,6 +163,8 @@ set -e # exit on errors (let contents2.txt backup above still fail, if this is t
 
 echo "CHIPSTER ANNOTATION CONTENTS FILE VERSION 2" > contents2.txt
 
+##########################################################################################
+
 
 # Species and releases
 
@@ -171,40 +172,37 @@ SPECIES="Human"
 VERSION="hg18 (NCBI36.54)"
 
 ensembl_mysql "ftp://ftp.ensembl.org/pub/release-54/mysql/homo_sapiens_core_54_36p/" "Homo_sapiens.NCBI36.54." "HGNC"
-download_and_rename "ftp://ftp.ensembl.org/pub/release-54/gtf/homo_sapiens/Homo_sapiens.NCBI36.54.gtf.gz" # no rename needed
-contents_append_all_chr "Transcript" "Homo_sapiens.NCBI36.54.gtf"
+process_gtf "ftp://ftp.ensembl.org/pub/release-54/gtf/homo_sapiens/Homo_sapiens.NCBI36.54.gtf.gz"
 download_chrs "Reference sequence" "ftp://ftp.ensembl.org/pub/release-54/fasta/homo_sapiens/dna/Homo_sapiens.NCBI36.54.dna.chromosome." ".fa.gz" "22" "X" "Y" 
+
+exit 0 
 
 SPECIES="Human"
 VERSION="hg19 (GRCh37.66)"
 
 ensembl_mysql "ftp://ftp.ensembl.org/pub/release-66/mysql/homo_sapiens_core_66_37/" "Homo_sapiens.GRCh37.66." "HGNC"
-download_and_rename "ftp://ftp.ensembl.org/pub/release-66/gtf/homo_sapiens/Homo_sapiens.GRCh37.66.gtf.gz" # no rename needed
-contents_append_all_chr "Transcript" "Homo_sapiens.GRCh37.66.gtf"
+process_gtf "ftp://ftp.ensembl.org/pub/release-66/gtf/homo_sapiens/Homo_sapiens.GRCh37.66.gtf.gz"
 download_chrs "Reference sequence" "ftp://ftp.ensembl.org/pub/release-66/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.66.dna.chromosome." ".fa.gz" "22" "X" "Y" 
 
 SPECIES="Mouse"
 VERSION="mm9 (NCBIM37.66)"
 
 ensembl_mysql "ftp://ftp.ensembl.org/pub/release-66/mysql/mus_musculus_core_66_37/" "Mus_musculus.NCBIM37.66." "MGI"
-download_and_rename "ftp://ftp.ensembl.org/pub/release-66/gtf/mus_musculus/Mus_musculus.NCBIM37.66.gtf.gz" # no rename needed
-contents_append_all_chr "Transcript" "Mus_musculus.NCBIM37.66.gtf"
+process_gtf "ftp://ftp.ensembl.org/pub/release-66/gtf/mus_musculus/Mus_musculus.NCBIM37.66.gtf.gz"
 download_chrs "Reference sequence" "ftp://ftp.ensembl.org/pub/release-66/fasta/mus_musculus/dna/Mus_musculus.NCBIM37.66.dna.chromosome." ".fa.gz" "19" "X" "Y" 
 
 SPECIES="Rat"
 VERSION="rn4 (RGSC3.4.66)"
 
 ensembl_mysql "ftp://ftp.ensembl.org/pub/release-66/mysql/rattus_norvegicus_core_66_34/" "Rattus_norvegicus.RGSC3.4.66." "RGD"
-download_and_rename "ftp://ftp.ensembl.org/pub/release-66/gtf/rattus_norvegicus/Rattus_norvegicus.RGSC3.4.66.gtf" # no rename needed
-contents_append_all_chr "Transcript" "Rattus_norvegicus.RGSC3.4.66.gtf"
+process_gtf "ftp://ftp.ensembl.org/pub/release-66/gtf/rattus_norvegicus/Rattus_norvegicus.RGSC3.4.66.gtf"
 download_chrs "Reference sequence" "ftp://ftp.ensembl.org/pub/release-66/fasta/rattus_norvegicus/dna/Rattus_norvegicus.RGSC3.4.66.dna.chromosome." ".fa.gz" "20" "X" 
 
 SPECIES="Arabidopsis lyrata"
 VERSION="(Araly1.13)"
 
 ensembl_mysql "ftp://ftp.ensemblgenomes.org/pub/plants/release-13/mysql/arabidopsis_lyrata_core_13_66_10/" "Arabidopsis_lyrata.Araly1.13." "EntrezGene"
-download_and_rename "ftp://ftp.ensemblgenomes.org/pub/plants/release-13/gtf/arabidopsis_lyrata/Arabidopsis_lyrata.Araly1.13.gtf" # no rename needed
-contents_append_all_chr "Transcript" "Arabidopsis_lyrata.Araly1.13.gtf"
+process_gtf "ftp://ftp.ensemblgenomes.org/pub/plants/release-13/gtf/arabidopsis_lyrata/Arabidopsis_lyrata.Araly1.13.gtf"
 download_chrs "Reference sequence" "ftp://ftp.ensemblgenomes.org/pub/plants/release-13/fasta/arabidopsis_lyrata/dna/Arabidopsis_lyrata.Araly1.13.dna.chromosome." ".fa.gz" "8"
 
 exit 0 
