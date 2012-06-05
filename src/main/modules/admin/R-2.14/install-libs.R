02395f15b624e84abd9b1bf561a9fab0d165d014
aleksi kallio <aleksi.kallio@helsinki.fi>
2012-06-05 16:37:55 +0300

    install-lib uses smip tool to install hdrcde package

---
+++ src/main/modules/admin/R-2.14/install-libs.R
@@ -0,0 +1,20 @@
+## Use install utility functions
+source("/opt/chipster/comp/modules/admin/R-2.14/smip.R")
+
+## Install packages, and dependencies, from CRAN
+install.packages(repos="http://ftp.sunet.se/pub/lang/CRAN", dependencies=TRUE, pkgs="locfit")
+
+## Install packages from Bioconductor
+source("http://bioconductor.org/biocLite.R")
+# Bionductor core packages
+biocLite()
+# Bioconductor specific packages
+biocLite("DESeq")
+biocLite("RPA")
+biocLite("IlluminaHumanMethylation450k.db") # annotation package, not needed if all annotation packages are installed
+
+biocLite(c("locfit", "ash", "ks")) # install hdrcde dependencies manually
+smart.install.packages(url.package="http://cran.r-project.org/src/contrib/00Archive/hdrcde/hdrcde_2.15.tar.gz") # install hdrcde manually because correct version is needed for R-2.14
+
+# EOF
+
