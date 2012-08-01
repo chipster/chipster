4a8947325ff6b6def5ff7ddb15165e9bea591e09
klemela <devnull@localhost>
2012-08-01 16:17:15 +0300

    check bed sorting

--- src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/message/AnnotationManager.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/gbrowser/message/AnnotationManager.java
@@ -166,7 +166,7 @@ public class AnnotationManager {
 	public void initialize() throws Exception {
 
 		// get annotation locations
-		//this.remoteAnnotationsRoot = getRemoteAnnotationsUrl();
+		this.remoteAnnotationsRoot = getRemoteAnnotationsUrl();
 		this.localAnnotationsRoot = DirectoryLayout.getInstance().getLocalAnnotationDir();
 
 		// try to parse the remote contents file
