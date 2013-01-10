08f86cd307c479db7a187590236df813d1204f43
klemela <devnull@localhost>
2013-01-10 16:14:30 +0200

    prepare new species for gb

--- src/main/admin/genome-preparation/setup-genome-browser-chicken.sh
+++ src/main/admin/genome-preparation/setup-genome-browser-chicken.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Script for downloading sheep genome and converting it into correct format for Chipster. 
-# Please verify the filesize (SEE THE LAST COMMENT), wget/server seems to cut off files sometimes.
+# Script for downloading chicken genome and converting it into correct format for Chipster. 
 
 # download assembly report
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/All/GCF_000002315.3.assembly.txt
 
 # keep only chromosome identifiers
-cat GCF_000002315.3.assembly.txt |grep assembled-molecule |grep -v "MT" |cut -d "	" -f 3 > chrs.txt
+cat GCF_000002315.3.assembly.txt |grep assembled-molecule |grep -v "MT" |grep -v "linkage group"|cut -d "	" -f 3 > chrs.txt
 
 # download sequence for each chromosome
 while read chr; do
@@ -21,11 +20,5 @@ cat *.fa > toplevel.fa
 # clean headers
 sed 's/>gi.*chromosome />/g' toplevel.fa | sed 's/, whole genome shotgun sequence//g' > toplevel-rename.fa
 
-# remove empty rows
-grep . toplevel-rename.fa > toplevel-clean.fa
+mv toplevel-rename.fa Gallus_gallus.4.0.fa
 
-# Result length should equal to "Total sequence length" minus "Total length" of "Unpaced scaffolds", in this case 2587507083
-BYTES=$(grep -v ">" toplevel-clean.fa | wc -c); LINES=$(grep -v ">" toplevel-clean.fa | wc -l);expr $BYTES - $LINES
-echo $BYTES
-
-mv toplevel-clean.fa Ovis_aries.Oar_v3.1.dna.toplevel.fa
