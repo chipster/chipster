3f68eb6392eea838136c91fe1ac275c04b44a71e
akallio <akallio@iki.fi>
2013-01-25 14:17:00 +0200

    made table initialisation more robust

--- src/main/java/fi/csc/microarray/filebroker/DerbyMetadataServer.java
+++ src/main/java/fi/csc/microarray/filebroker/DerbyMetadataServer.java
@@ -1,5 +1,7 @@
 package fi.csc.microarray.filebroker;
 
+import org.apache.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,77 +11,88 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class DerbyMetadataServer {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(DerbyMetadataServer.class);
 
 	private Connection connection = null;
 
-	private static String SESSION_DBTABLE = "chipster.sessions";
-	private static String FILE_DBTABLE = "chipster.files";
-	private static String BELONGS_TO_DBTABLE = "chipster.belongs_to";
-	private static String SPECIAL_USERS_DBTABLE = "chipster.special_users";
-	private static String[] DBTABLES = new String[] {
-		SESSION_DBTABLE,
-		FILE_DBTABLE,
-		BELONGS_TO_DBTABLE,
-		SPECIAL_USERS_DBTABLE
-	};
+	private static String SCHEMA = "chipster";
+	private static String SESSION_DBTABLE = "sessions";
+	private static String FILE_DBTABLE = "files";
+	private static String BELONGS_TO_DBTABLE = "belongs_to";
+	private static String SPECIAL_USERS_DBTABLE = "special_users";
 	
-	private static String[] SQL_CREATE_TABLES = new String[] {
-		"CREATE TABLE " + SESSION_DBTABLE + " (" +
-			"uuid VARCHAR(200) PRIMARY KEY,  " +
-			"name VARCHAR(200),  " +
-			"username VARCHAR(200))",
-		"CREATE TABLE " + FILE_DBTABLE + " (" +
-			"uuid VARCHAR(200) PRIMARY KEY,  " +
-			"size BIGINT,  " +
-			"created TIMESTAMP,  " +
-			"last_accessed TIMESTAMP)",
-		"CREATE TABLE " + BELONGS_TO_DBTABLE + " (" + 
-			"session_uuid VARCHAR(200) CONSTRAINT session_foreign_key REFERENCES " + SESSION_DBTABLE + "," +
-			"file_uuid VARCHAR(200) CONSTRAINT file_foreign_key REFERENCES " + FILE_DBTABLE + ")",
-		"CREATE TABLE " + SPECIAL_USERS_DBTABLE + " (" + 
-			"username VARCHAR(200) PRIMARY KEY)"
+	private static String[][] SQL_CREATE_TABLES = new String[][] {
+		{ 
+			"sessions",
+			"CREATE TABLE " + SCHEMA + ". " + SESSION_DBTABLE + " (" +
+					"uuid VARCHAR(200) PRIMARY KEY,  " +
+					"name VARCHAR(200),  " +
+				"username VARCHAR(200))" 
+		},
+		{
+			FILE_DBTABLE,
+			"CREATE TABLE " + SCHEMA + ". " + FILE_DBTABLE + " (" +
+					"uuid VARCHAR(200) PRIMARY KEY,  " +
+					"size BIGINT,  " +
+					"created TIMESTAMP,  " +
+					"last_accessed TIMESTAMP)"
+		},
+		{
+			BELONGS_TO_DBTABLE,
+			"CREATE TABLE " + SCHEMA + ". " + BELONGS_TO_DBTABLE + " (" + 
+					"session_uuid VARCHAR(200) CONSTRAINT session_foreign_key REFERENCES " + SCHEMA + ". " + SESSION_DBTABLE + "," +
+					"file_uuid VARCHAR(200) CONSTRAINT file_foreign_key REFERENCES " + SCHEMA + ". " + FILE_DBTABLE + ")"
+		},
+		{
+			SPECIAL_USERS_DBTABLE,
+			"CREATE TABLE " + SCHEMA + ". " + SPECIAL_USERS_DBTABLE + " (" + 
+					"username VARCHAR(200) PRIMARY KEY)"
+		}
 	};
 
-	private static String SQL_SELECT_SESSIONS_BY_USERNAME = "SELECT name, uuid FROM " + SESSION_DBTABLE + " WHERE username = ? OR username in (SELECT username FROM " + SPECIAL_USERS_DBTABLE + ")";
-	private static String SQL_INSERT_SESSION  = "INSERT INTO " + SESSION_DBTABLE + " (name, username, uuid) VALUES (?, ?, ?)";
-	private static String SQL_DELETE_SESSION  = "DELETE FROM " + SESSION_DBTABLE + " WHERE uuid = ?";
+	private static String SQL_SELECT_SESSIONS_BY_USERNAME = "SELECT name, uuid FROM " + SCHEMA + "." + SESSION_DBTABLE + " WHERE username = ? OR username in (SELECT username FROM " + SCHEMA + "." + SPECIAL_USERS_DBTABLE + ")";
+	private static String SQL_INSERT_SESSION  = "INSERT INTO " + SCHEMA + "." + SESSION_DBTABLE + " (name, username, uuid) VALUES (?, ?, ?)";
+	private static String SQL_DELETE_SESSION  = "DELETE FROM " + SCHEMA + "." + SESSION_DBTABLE + " WHERE uuid = ?";
 	
 	
 	public DerbyMetadataServer() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		
-		// initialise database
+		// initialise connection
 		System.setProperty("derby.system.home", "db-root");
 		Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance(); // allows multiple connections in one JVM, but not from multiple JVM's
-		String strUrl = "jdbc:derby:TestDatabase;create=true";
+		String strUrl = "jdbc:derby:MetadataDatabase;create=true";
 		connection = DriverManager.getConnection(strUrl);
 		
-		// initialise tables, if empty
-		if (!isInitialised()) {
-			initialise();
-		}
+		// initialise database, if needed
+		initialise();
 
-		System.out.println("Database started");
+		logger.info("Database started");
 	}
 	
-	private boolean isInitialised() throws SQLException {
-		for (String table : DBTABLES) {
-			ResultSet tables = connection.getMetaData().getTables(null, null, table.toUpperCase(), new String[] { "TABLE" });
+	private void initialise() throws SQLException {
+		
+		int tableCount = 0;
+		
+		for (int i = 0; i < SQL_CREATE_TABLES.length; i++) {
+			
+			String table = SQL_CREATE_TABLES[i][0];
+		
+			ResultSet tables = connection.getMetaData().getTables(null, SCHEMA.toUpperCase(), table.toUpperCase(), new String[] { "TABLE" });
 			if (!tables.next()) {
-				return false;
+				// table does not exist, create it
+				String createTable = SQL_CREATE_TABLES[i][1];
+				PreparedStatement ps = connection.prepareStatement(createTable);
+				ps.execute();
+				tableCount++;
 			}
 		}
-		return true;
-	}
-
-	private void initialise() throws SQLException {
-
-		int tableCount = 0;
-		for (String createTable : SQL_CREATE_TABLES) {
-			PreparedStatement ps = connection.prepareStatement(createTable);
-			ps.execute();
-			tableCount++;
+		
+		if (tableCount > 0) {
+			logger.info("Created " + tableCount + " missing tables to database");
 		}
-		System.out.println("Initialised database schema with " + tableCount + " tables");
 	}
 
 	@SuppressWarnings("unchecked")
