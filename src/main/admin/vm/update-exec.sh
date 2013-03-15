d84d0df3db278fbc6197f0b654b21f073753a836
Taavi Hupponen <taavi.hupponen@csc.fi>
2013-03-15 10:51:06 +0200

    Added qvalue installation to update-exec.sh.

--- src/main/admin/vm/update-exec.sh
+++ src/main/admin/vm/update-exec.sh
@@ -414,6 +414,18 @@ if [ $CURRENT_COMPARED -lt 0 ] ; then
 fi
 
 
+# 2.4.1, not released yet
+compare_to_current "2.4.1"
+if [ $CURRENT_COMPARED -lt 0 ] ; then 
+
+  echo "** Installing R library qvalue"
+  curl -s http://www.nic.funet.fi/pub/sci/molbio/chipster/dist/tools_extras/R/R-2.12.1-vmbin/library/qvalue-vmbin.tar.gz | tar -xz -C ${TOOLS_PATH}/R-2.12.1/lib64/R/library/
+
+fi
+
+
+  
+
 #####################################
 # VERSION SPECIFIC ENTRIES END HERE #
 #####################################
