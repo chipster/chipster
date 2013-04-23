743cff51922746c29a3323d209a5faa01311b8c1
ekorpela <ekorpela@EKORPELA-X220.windows.csc.fi>
2013-04-23 13:05:02 +0300

    Added .tsv ending to expression column names is "process prenormalized" tools so that sample renaming is possible in interactive visualizations.

--- src/main/modules/microarray/R-2.12/norm-prenormalized-affy.R
+++ src/main/modules/microarray/R-2.12/norm-prenormalized-affy.R
@@ -8,7 +8,8 @@
 
 # Process prenormalized affy
 # MG 6.4.2010
-# MK 18.02.2013
+# MK 18.2.2013
+# EK 23.4.2013 added .tsv ending to expression column names so that sample renaming is possible in interactive visualizations
 
 # Loads the libraries
 library(limma)
@@ -50,7 +51,7 @@ if (keep.flags=="yes") {
 # Mock normalization
 dat2<-normalizeBetweenArrays(dat$E, method="none")
 sample<-colnames(dat2)
-colnames(dat2)<-paste("chip.", colnames(dat2), sep="")
+colnames(dat2)<-paste("chip.", colnames(dat2), ".tsv", sep="")
 
 # Writes out a phenodata table
 # sample<-paste(sample, ".tsv", sep="")
