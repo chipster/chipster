39c10ec6b7299418f52e28f4e006d4871971b023
mkankain <kankainen.matti@gmail.com>
2013-04-29 12:03:14 +0300

    Bugs fixed related to annotate miRNA targets.

--- src/main/modules/ngs/R-2.12/ngs-annotate-miRNA-targets.R
+++ src/main/modules/ngs/R-2.12/ngs-annotate-miRNA-targets.R
@@ -4,9 +4,9 @@
 # PARAMETER species: species TYPE [human: human] DEFAULT human (The species for which the miRNA:s have been analyzed. Mouse and rat will be supported as soon as the RmiR package from Bioconductor supports this.)
 # PARAMETER database: database TYPE [miranda: miranda, mirbase: mirbase, mirtarget2: mirtarget2, pictar: pictar, tarbase: tarbase, targetscan: targetscan] DEFAULT mirtarget2 (The database from which to search for predicted target genes.)
 
-# miRNA hypergeometric test for GO
 # MG, 22.2.2010
 # IS, 13.10.2010, added rownames of miRNA-target-pairs to allow intersecting through Venn diagrams
+# MK, 26.04.2013, supports now files having IDs in ID-field and/or having alignment information in their ID-field
 
 # Load the required libraries
 library(RmiR.Hs.miRNA)
@@ -16,11 +16,35 @@ library(biomaRt)
 mirna_data <- read.table("normalized_mirna.tsv", sep="\t", header=T)
 
 # Extracts the identifiers
-mirna_id <- as.character(rownames(mirna_data))
 
+#if has ID, field try that
+if("id" %in% colnames(mirna_data)) {
+	mirna_id <- mirna_data$id
+} else {
+	mirna_id <- as.character(rownames(mirna_data))
+}
+	
 # Fetch the complete set of target genes for a specified database
 full_database <- dbReadTable(RmiR.Hs.miRNA_dbconn(), database)[, 1:2]
 target_genes <- full_database[full_database$mature_miRNA %in% mirna_id,]
+
+# If convert genomic BAM file has been used, IDs contain read information preveting direct matching. To correct this,
+# database IDs are first compared to user IDs (allowing partial matches), after which those user IDs having more than
+# one match are rejected as I cannot be known which of the many is the correct
+if(nrow(target_genes) == 0) {
+	mature_miRNA_mod 		<- unique(paste(full_database$mature_miRNA, "_", sep=""))
+	mirna_id_mat 	  		<- data.frame(counts=rep(0, length(mirna_id)), names=rep("", length(mirna_id)), stringsAsFactors = FALSE)
+	rownames(mirna_id_mat) 	<- mirna_id
+
+	for(i in 1:length(mature_miRNA_mod)) {
+		grep.positions <- grep(paste("^",mature_miRNA_mod[i], sep=""), mirna_id)
+		if(length(grep.positions) > 0) {
+			mirna_id_mat[grep.positions, 1] <- mirna_id_mat[grep.positions, 1] + 1
+			mirna_id_mat[grep.positions, 2] <- paste(mirna_id_mat[grep.positions, 2], mature_miRNA_mod[i], sep="")		
+		}
+	}
+	target_genes <- full_database[paste(full_database$mature_miRNA, "_", sep="") %in% mirna_id_mat[mirna_id_mat[,1] >= 1, 2],]
+}
 gene_id <- target_genes$gene_id
 mirna_id_2 <- target_genes$mature_miRNA
 
@@ -34,6 +58,11 @@ if (species=="mouse") {
 if (species=="rat") {
 	dataset <- "rnorvegicus_gene_ensembl"
 }
+
+if(length(gene_id) < 1) {
+	stop("CHIPSTER-NOTE: Your analysis was terminated, because no matches were found between your and RmiR database miRNA identifiers")
+}
+
 ensembl <- useMart("ensembl", dataset=dataset)
 annotated_genes <- getBM(mart=ensembl, attributes=c("entrezgene","hgnc_symbol","description"), filters="entrezgene", values=gene_id)
 
