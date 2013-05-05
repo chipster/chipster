cb5cb25381e9d0d16116f034b29da9d2a0c85ef9
ekorpela <ekorpela@EKORPELA-X220.windows.csc.fi>
2013-05-05 21:56:54 +0300

    edgeR glm, moved filtering and resizing before normalization

--- src/main/modules/ngs/R-2.15/edgeR-multivariate.R
+++ src/main/modules/ngs/R-2.15/edgeR-multivariate.R
@@ -11,7 +11,7 @@
 # PARAMETER OPTIONAL treat.main.effect3.as.factor: "Treat main effect 3 as factor" TYPE [no: no, yes: yes] DEFAULT yes (Should main.effect3 be treated as a factor)
 # PARAMETER OPTIONAL interactions: "Include interactions in the model" TYPE [main: "no", all: "yes"] DEFAULT main (Should interactions be included in the model in addition to the main effects.)
 # PARAMETER OPTIONAL normalization: "Apply TMM normalization" TYPE [yes, no] DEFAULT yes (Should normalization based on the trimmed mean of M-values \(TMM\) be performed to reduce the RNA composition effect.)
-# PARAMETER OPTIONAL filter: "Analyze only genes which have counts in at least this many samples" TYPE INTEGER FROM 0 TO 1000 DEFAULT 0 (Analyze only genes which have at least one count per million in at least this many samples)
+# PARAMETER OPTIONAL filter: "Analyze only genes which have counts in at least this many samples" TYPE INTEGER FROM 0 TO 1000 DEFAULT 0 (Analyze only genes which have at least 5 counts in at least this many samples)
 # PARAMETER OPTIONAL w: "Plot width" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted image)
 # PARAMETER OPTIONAL h: "Plot height" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted image)
 
@@ -38,16 +38,23 @@ phenodata <- read.table("phenodata.tsv", header=T, sep="\t")
 # Forms the DGElist object
 dge<-DGEList(counts=dat2)
 
+# filter out genes which have less than 5 counts in user-defined number of samples 
+if (filter > 0) {
+	keep <- rowSums(dge$counts>5) >= filter
+	dge <- dge[keep,]
+	dge$lib.size <- colSums(dge$counts)
+}
+
 # Calculate normalization factors
 if(normalization=="yes") {
    dge<-calcNormFactors(dge)
 }
-# filter out genes which have less than 1 cpm in user-defined number of samples 
-if (filter > 0) {
-	keep <- rowSums(cpm(dge)>1) >= filter
-	dge <- dge[keep,]
-	dge$lib.size <- colSums(dge$counts)
-}
+# For later use: filter out genes which have less than 1 cpm in user-defined number of samples 
+# if (filter > 0) {
+# 	keep <- rowSums(cpm(dge)>1) >= filter
+# 	dge <- dge[keep,]
+# 	dge$lib.size <- colSums(dge$counts)
+# }
 
 # Form a model matrix
 formula<-"~"
