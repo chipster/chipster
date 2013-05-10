8edc3b451efb42fc65831640c44ab3e1ab2a1a1e
klemela <petri.klemela@csc.fi>
2013-05-10 14:07:19 +0300

    Create a local copy for the pdf viewer.

--- src/main/java/fi/csc/microarray/client/visualisation/methods/PDFViewer.java
+++ src/main/java/fi/csc/microarray/client/visualisation/methods/PDFViewer.java
@@ -1,19 +1,16 @@
 package fi.csc.microarray.client.visualisation.methods;
 
-import java.io.InputStream;
+import java.io.File;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import org.icepdf.ri.common.SwingController;
 import org.icepdf.ri.common.SwingViewBuilder;
-import org.icepdf.ri.common.views.DocumentViewControllerImpl;
 
 import fi.csc.microarray.client.visualisation.Visualisation;
 import fi.csc.microarray.client.visualisation.VisualisationFrame;
 import fi.csc.microarray.databeans.DataBean;
-import fi.csc.microarray.databeans.DataBean.DataNotAvailableHandling;
-import fi.csc.microarray.databeans.DataManager.StorageMethod;
 import fi.csc.microarray.exception.MicroarrayException;
 
 public class PDFViewer extends Visualisation {
@@ -40,15 +37,22 @@ public class PDFViewer extends Visualisation {
 		// Use the factory to build a JPanel that is pre-configured
 		//with a complete, active Viewer UI.
 		JPanel viewerComponentPanel = factory.buildViewerPanel();
-
-		// Open a PDF document to view
-		InputStream in = data.getContentStream(DataNotAvailableHandling.NULL_ON_NA);
-		if (in != null) {
-			controller.openDocument(in, data.getName(), data.getContentLocation(StorageMethod.LOCAL_FILE_METHODS).getUrl().toString());
-
-			// Set view mode
-			controller.setPageViewMode(DocumentViewControllerImpl.ONE_COLUMN_VIEW, true);
-		}
+		
+//the pdf reader can't read file over http but says the file is corrupted
+//		
+//		// Open a PDF document to view
+//		InputStream in = data.getContentStream(DataNotAvailableHandling.NULL_ON_NA);
+//		if (in != null) {
+//			
+//			controller.openDocument(in, data.getName(), data.getContentLocation(StorageMethod.LOCAL_FILE_METHODS).getUrl().toString());
+//			
+//			// Set view mode
+//			controller.setPageViewMode(DocumentViewControllerImpl.ONE_COLUMN_VIEW, true);
+//		}
+		
+		//use local file for now
+		File file = application.getDataManager().getLocalFile(data);
+		controller.openDocument(file.getPath());
 		
 		return viewerComponentPanel;
 	}
