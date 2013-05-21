85e03ae7cc95330726b38d879bfba7d23f4d14da
Taavi Hupponen <taavi.hupponen@csc.fi>
2013-05-21 11:55:47 +0300

    Added basic announcement for client

--- src/main/java/fi/csc/microarray/client/ClientApplication.java
+++ src/main/java/fi/csc/microarray/client/ClientApplication.java
@@ -13,7 +13,10 @@ import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -68,6 +71,7 @@ import fi.csc.microarray.messaging.auth.ClientLoginListener;
 import fi.csc.microarray.module.Module;
 import fi.csc.microarray.module.ModuleManager;
 import fi.csc.microarray.util.Files;
+import fi.csc.microarray.util.IOUtils;
 
 
 /**
@@ -179,6 +183,8 @@ public abstract class ClientApplication {
     protected Configuration configuration;
 
 	private String initialisationWarnings = "";
+	
+	private String announcementText = null;
 
 	public ClientApplication() {
 		this(false, null);
@@ -199,6 +205,9 @@ public abstract class ClientApplication {
 
 		try {
 
+			// Fetch announcements
+			fetchAnnouncements();
+			
 			// Initialise modules
 			ModuleManager modules = new ModuleManager(requestedModule);
 			Session.getSession().setModuleManager(modules);
@@ -829,5 +838,33 @@ public abstract class ClientApplication {
 		return initialisationWarnings;
 	}
 
+	private void fetchAnnouncements() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				InputStream input = null;
+				try {
+					
+					URL url = new URL("http://chipster.csc.fi/announcements/client.txt");
+
+					URLConnection connection = url.openConnection();
+					connection.setUseCaches(false);
+					connection.setConnectTimeout(10*1000);
+					connection.setReadTimeout(10*1000);
+					input = connection.getInputStream();
+					announcementText = org.apache.commons.io.IOUtils.toString(input);
+				} catch (Exception e) {
+					// could fail for many reasons, not critical
+				} finally {
+					org.apache.commons.io.IOUtils.closeQuietly(input);
+				}
+			}
+		}).start();
+	}
+	
+	public String getAnnouncementText() {
+		return this.announcementText;
+	}
 	
 }
