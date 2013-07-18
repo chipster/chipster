ac3f248ad6048a8ad026046710ad75f65f069302
ekorpela <ekorpelainen@gmail.com>
2013-07-18 10:41:30 +0300

    DEXSeq changes.

--- src/main/modules/ngs/R-2.15/DEXseq.R
+++ src/main/modules/ngs/R-2.15/DEXseq.R
@@ -1,9 +1,11 @@
 # TOOL DEXSeq.R: "Differential exon expression using DEXSeq" (Infers differential exon usage from RNA-seq data using the Bioconductor package DEXSeq. Replicates are necessary for this tool to work properly. In order to prepare the input, run the tool \"Count aligned reads per exons for DEXSeq\" for your BAM files and combine the results to a count table using the tool \"Utilities - Define NGS experiment\". Please use the group column of the phenodata file to indicate your experimental groups.)
 # INPUT countfile.tsv: "Count table" TYPE GENERIC 
 # INPUT phenodata.tsv: "Phenodata" TYPE GENERIC
-# OUTPUT DEXSeq-result-table.tsv: DEXSeq-filtered-result-table.tsv
-# OUTPUT OPTIONAL DEXSeq-exons.pdf: DEXSeq-exons.pdf
-# OUTPUT OPTIONAL MAplot.pdf: MAplot.pdf
+# OUTPUT dexseq-all-genes.tsv: dexseq-all-genes.tsv
+# OUTPUT OPTIONAL dexseq-genes-with-significant-exons.tsv: dexseq-genes-with-significant-exons.tsv
+# OUTPUT OPTIONAL dexseq-exons.pdf: dexseq-exons.pdf
+# OUTPUT OPTIONAL dexseq-MAplot.pdf: dexseq-MAplot.pdf
+# OUTPUT OPTIONAL dexeq-dispersion-plot.pdf: dexseq-dispersion-plot.pdf
 # PARAMETER organism: "Organism" TYPE [Homo_sapiens.GRCh37.68.chr.DEXSeq.gtf: "Human (hg19.68)", Mus_musculus.GRCm38.68.chr.DEXSeq.gtf: "Mouse (mm10.68)", Rattus_norvegicus.RGSC3.4.68.chr.DEXSeq.gtf: "Rat (rn4.68)"] DEFAULT Homo_sapiens.GRCh37.68.chr.DEXSeq.gtf (Which organism is your data from.)
 # PARAMETER pvalue: "Threshold for adjusted p-value" TYPE DECIMAL FROM 0 TO 1 DEFAULT 0.05 (Threshold for BH adjusted p-values. If a gene has at least one exon below this p-value, all its exons will be included in the result list.)
 # PARAMETER OPTIONAL dispersion: "Common dispersion" TYPE DECIMAL FROM 0 TO 100 DEFAULT 0.1 (If dispersions can not be estimated, this common dispersion value is used for all exons. In this case no graphical output is generated.)
@@ -16,8 +18,6 @@
 ####pvalue<-0.05
 ####dispersion<-0.1
 ####
-# OUTPUT DEXSeq-full-result-table.tsv: DEXSeq-full-result-table.tsv
-# OUTPUT OPTIONAL dispersion-plot.pdf: dispersion-plot.pdf
 
 # Loads the library 
 library(DEXSeq)
@@ -81,13 +81,13 @@ res <- DEUresultTable(ecs)
 siggenes<-as.character(unique(res$geneID[res$padjust<pvalue]))
 res2<-res[as.character(res$geneID) %in% siggenes,]
 
-#write.table(res, "DEXSeq-full-result-table.tsv", col.names=TRUE, row.names=TRUE, sep="\t", quote=FALSE)
-write.table(res2, "DEXSeq-result-table.tsv", col.names=TRUE, row.names=TRUE, sep="\t", quote=FALSE)
+write.table(res, "dexseq-all-genes.tsv", col.names=TRUE, row.names=TRUE, sep="\t", quote=FALSE)
+write.table(res2, "dexseq-genes-with-significant-exons.tsv", col.names=TRUE, row.names=TRUE, sep="\t", quote=FALSE)
 
 # Visualization
 if(doplot & nrow(res2)>0) {
    genes<-unique(as.character(res[which(res$padjust<=pvalue),]$geneID))
-   pdf("DEXSeq-exons.pdf", width=297/25.4, height=210/25.4)
+   pdf("dexseq-exons.pdf", width=297/25.4, height=210/25.4)
    for(i in 1:length(genes)) {
       plottry<-try(plotDEXSeq(ecs, genes[i], displayTranscripts = FALSE, cex.axis = 1.2, cex = 1.3, lwd = 2, legend = TRUE))
       if(class(plottry)=="try-error") {
@@ -100,14 +100,14 @@ if(doplot & nrow(res2)>0) {
 }
 
 if(nrow(res)>0) {
-   pdf("MAplot.pdf", width=297/25.4, height=210/25.4)
+   pdf("dexseq-MAplot.pdf", width=297/25.4, height=210/25.4)
    plot(x=log2(res[,6]), y=res[,7], xlab="mean of normalized counts (log2)", ylab="log2 fold change", type="n")
    points(x=log2(res[res[,5]>pvalue,6]), y=res[res[,5]>pvalue,7], col="black", pch=16, cex=0.5)
    points(x=log2(res[res[,5]<=pvalue,6]), y=res[res[,5]<=pvalue,7], col="#CC0000", pch=16, cex=0.5)
    dev.off()
 }
 
-#pdf("dispersion-plot.pdf", width=297/25.4, height=210/25.4)
+#pdf("dexseq-dispersion-plot.pdf", width=297/25.4, height=210/25.4)
 #px = rowMeans(counts(ecs, normalized = TRUE))
 #sel<-px>0
 #px = px[sel]
