7af5f91a09ab4004f56e68a9178fab36a7192139
mkankain <kankainen.matti@gmail.com>
2013-08-21 17:30:05 +0300

    Delete-and-sbtract function extended to cover flagged-data

--- src/main/modules/microarray/R-2.12/delete-and-subtract-columns.R
+++ src/main/modules/microarray/R-2.12/delete-and-subtract-columns.R
@@ -19,32 +19,50 @@ file<-c("normalized.tsv")
 dat<-read.table(file, sep="\t", header=T, row.names=1)
 phenodata <- read.table('phenodata.tsv', header=TRUE, sep='\t', as.is=TRUE)
 
-# Identify replicates
+# identify matrices (chip, flag, segmented, ...) present in the data
+datnames <- colnames(dat)
+suffix <- sub('^chip\\.', '', datnames[grep('^chip\\.', datnames)[1]])	#name of the first array with the chip-prefix
+matrices <- sub(suffix, '', datnames[grep(suffix, datnames)])			#returns chip.,  flags., etc
+
+# identify annotation columns (that are not part of any of the matrices)
+annotations <- 1:ncol(dat)
+for (m in matrices) {
+	annotations <- setdiff(annotations, grep(m, datnames))
+}
+dat2 <- dat[,annotations]
+
+#identify samples to be deleted
 del.cols   <- phenodata[, column1]
 del.groups <- unique(phenodata[which(del.cols == 1), column2])
 sample.groups <- phenodata[, column2]
 
-dat.sub <- matrix(0, ncol=ncol(dat), nrow(dat));
-for (i in 1:length(del.groups)) {
-	if(length(which(del.cols %in% 1 & sample.groups %in% del.groups[i] == TRUE)) == 1) {
-		dat.sub[, which(sample.groups == del.groups[i])] <- dat[, (del.cols %in% 1 & sample.groups %in% del.groups[i])]
-	} else {
-		dat.sub[, which(sample.groups == del.groups[i])] <- apply(dat[, (del.cols %in% 1 & sample.groups %in% del.groups[i])], 1, mean)
-	}
-
-}
-
-# 
-dat2 <- dat - dat.sub
-dat2 <- dat2[, -(which(del.cols==1))]
-
+#create new phenodata
 phenodata2 <- phenodata[-(which(del.cols==1)),]
 phenodata2$sample <- sprintf('microarray%.3i', 1:nrow(phenodata2))
-colnames(dat2) <- phenodata2$sample
-#rownames(phenodata2) <- sprintf('microarray%.3i', 1:nrow(phenodata2))
+rownames(phenodata2) <- sprintf('microarray%.3i', 1:nrow(phenodata2))
+
+#create data matrices
+for (m in matrices) {
+	dat3 <- dat[, grep(m, datnames)]
+	if(m == "chip.") {
+		dat.sub <- matrix(0, ncol=ncol(dat3), nrow(dat3));
+		for (i in 1:length(del.groups)) {
+			if(length(which(del.cols %in% 1 & sample.groups %in% del.groups[i] == TRUE)) == 1) {
+				dat.sub[, which(sample.groups == del.groups[i])] <- dat3[, (del.cols %in% 1 & sample.groups %in% del.groups[i])]
+			} else {
+				dat.sub[, which(sample.groups == del.groups[i])] <- apply(da3t[, (del.cols %in% 1 & sample.groups %in% del.groups[i])], 1, mean)
+			}
+		}
+		dat4 <- dat3 - dat.sub
+		dat4 <- dat4[, -(which(del.cols==1))]
+	} else {		
+		#handle flag information
+		dat4 <- dat3[, -(which(del.cols==1))]
+	}
+	colnames(dat4) <- paste(m, phenodata2$sample, sep="")
+	dat2 <- cbind(dat2, dat4)
+}		
 
 # Writes out the combined table
-write.table(dat2, "deleted.tsv", sep="\t", row.names=T, col.names=T, quote=F)
+write.table(dat2, file="deleted.tsv", sep="\t", quote=F)
 write.table(phenodata2, file='phenodata.tsv', quote=FALSE, sep='\t', row.names=FALSE)
-
-c
