8d8e548795f51fbfe3834c3e944fd0a55d6fc21f
Ilari Scheinin <ilari.scheinin@gmail.com>
2014-01-11 13:51:08 +0100

    Added a new library file of R code snippets that are useful for sharing between tools. First example is a function that identifies annotation columns present in the input file.

---
+++ src/main/modules/common/R-2.15/chipster-library.R
@@ -0,0 +1,18 @@
+# Collection of handy code snippets to be shared between tools.
+# Ilari Scheinin <firstname.lastname@gmail.com>
+# 2014-01-11
+
+# This function identifies matrices (chip, flag, segmented, ...) present in the
+# data and returns the names of annotation columns, i.e. the ones that are not
+# part of any matrix.
+annotationColumns <- function(columns) {
+  suffix <- sub("^chip\\.", "", columns[grep("^chip\\.", columns)[1]])
+  suffix <- paste0(suffix, "$")
+  matrices <- sub(suffix, "", columns[grep(suffix), columns)])
+  annotations <- seq_along(columns)
+  for (m in matrices)
+    annotations <- setdiff(annotations, grep(m, columns))
+  columns[annotations]
+}
+
+# EOF
