06398b579f2c0baaa0ce8da1fee5a9d822656774
mkankain <kankainen.matti@gmail.com>
2014-02-22 19:37:52 +0200

    Posibility to select data columns for plot-chrom-pos.R

--- src/main/modules/microarray/R/plot-chrom-pos.R
+++ src/main/modules/microarray/R/plot-chrom-pos.R
@@ -2,13 +2,16 @@
 # INPUT normalized.tsv: normalized.tsv TYPE GENE_EXPRS 
 # INPUT META phenodata.tsv: phenodata.tsv TYPE GENERIC 
 # OUTPUT chromloc.pdf: chromloc.pdf 
-# PARAMETER chip.to.plot: chip.to.plot TYPE INTEGER FROM 1 TO 1000 DEFAULT 1 (Which chip to plot)
-# PARAMETER image.width: image.width TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
-# PARAMETER image.height: image.height TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
+# PARAMETER chip.to.plot: "Column to plot" TYPE COLUMN_SEL DEFAULT EMPTY (Column that contains the expression values. Data is mean centered)
+# PARAMETER image.width: "Image width" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Width of the plotted network image)
+# PARAMETER image.height: "Image height" TYPE INTEGER FROM 200 TO 3200 DEFAULT 600 (Height of the plotted network image)
+
+# PARAMETER chip.to.plot: chip.to.plot TYPE INTEGER FROM 1 TO 1000 DEFAULT 1 (Which columns to plot)
 
 # JTT 13.06.2006, created
 # JTT 22.11.2006, modified according to DG
 # MK 01.10.2013, small polishing
+# MK 22.02.2014, added ability select column for plotting
 
 # Loads the libraries
 library(geneplotter)
@@ -41,18 +44,31 @@ if (length(grep(".db", lib)) == 0 & length(grep("pmcdf", lib)) == 0) {
 # Need a parameter lib that defines the Affymetrix chip type
 chromloc<-buildChromLocation(gsub(".db", "", lib))
 
-# Separates expression values and flags
-dat2<-dat[,grep("chip", names(dat))]
-
-# Scaling the data to the same mean
-scaled.dat<-genescale(dat2)
-
-# Which genes are up- or down-regulated
-if(chip > ncol(scaled.dat)) {
-	stop("CHIPSTER-NOTE: You have selected a chip that does not exists")
+#scaled.dat <-dat[,grep(chip, colnames(dat))]
+if(length(grep("chip", chip)) > 0) {
+	chip.cols <- grep("chip", names(dat))
+	dat2 <- dat[,grep("chip", names(dat))]
+	# Scaling the data to the same mean
+	dat2 <- genescale(dat2)
+	dat[, chip.cols] <- dat2
 }
-up<-dat2[which(scaled.dat[,chip]>0),]
-down<-dat2[which(scaled.dat[,chip]<0),]
+
+# Separates expression values and flags
+#dat2<-dat[,grep("chip", names(dat))]
+# Scaling the data to the same mean
+#scaled.dat<-genescale(dat2)
+# Which genes are up- or down-regulated
+#if(chip > ncol(scaled.dat)) {
+#	stop("CHIPSTER-NOTE: You have selected a chip that does not exists")
+#}
+
+scaled.dat <- dat[, grep(chip, colnames(dat))]
+up <- dat[which(scaled.dat > 0), grep(chip, colnames(dat))]
+down <- dat[which(scaled.dat < 0), grep(chip, colnames(dat))]
+
+print(up)
+print(down)
+koe <- koe +1
 
 # What chromosomes are there for the species?
 x<-get(paste(gsub(".db", "", lib), "CHR", sep=""))
