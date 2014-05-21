7fac9dfabae224252656d95491029ed6184d3b36
ekorpela <ekorpela@EKORPELA-X220.windows.csc.fi>
2014-05-21 09:18:58 +0300

    Mothur tools and some utility scripts moved to R3.

--- src/main/modules/ngs/R/fastx-collapser.R
+++ src/main/modules/ngs/R/fastx-collapser.R
@@ -1,4 +1,4 @@
-# TOOL fastx-collapser.R: "Remove duplicate reads from FASTQ or FASTA" (Collapses identical sequences in a FASTQ/FASTA file into a single sequence. The sequences are renamed with sequence number and the multiplicity value. This tool is based on the FASTA/Q Collapser tool of the FASTX package.)
+# TOOL fastx-collapser.R: "Remove duplicate reads from FASTQ" (Collapses identical sequences in a FASTQ file into a single sequence. The sequences are renamed with sequence number and the multiplicity value. This tool is based on the FASTA/Q Collapser tool of the FASTX package.)
 # INPUT reads.fastq TYPE GENERIC
 # OUTPUT duplicates-removed.fastq 
 # PARAMETER quality.format: "Quality value format used" TYPE [sanger: Sanger, illuminaold: "Illumina GA v1.3-1.5"] DEFAULT sanger (What quality encoding is used in your FASTQ file. Select Sanger if your data comes from Illumina 1.8 or later, SOLiD or 454.)
