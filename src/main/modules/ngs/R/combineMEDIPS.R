e7206fdb82f5e99693836cbe52f7b2234a1f7ff0
ekorpela <ekorpelainen@gmail.com>
2014-06-03 11:57:45 +0300

    Moved MEDIPS preprocessors to R3.0

---
+++ src/main/modules/ngs/R/combineMEDIPS.R
@@ -0,0 +1,9 @@
+# TOOL combineMEDIPS.R: "Combine several MEDIPS formatted files" (Combines MEDIPS formatted files.)
+# INPUT MEDIPS-input{...}.tsv: "MEDIPS files" TYPE GENERIC
+# OUTPUT MEDIPS-input.tsv: "A converted BAM file suitable for MEDIPS analysis"
+
+files<-dir(pattern=".tsv")
+for(i in 1:length(files)) {
+   dat<-read.table(files[i], header=F, sep="\t")
+   write.table(dat, "MEDIPS-input.tsv", append=TRUE, quote=F, sep="\t", row.names=FALSE, col.names=FALSE)
+}
