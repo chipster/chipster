1d56e66106bc130937169e91d0b57c737a541bc0
tourunen <olli.tourunen@csc.fi>
2014-06-09 22:11:31 +0300

    removed R-2.12.1-medips

--- src/main/admin/vm/install-chipster.sh
+++ src/main/admin/vm/install-chipster.sh
@@ -282,7 +282,7 @@ then
   # old R versions for Chipster 3.0
 
   # R-2.12.1-medips
-  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-2.12.1-medips.tar.gz | tar -xz -C ${TOOLS_PATH}/
+  #curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-2.12.1-medips.tar.gz | tar -xz -C ${TOOLS_PATH}/
 
   # R-2.15.1-variantannotation
   curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-2.15.1-variantannotation.tar.gz | tar -xz -C ${TOOLS_PATH}/
