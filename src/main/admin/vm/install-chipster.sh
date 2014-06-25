b58695b857f14410d8ad1f3b0661020912bea1ab
Taavi Hupponen <taavi.hupponen@csc.fi>
2014-06-25 15:33:36 +0300

    Set permissions in the end of install-chipster.sh

--- src/main/admin/vm/install-chipster.sh
+++ src/main/admin/vm/install-chipster.sh
@@ -787,6 +787,7 @@ fi
 chown -R -h ${USERNAME}.${GROUPNAME} ${CHIP_PATH}/
 if [ -d /mnt/tools/ ]; then
   chown -R ${USERNAME}.${GROUPNAME} /mnt/tools/
+  chmod -R go-rwxst,u=rwX,go=rX /mnt/tools/
 fi
 
 ## Clean up:
