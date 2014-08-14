f965edd46cb6f9b63ceca468061ee9a7e52798fc
Taavi Hupponen <taavi.hupponen@csc.fi>
2014-08-14 09:50:10 +0300

    Export EMBOSS_OPTIONS when building tools

--- src/main/admin/vm/modules/external_tools/EMBOSS.bash
+++ src/main/admin/vm/modules/external_tools/EMBOSS.bash
@@ -1,21 +1,23 @@
 ##depends:none
 
 # EMBOSS, GPL
-  	apt-get -y install libgd2-noxpm-dev # sudo, emboss needs this to create png images
- 	 # also vmbin from nic
+  apt-get -y install libgd2-noxpm-dev # sudo, emboss needs this to create png images
+ 	# also vmbin from nic
 
- 	 EMBOSS_VERSION=6.5.7
- 	 EMBOSS_PATH=${TOOLS_PATH}/EMBOSS-${EMBOSS_VERSION}
+ 	EMBOSS_VERSION=6.5.7
+ 	EMBOSS_PATH=${TOOLS_PATH}/EMBOSS-${EMBOSS_VERSION}
  	 
 	#note version in path                                                                                                                                                              
 	curl -s http://www.nic.funet.fi/pub/sci/molbio/chipster/dist/tools_extras/EMBOSS/EMBOSS-${EMBOSS_VERSION}.tar.gz | tar -xz -C ${TMPDIR_PATH}/
-  	cd ${TMPDIR_PATH}/EMBOSS-6.5.7
+  cd ${TMPDIR_PATH}/EMBOSS-6.5.7
 	
-  	#wget ftp://emboss.open-bio.org/pub/EMBOSS/fixes/patches/patch-1-11.gz                                                                                                              
- 	 #gunzip patch-1-11.gz                                                                                                                                                               
-	  #patch -p1 < patch-1-11                                                                                                                                                             
+  #wget ftp://emboss.open-bio.org/pub/EMBOSS/fixes/patches/patch-1-11.gz                                                                                                              
+ 	#gunzip patch-1-11.gz                                                                                                                                                               
+	#patch -p1 < patch-1-11                                                                                                                                                             
 	
-	EMBOSS_OPTIONS="--prefix=${EMBOSS_PATH}"
+	# used also by other emboss related apps (depend on EMBOSS.bash)
+	export EMBOSS_OPTIONS="--prefix=${EMBOSS_PATH}"
+	
 	./configure ${EMBOSS_OPTIONS}
 	make
 	make install
