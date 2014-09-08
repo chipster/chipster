eb59455ae9bbae4ce1c0f5556a698a3d798b01e3
klemela <petri.klemela@csc.fi>
2014-09-08 17:37:40 +0300

    Add A5 and Fseq bff files to tools

---
+++ src/main/admin/vm/modules/external_tools/A5.bash
@@ -0,0 +1,13 @@
+##depends:none
+
+# Fseq bff files
+  cd ${TMPDIR_PATH}/
+  
+  wget_retry -nv -O a5_miseq_linux_20140604.tar.gz http://downloads.sourceforge.net/project/ngopt/a5_miseq_linux_20140604.tar.gz
+  
+  tar zxvf a5_miseq_linux_20140604.tar.gz
+  
+  mv a5_miseq_linux_20140604 ${TOOLS_PATH}/a5_miseq
+   
+  rm -f a5_miseq_linux_20140604.tar.gz
+  
