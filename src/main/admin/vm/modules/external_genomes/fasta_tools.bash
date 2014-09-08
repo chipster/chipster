723239106517f1cdce4cde59a0b18219001b8f84
klemela <petri.klemela@csc.fi>
2014-09-08 17:37:07 +0300

    Remove unused genomes and inst_files from tool building

--- src/main/admin/vm/modules/external_genomes/fasta_tools.bash
+++
@@ -1,7 +0,0 @@
-##depends:external_genomes/data_for_tools.bash
-
-# Fasta files for tools
-
-#  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/genomes/fasta_mm9.tar.gz | tar -xz -C ${TOOLS_PATH}/
-#  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/genomes/fasta_rn4.tar.gz | tar -xz -C ${TOOLS_PATH}/
-#  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/genomes/fasta_athaliana.TAIR10.tar.gz | tar xz -C ${TOOLS_PATH}/
--- src/main/admin/vm/modules/inst_files/finish.bash
+++
@@ -1,4 +0,0 @@
-#finish/checksums.bash
-#finish/fix_rights.bash
-#finish/cleaner.bash
-#finish/init_d.bash
