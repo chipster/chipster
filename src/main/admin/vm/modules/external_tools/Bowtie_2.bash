6eff29785c01852477f8b1d33976ae2b11f7c941
Harri Hämäläinen <github@hhamalai.net>
2014-10-09 16:58:10 +0300

    Testing local CI server

--- src/main/admin/vm/modules/external_tools/Bowtie_2.bash
+++ src/main/admin/vm/modules/external_tools/Bowtie_2.bash
@@ -2,7 +2,8 @@
 
 # Bowtie 2, Artistic License
   cd ${TMPDIR_PATH}/
-  wget_retry -nv -O bowtie2-2.1.0-linux-x86_64.zip http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.1.0/bowtie2-2.1.0-linux-x86_64.zip/download
+  # wget_retry -nv -O bowtie2-2.1.0-linux-x86_64.zip http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.1.0/bowtie2-2.1.0-linux-x86_64.zip/download
+  wget_retry -nv -O bowtie2-2.1.0-linux-x86_64.zip http://192.168.15.254/artefacts/build_tools/bowtie2-2.1.0-linux-x86_64.zip
   unzip -q bowtie2-2.1.0-linux-x86_64.zip
   mv bowtie2-2.1.0 ${TOOLS_PATH}
   ln -s bowtie2-2.1.0 ${TOOLS_PATH}/bowtie2
