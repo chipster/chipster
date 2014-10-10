9ea38a1b306b13cd9e3405732bbe08dcf3565844
Harri Hämäläinen <hhamalai@iki.fi>
2014-10-10 13:35:33 +0300

    testing sourcing the bash functions in modules

--- src/main/admin/vm/modules/start/htseq_pre.bash
+++ src/main/admin/vm/modules/start/htseq_pre.bash
@@ -2,6 +2,7 @@
 
 # HTSeq, GPL v3 or later
 # part 1
+source ../installation_files/functions.bash
 pip install HTSeq==0.6.1
 wget_retry -O /usr/local/bin/htseq-count_chr http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/htseq/htseq-count_chr 
 chmod 755 /usr/local/bin/htseq-count_chr
