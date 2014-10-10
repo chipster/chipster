c2d37cb4e1225c892b3fb244d10e5301ec2233ce
Harri Hämäläinen <github@hhamalai.net>
2014-10-10 10:24:04 +0300

    load Bash functions used later in modules

--- src/main/admin/vm/config.bash
+++ src/main/admin/vm/config.bash
@@ -64,6 +64,9 @@ if [ ! -d $paralleldir ]; then
 	mkdir $paralleldir
 fi
 
+# load a few auxiliary Bash functions
+source installation_files/functions.bash
+
 # squid proxy
 export http_proxy="http://192.168.15.254:3128"
 
