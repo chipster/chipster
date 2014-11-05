aff6e9759d97355b5a4141b480578385124bfbdb
Taavi Hupponen <taavi.hupponen@csc.fi>
2014-11-05 14:20:56 +0200

    Update download-tools.sh, add it to tar

---
+++ src/main/applications/wrapper/download-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                                                     
+# Download tools binaries
+
+CHIPSTER_PATH=/opt/chipster
+TOOLS_PATH=${CHIPSTER_PATH}/tools
+HOST=www.nic.funet.fi
+URL_PATH_PREFIX=/pub/sci/molbio/chipster/dist/virtual_machines
+
+# get Chipster version
+CHIPSTER_VERSION=`ls -1 ${CHIPSTER_PATH}/shared/lib | grep ^chipster-[0-9\\.]*.jar | gawk 'match($0, "chipster-([0-9\\\\.]*).jar", g) {print g[1]}'`
+
+# construct url 
+TOOLS_URL=http://${HOST}/${URL_PATH_PREFIX}/${CHIPSTER_VERSION}/tools/tools.tar.gz
+
+# download
+curl ${TOOLS_URL} | tar -xz -C ${TOOLS_PATH}/
