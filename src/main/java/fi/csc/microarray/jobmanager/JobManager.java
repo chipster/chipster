dc3b6d76eaab25c320ef3d2f5812b691d2b5f2f4
Taavi Hupponen <taavi.hupponen@csc.fi>
2015-05-22 14:55:36 +0300

    Handle some job messages

--- src/main/java/fi/csc/microarray/jobmanager/JobManager.java
+++ src/main/java/fi/csc/microarray/jobmanager/JobManager.java
@@ -17,6 +17,10 @@ import fi.csc.microarray.messaging.MessagingTopic.AccessMode;
 import fi.csc.microarray.messaging.MonitoredNodeBase;
 import fi.csc.microarray.messaging.Topics;
 import fi.csc.microarray.messaging.message.ChipsterMessage;
+import fi.csc.microarray.messaging.message.CommandMessage;
+import fi.csc.microarray.messaging.message.JobLogMessage;
+import fi.csc.microarray.messaging.message.JobMessage;
+import fi.csc.microarray.messaging.message.ResultMessage;
 import fi.csc.microarray.service.KeepAliveShutdownHandler;
 import fi.csc.microarray.service.ShutdownCallback;
 import fi.csc.microarray.util.SystemMonitorUtil;
@@ -26,6 +30,105 @@ public class JobManager extends MonitoredNodeBase implements MessagingListener, 
 	private static Logger logger;
 
 	private MessagingEndpoint endpoint;
+	
+	MessagingTopic fromClientTopic;
+	MessagingTopic jobManagerAdminTopic;	
+	MessagingTopic jobManagerTopic;
+	MessagingTopic compTopic;
+	
+	
+	private class ClientMessageListener implements MessagingListener {
+
+		@Override
+		public void onChipsterMessage(ChipsterMessage msg) {
+			try {
+
+				// JobMessage
+				if (msg instanceof JobMessage) {
+					handleClientJobMessage((JobMessage) msg);
+				} 
+
+				// CommandMessage
+				else if (msg instanceof CommandMessage) {
+					handleClientCommandMessage((CommandMessage) msg);
+				}
+
+				// other messages
+				// TODO
+				else {	
+				//	                self.send_to(TOPICS['comp_topic'], frame.headers, frame.body,
+				//	                             reply_to=TOPICS['jobmanager_topic'])
+				}
+					
+			} catch (Exception e) {
+				logger.error(e);
+			}
+		}
+
+	}
+	
+
+	private class CompMessageListener implements MessagingListener {
+
+		@Override
+		public void onChipsterMessage(ChipsterMessage msg) {
+			try {
+				
+				if (msg instanceof ResultMessage) {
+					handleResultMessage((ResultMessage) msg);
+				} 
+
+				else if (msg instanceof JobLogMessage) {
+					handleJobLogMessage((JobLogMessage) msg);
+				}
+
+				else if (msg instanceof CommandMessage) {
+					handleCompCommandMessage((CommandMessage) msg);
+				}
+
+				// TODO what is StatusMessage
+//				else if (msg instanceof StatusMessage) {
+//					// do nothing?
+//				}
+
+				// TODO
+				else {
+//		            try:
+//		                client_topic = self.resolve_reply_to(body)
+//		                logger.warning("unknown msg: %s" % frame.body)
+//		                self.send_to(client_topic, headers, json.dumps(frame.body),
+//		                             reply_to=headers.get('reply-to'))
+//		            except ReplyToResolutionException:
+//		                logger.warning("unable to resolve reply_to address")
+				}
+
+			} catch (Exception e) {
+				logger.error(e);
+			}
+		}
+
+		private void handleCompCommandMessage(CommandMessage msg) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void handleJobLogMessage(JobLogMessage msg) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void handleResultMessage(ResultMessage msg) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
+	
+	
+	
+	
+	
+		
 	
 
 	public JobManager(String configURL) throws Exception {
@@ -42,22 +145,23 @@ public class JobManager extends MonitoredNodeBase implements MessagingListener, 
 		this.endpoint = new JMSMessagingEndpoint(this);
 		
 
-		MessagingTopic clientTopic = endpoint.createTopic(Topics.Name.AUTHORISED_REQUEST_TOPIC, AccessMode.READ);
-		clientTopic.setListener(this);
+		fromClientTopic = endpoint.createTopic(Topics.Name.AUTHORISED_REQUEST_TOPIC, AccessMode.READ);
+		fromClientTopic.setListener(new ClientMessageListener());
 
-		MessagingTopic jobmanagerAdminTopic = endpoint.createTopic(Topics.Name.JOBMANAGER_ADMIN_TOPIC, AccessMode.READ);
-		jobmanagerAdminTopic.setListener(this);
+		jobManagerAdminTopic = endpoint.createTopic(Topics.Name.JOBMANAGER_ADMIN_TOPIC, AccessMode.READ);
+		jobManagerAdminTopic.setListener(new CompMessageListener());
+		
+		jobManagerTopic = endpoint.createTopic(Topics.Name.JOBMANAGER_TOPIC, AccessMode.READ);
+		jobManagerTopic.setListener(this);
+		
+		compTopic = endpoint.createTopic(Topics.Name.AUTHORIZED_MANAGED_REQUEST_TOPIC, AccessMode.WRITE);
+
+		
+		//	    'comp_admin_topic': '/topic/comp-admin-topic',
+		//        TOPICS['admin_topic'],
+
 		
 		
-//      TOPICS['jobmanager_topic'],
-//		MessagingTopic jobmanagerTopic = endpoint.createTopic(Topics.Name., AccessMode.READ);
-//		jobManagerTopic.setListener(this);
-		
-//        TOPICS['admin_topic'],
-		
-		
-//		MessagingTopic analyseTopic = endpoint.createTopic(Topics.Name.AUTHORIZED_MANAGED_REQUEST_TOPIC, AccessMode.READ);
-//		analyseTopic.setListener(this);
 
 		
 
@@ -67,6 +171,50 @@ public class JobManager extends MonitoredNodeBase implements MessagingListener, 
 		logger.info("jobmanager is up and running [" + ApplicationConstants.VERSION + "]");
 		logger.info("[mem: " + SystemMonitorUtil.getMemInfo() + "]");
 	}
+	
+	
+	private void handleClientJobMessage(JobMessage msg) throws JMSException {
+		// TODO add to db
+		
+		// forward
+		msg.setReplyTo(jobManagerTopic.getJMSTopic());
+		compTopic.sendMessage(msg);
+	}
+
+	private void handleClientCommandMessage(CommandMessage msg) throws JMSException {
+
+		if (CommandMessage.COMMAND_GET_JOB.equals(msg.getCommand())) {
+			// TODO
+//            client_topic = headers.get('reply-to')
+//            job_id = msg.get('job-id')
+//            headers = populate_headers(client_topic, RESULT_MESSAGE)
+//            with self.session_scope() as session:
+//                try:
+//                    job = update_job_reply_to(session, job_id, client_topic)
+//                    if job.finished and job.results:
+//                        resp_body = job.results
+//                    elif job.finished:
+//                        resp_body = populate_job_result_body(job_id, exit_state='CANCELLED')
+//                    else:
+//                        resp_body = populate_job_running_body(job.job_id)
+//                except JobNotFound:
+//                    logger.exception("job %s not found" % job_id)
+//                    resp_body = populate_job_result_body(job_id, error_msg='job not found')
+//            self.send_to(client_topic, headers, resp_body)
+
+			
+		} else if (CommandMessage.COMMAND_CANCEL.equals(msg.getCommand())) {
+			// TODO
+//            job_id = msg.get('parameter0')
+//            session_id = headers.get('session-id')
+//            self.cancel_job(job_id, session_id)
+			
+		} else {
+			compTopic.sendMessage(msg);
+		}
+	}
+	
+	
 	
 	
 	@Override
