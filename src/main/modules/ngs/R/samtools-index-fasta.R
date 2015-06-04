be11446273900f388f3fcc4c0ebcb395fe2db6ec
saren <ari-matti.saren@csc.fi>
2015-06-04 15:24:10 +0300

    tool-utils.R and the first tools using it

--- src/main/modules/ngs/R/samtools-index-fasta.R
+++ src/main/modules/ngs/R/samtools-index-fasta.R
@@ -1,4 +1,4 @@
-# TOOL samtools-index-fasta.R: "Index FASTA" (Creates an index for a FASTA file. Please rename the index file to have the same name as the FASTA file. This tool is based on the SAMTools package.)
+# TOOL samtools-index-fasta.R: "Index FASTA" (Creates an index for a FASTA file. This tool is based on the SAMTools package.)
 # INPUT sequence.fa TYPE GENERIC 
 # OUTPUT sequence.fa.fai
 
@@ -12,8 +12,16 @@ samtools.binary <- c(file.path(chipster.tools.path, "samtools", "samtools"))
 
 system(paste(samtools.binary, "faidx sequence.fa"))
 
+# Handle output names
+source(file.path(chipster.common.path, "tool-utils.R"))
 
+# read input names
+inputnames <- read_input_definitions()
 
+# Make a matrix of output names
+outputnames <- matrix(NA, nrow=1, ncol=2)
+outputnames[1,] <- c("sequence.fa.fai", paste(remove_postfix(inputnames$sequence.fa, ".gz"), ".fai", sep =""))
 
-
+# Write output definitions file
+write_output_definitions(outputnames)
 
