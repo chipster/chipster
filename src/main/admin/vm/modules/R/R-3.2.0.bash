176aa9d70c3266836cfa288dee18bf97dd9d624e
Taavi Hupponen <taavi.hupponen@csc.fi>
2015-06-24 13:12:11 +0300

    Install libs for R-3.2.0

--- src/main/admin/vm/modules/R/R-3.2.0.bash
+++ src/main/admin/vm/modules/R/R-3.2.0.bash
@@ -16,6 +16,17 @@ export MAKEFLAGS=
 cd ../
 rm -rf R-${R_VER}/
 
+
+# R libraries, if parallel processing is on, run libs installation with GNU parallel
+#if [ $parallel == "1" ]; then
+    #    sem --no-notice --tmpdir $paralleldir -j 3 ${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla ${CHIP_PATH}/comp/modules/admin/R/install-libs.R
+#else
+   #   ${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla ${CHIP_PATH}/comp/modules/admin/R/install-libs.R
+#fi
+
+${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla ${CHIP_PATH}/comp/modules/admin/R-${R_VER}/install-libs.R
+
+
 #curl -L http://bio.nic.funet.fi/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.1.2-vmbin/R-3.1.2-2015-02-05.tar.gz | tar -xz -C ${TOOLS_PATH}/
 #curl -L http://nic.funet.fi/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.1.2-vmbin/R-3.1.2-2015-04-08.tar.gz | tar -xz -C ${TOOLS_PATH}/
 #curl -L http://nic.funet.fi/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.1.2-vmbin/R-3.1.2-2015-06-01.tar.gz | tar -xz -C ${TOOLS_PATH}/
