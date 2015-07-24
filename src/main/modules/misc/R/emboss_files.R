97cb34cf429668c2db6f744cc98b7a08f9549944
kkmattil <kimmo.mattila@csc.fi>
2015-07-24 15:47:58 +0300

    concatenate manual added.

--- src/main/modules/misc/R/emboss_files.R
+++ src/main/modules/misc/R/emboss_files.R
@@ -1,7 +1,7 @@
 # TOOL emboss_files.R: "Simple file operations" (Tool to remove certain characters from text files.)
 # INPUT input.txt: "Input file" TYPE GENERIC
 # OUTPUT output.txt
-# PARAMETER OPTIONAL ecommand: "Select task" TYPE [nohtml: "(nohtml\) Remove mark-up e.g. HTML tags from a text file", noreturn: "(noreturn\) Remove carriage return from a text file", nospace: "(nospace\) Remove whitespace from a text file", notab: "(notab\) Replace tabs with spaces in a text file", trimspace: "(trimspace\) Remove extra whitespace from a text file", semicolon: "Remove semicolons (;\) from a text file", dot: "Remove dots (.\) from a text file", slash: "Remove slash characters (\/\) from a text file", at: "Remove at characters (\@\) from a text file", hash: "Remove hash characters (#\) from a text file", sqbrc: "Remove square bracket characters (\[\]\) from a text file", curbrc: "Remove curly bracket characters (\{\}\) from a text file", percent: "Remove percent characters (\%\) from a text file", dollar: "Remove dollar characters (\$\) from a text file", dq: "Remove double quotation characters (\"\) from a text file", sq: "Remove singe quotation characters (\'\) from a text file", vb: "Remove vertical bar (\|\) from a text file", et: "Remove et characters (\&\) from a text file" ] DEFAULT nohtml (Choose operateion to execute) 
+# PARAMETER OPTIONAL ecommand: "Select task" TYPE [nohtml: "(nohtml\) Remove mark-up e.g. HTML tags from a text file", noreturn: "(noreturn\) Remove carriage return from a text file", nospace: "(nospace\) Remove whitespace from a text file", notab: "(notab\) Replace tabs with spaces in a text file", trimspace: "(trimspace\) Remove extra whitespace from a text file", semicolon: "Remove semicolons (;\) from a text file", dot: "Remove dots (.\) from a text file", slash: "Remove slash characters (\/\) from a text file", at: "Remove at characters (\@\) from a text file", hash: "Remove hash characters (#\) from a text file", sqbrc: "Remove square bracket characters (\[\]\) from a text file", curbrc: "Remove curly bracket characters (\{\}\) from a text file", percent: "Remove percent characters (\%\) from a text file", dollar: "Remove dollar characters (\$\) from a text file", dq: "Remove double quotation characters (\"\) from a text file", sq: "Remove singe quotation characters (\'\) from a text file", vb: "Remove vertical bar (\|\) from a text file", et: "Remove et characters (\&\) from a text file" , gt: "Remove greater-than characters (\>\) from a text file", lt: "Remove less-than characters (\<\) from a text file"] DEFAULT nohtml (Choose operateion to execute) 
 
 
 source(file.path(chipster.common.path, "zip-utils.R"))
@@ -60,13 +60,19 @@ if ( ecommand == "sq"){
 }
 
 if ( ecommand == "vb"){
-	system("tr -d \| <  input.txt  > output.txt ")
+	system("tr -d '|' <  input.txt  > output.txt ")
 }
 
 if ( ecommand == "et"){
-	system("tr -d "&" <  input.txt  > output.txt ")
+	system("tr -d '&' <  input.txt  > output.txt ")
 }
 
+if ( ecommand == "gt"){
+	system("tr -d '>' <  input.txt  > output.txt ")
+}
 
+if ( ecommand == "lt"){
+	system("tr -d '<' <  input.txt  > output.txt ")
+}
 
 
