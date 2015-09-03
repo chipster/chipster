2b6631269713d4a94fdce09c0daaddb2f15287f1
saren <ari-matti.saren@csc.fi>
2015-09-03 13:39:52 +0300

    Picard tools 1.138

--- src/main/admin/vm/modules/external_tools/Picard_tools.bash
+++ src/main/admin/vm/modules/external_tools/Picard_tools.bash
@@ -4,11 +4,11 @@ source ../installation_files/functions.bash
 
 # Picard tools, Apache License V2.0, MIT
   cd ${TMPDIR_PATH}/
-  wget_retry -nv -O picard-tools-1.105.zip http://sourceforge.net/projects/picard/files/picard-tools/1.105/picard-tools-1.105.zip/download
-  unzip -q picard-tools-1.105.zip
-  rm picard-tools-1.105.zip
+  wget_retry -nv -O picard-tools-1.138.zip https://github.com/broadinstitute/picard/releases/download/1.138/picard-tools-1.138.zip
+  unzip -q picard-tools-1.138.zip
+  rm picard-tools-1.138.zip
   # remove this optional jar because it's in the root of the zip
-  rm snappy-java-1.0.3-rc3.jar
-  mv picard-tools-1.105/ ${TOOLS_PATH}
+  #rm snappy-java-1.0.3-rc3.jar
+  mv picard-tools-1.138/ ${TOOLS_PATH}
   cd ${TOOLS_PATH}
-  ln -s picard-tools-1.105 picard-tools
+  ln -s picard-tools-1.138 picard-tools
