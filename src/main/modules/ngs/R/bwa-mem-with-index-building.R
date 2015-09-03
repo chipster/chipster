7b929ddd78962ac50d3cb8a52783bf4387e1ff5f
ekorpela <ekorpelainen@gmail.com>
2015-09-03 15:03:26 +0300

    Polished BWA MEM.

--- src/main/modules/ngs/R/bwa-mem-with-index-building.R
+++ src/main/modules/ngs/R/bwa-mem-with-index-building.R
@@ -1,37 +1,37 @@
-# TOOL bwa-mem-with-index-building.R: "BWA MEM for single and paired-end reads and own genome" (Aligns reads to genomes using the BWA MEM algorithm. If just one reads set defined the a sigle end analysis is run. If two reads files are defined, then mapping is done in paired-end mode. Results are sorted and indexed BAM files, which are ready for viewing in the Chipster genome browser. 
-# Note that this BWA tool requires that you have imported the reference genome to Chipster in fasta format. If you would like to align reads against publicly available genomes, please use the tool \"BWA MEM for single end reads\".)
+# TOOL bwa-mem-with-index-building.R: "BWA MEM for single or paired end reads and own genome" (Aligns reads to genomes using the BWA MEM algorithm. If just one read file is given, then a single end analysis is run. If two read files are given, then mapping is done in paired end mode. Results are sorted and indexed BAM files, which are ready for viewing in the Chipster genome browser. 
+# Note that this BWA tool requires that you have imported the reference genome to Chipster in fasta format. If you would like to align reads against publicly available genomes, please use the tool \"BWA MEM for single or paired end reads\".)
 # INPUT genome.txt: "Reference genome" TYPE GENERIC
-# INPUT reads.txt: "Read set to align" TYPE GENERIC 
-# INPUT OPTIONAL reads2.txt: "Paired-end read set 2 to align" TYPE GENERIC 
+# INPUT reads.fastq: "Read file 1" TYPE GENERIC 
+# INPUT OPTIONAL reads2.fastq: "Read file 2 for paired end reads" TYPE GENERIC 
 # OUTPUT bwa.bam 
 # OUTPUT bwa.bam.bai 
 # OUTPUT bwa.log 
-# PARAMETER minseedlen: "Minimum seed length" TYPE INTEGER DEFAULT 19 (Minimum seed lengt)
-# PARAMETER bandwith: "Band width" TYPE INTEGER DEFAULT 100 (Band width for banded alignment)
-# PARAMETER matchscore: "Match score" TYPE INTEGER DEFAULT 1 (Score for a sequence match)
-# PARAMETER mismatchscore: "Mismatch penalty" TYPE INTEGER DEFAULT 4 (Penalty for a mismatch)
-# PARAMETER gapopen: "Gap opening penalty" TYPE INTEGER DEFAULT 6 (Gap opening penalty)
-# PARAMETER gapextension: "Gap extension penalty" TYPE INTEGER DEFAULT 1 (Gap extension penalty)
-# PARAMETER clippenalty: "Penalty for end clipping" TYPE INTEGER DEFAULT 5 ( penalty for 5\'- and 3\'-end clipping ) 
-# PARAMETER OPTIONAL rgid: "Read group identifier" TYPE STRING (Read group identifier.)
-# PARAMETER OPTIONAL rgsm: "Sample name for read group" TYPE STRING (The name of the sample sequenced in this read group.)
-# PARAMETER OPTIONAL rgpl: "Platform for read group" TYPE [ none: "Not defined", ILLUMINA, SOLID, LS454, HELICOS, PACBIO] DEFAULT none (Platform\/technology used to produce the read.)
-# PARAMETER OPTIONAL rglb: "DNA preparation library identify" TYPE STRING ( DNA preparation library identify, MarkDuplicates uses this field to determine which read groups might contain molecular duplicates, in case the same DNA library was sequenced on multiple lanes.)
+# PARAMETER OPTIONAL minseedlen: "Minimum seed length" TYPE INTEGER DEFAULT 19 (Matches shorter than this will be missed when looking for maximal exact matches or MEMs in the first alignment phase.)
+# PARAMETER OPTIONAL bandwith: "Maximum gap length" TYPE INTEGER DEFAULT 100 (Gaps longer than this will not be found. Note also scoring matrix and hit length affect the maximum gap length, in addition to this band width parameter.)
+# PARAMETER OPTIONAL matchscore: "Match score" TYPE INTEGER DEFAULT 1 (Score for a matching base.)
+# PARAMETER OPTIONAL mismatchscore: "Mismatch penalty" TYPE INTEGER DEFAULT 4 (Penalty for a mismatching base.)
+# PARAMETER OPTIONAL gapopen: "Gap opening penalty" TYPE INTEGER DEFAULT 6 (Gap opening penalty.)
+# PARAMETER OPTIONAL gapextension: "Gap extension penalty" TYPE INTEGER DEFAULT 1 (Gap extension penalty.)
+# PARAMETER OPTIONAL clippenalty: "Penalty for end clipping" TYPE INTEGER DEFAULT 5 (Penalty for 5\'- and 3\'-end clipping. When performing the Smith-Waterman extension of the seed alignments, BWA-MEM keeps track of the best score reaching the end of the read. If this score is larger than the best SW score minus the clipping penalty, clipping will not be applied.) 
+# PARAMETER OPTIONAL rgid: "Read group dentifier" TYPE STRING (Read group identifier. If you want to add the read group line in the BAM file, you have to give this information.)
+# PARAMETER OPTIONAL rgsm: "Sample name for read group" TYPE STRING (The name of the sample sequenced in this read group. Note that you have to fill in also the read group identifier parameter for the read group information to appear in the BAM file.)
+# PARAMETER OPTIONAL rgpl: "Platform for read group" TYPE [ none: "Not defined", ILLUMINA, SOLID, LS454, HELICOS, PACBIO] DEFAULT none (Platform\/technology used to produce the read. Note that you have to fill in also the read group identifier parameter for the read group information to appear in the BAM file.)
+# PARAMETER OPTIONAL rglb: "Library identifier for read group" TYPE STRING (DNA preparation library identifier. The Mark Duplicates tool uses this field to determine which read groups might contain molecular duplicates, in case the same DNA library was sequenced on multiple lanes. Note that you have to fill in also the read group identifier parameter for the read group information to appear in the BAM file.)
 
 # KM 1.9.2015
 
 
 # check out if the file is compressed and if so unzip it
 source(file.path(chipster.common.path, "zip-utils.R"))
-unzipIfGZipFile("reads.txt")
+unzipIfGZipFile("reads.fastq")
 unzipIfGZipFile("genome.txt")
 
-pe_check.command <- ("ls reads2.txt 2>/dev/null | wc -l")
+pe_check.command <- ("ls reads2.fastq 2>/dev/null | wc -l")
 
 paired.end <- system(pe_check.command, intern = TRUE )
 
 if ( paired.end == 1 ){
-	unzipIfGZipFile("reads2.txt")
+	unzipIfGZipFile("reads2.fastq")
 }
 
 
@@ -69,16 +69,16 @@ if ( nchar(rgid) > 0 ){
 }
 
 # command ending
-command.end <- paste(bwa.genome, "reads.txt 1> alignment.sam 2>> bwa.log")
+command.end <- paste(bwa.genome, "reads.fastq 1> alignment.sam 2>> bwa.log")
 if ( paired.end == 1 ){
-	command.end <- paste(bwa.genome, "reads.txt reads2.txt 1> alignment.sam 2>> bwa.log")
+	command.end <- paste(bwa.genome, "reads.fastq reads2.fastq 1> alignment.sam 2>> bwa.log")
 }	
 
 bwa.command <- paste(command.start, bwa.parameters, command.end)
 
-echo.command <- paste("echo '", bwa.binary, bwa.parameters, bwa.genome, "reads.txt ' > bwa.log" )
+echo.command <- paste("echo '", bwa.binary, bwa.parameters, bwa.genome, "reads.fastq ' > bwa.log" )
 if ( paired.end == 1 ){
-	echo.command <- paste("echo '", bwa.binary, bwa.parameters, bwa.genome, "reads.txt raeds2.txt ' > bwa.log" )
+	echo.command <- paste("echo '", bwa.binary, bwa.parameters, bwa.genome, "reads.fastq reads2.fastq ' > bwa.log" )
 }
 
 #stop(paste('CHIPSTER-NOTE: ', bwa.command))
@@ -109,7 +109,7 @@ source(file.path(chipster.common.path, "tool-utils.R"))
 inputnames <- read_input_definitions()
 
 # Determine base name
-basename <- strip_name(inputnames$reads.txt)
+basename <- strip_name(inputnames$reads.fastq)
 
 # Make a matrix of output names
 outputnames <- matrix(NA, nrow=2, ncol=2)
