a8bd64de24273d035d09ad59e9dbcdbd11a60105
saren <ari-matti.saren@csc.fi>
2015-09-04 15:09:17 +0300

    Updated install scripst for BEDtools, BWA, FastQC, Trimmomatic, vcftools, cd-hit, maview

--- src/main/admin/vm/modules/external_tools/BEDTools.bash
+++ src/main/admin/vm/modules/external_tools/BEDTools.bash
@@ -1,14 +1,20 @@
 ##depends:none
 
+source ../installation_files/functions.bash
+
 # BEDTools, GNU GPL v2
   cd ${TMPDIR_PATH}/
-  curl -s http://bedtools.googlecode.com/files/BEDTools.v2.17.0.tar.gz | tar -xz
-  cd bedtools-2.17.0
-  make clean
+  wget_retry -nv https://github.com/arq5x/bedtools2/archive/v2.25.0.tar.gz
+  tar xf v2.25.0.tar.gz
+  cd bedtools2-2.25.0
+    make clean
   make all
   cd ../
-  mv bedtools-2.17.0 ${TOOLS_PATH}/
-  ln -s bedtools-2.17.0 ${TOOLS_PATH}/bedtools
+  mv bedtools2-2.25.0 ${TOOLS_PATH}/
+  rm v2.25.0.tar.gz
+  cd ${TOOLS_PATH}
+  ln -s bedtools2-2.25.0 ${TOOLS_PATH}/bedtools
+  
 
   #curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/bedtools-2.17.0-vmbin.tar.gz | tar -xz -C ${TOOLS_PATH}/
 
--- src/main/admin/vm/modules/external_tools/vcftools.bash
+++ src/main/admin/vm/modules/external_tools/vcftools.bash
@@ -5,12 +5,13 @@ source ../installation_files/functions.bash
 
 # vcftools, GPLv3
   cd ${TMPDIR_PATH}/
-  wget_retry -O vcftools_0.1.11.tar.gz http://sourceforge.net/projects/vcftools/files/vcftools_0.1.11.tar.gz/download
-  tar xf vcftools_0.1.11.tar.gz
-  cd vcftools_0.1.11/
+  wget_retry http://downloads.sourceforge.net/project/vcftools/vcftools_0.1.13.tar.gz
+  tar xf vcftools_0.1.13.tar.gz
+  cd vcftools_0.1.13/
   make
   cd ../
-  rm -f vcftools_0.1.11.tar.gz
-  mv vcftools_0.1.11/ ${TOOLS_PATH}/
+  rm -f vcftools_0.1.13.tar.gz
+  mv vcftools_0.1.13/ ${TOOLS_PATH}/
   #curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/vcftools_0.1.11-vmbin.tar.gz | tar -xz -C ${TOOLS_PATH}/
-  ln -s vcftools_0.1.11 ${TOOLS_PATH}/vcftools
+  cd ${TOOLS_PATH}
+  ln -s vcftools_0.1.13 ${TOOLS_PATH}/vcftools
