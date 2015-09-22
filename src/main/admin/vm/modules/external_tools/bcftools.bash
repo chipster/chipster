e35ad91ba05801387511830451ec82ad16c0577c
saren <ari-matti.saren@csc.fi>
2015-09-22 12:21:40 +0300

    Added link to bcftools in /mnt/tools

--- src/main/admin/vm/modules/external_tools/bcftools.bash
+++ src/main/admin/vm/modules/external_tools/bcftools.bash
@@ -1,5 +1,5 @@
 ##depends:none
-# SAM tools, BSD License, MIT License
+# BCF tools, BSD License, MIT License
   cd ${TMPDIR_PATH}/
   wget_retry https://github.com/samtools/bcftools/releases/download/1.2/bcftools-1.2.tar.bz2
   tar xf bcftools-1.2.tar.bz2
@@ -8,4 +8,4 @@
   cd ..
   mv bcftools-1.2/ ${TOOLS_PATH}/
   rm bcftools-1.2.tar.bz2
-  #ln -s ${TOOLS_PATH}/bcftools-1.2 ${TOOLS_PATH}/bcftools
+  ln -s ${TOOLS_PATH}/bcftools-1.2 ${TOOLS_PATH}/bcftools
