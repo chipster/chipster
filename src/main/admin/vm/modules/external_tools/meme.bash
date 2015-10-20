1bc686384cc508206cd4d73ca6710e5f081fa6ce
hupponen <taavi.hupponen@csc.fi>
2015-10-20 07:51:12 +0300

    Update meme download url

--- src/main/admin/vm/modules/external_tools/meme.bash
+++ src/main/admin/vm/modules/external_tools/meme.bash
@@ -2,7 +2,7 @@
 
 # meme, quite free, see documentation                                    
 cd ${TMPDIR_PATH}
-curl http://ebi.edu.au/ftp/software/MEME/4.2.0/meme_4.2.0.tar.gz | tar -xz -C ${TMPDIR_PATH}
+curl http://meme-suite.org/meme-software/4.2.0/meme_4.2.0.tar.gz | tar -xz -C ${TMPDIR_PATH}
 cd meme_4.2.0/
 ./configure --prefix=${TOOLS_PATH}/meme_4.2.0 --with-url="http://meme.nbcr.net/meme"
 make
