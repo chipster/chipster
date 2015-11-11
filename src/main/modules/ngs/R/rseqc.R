263ea5ee96def176cd71e13e616dfba924add22d
saren <ari-matti.saren@csc.fi>
2015-11-11 16:22:47 +0200

    Added read_distribution, changed output name

--- src/main/modules/ngs/R/rseqc.R
+++ src/main/modules/ngs/R/rseqc.R
@@ -1,7 +1,7 @@
 # TOOL rseqc.R: "RNA-seq quality metrics with RseQC" (Given an RNA-seq BAM file and gene and exon locations in a BED file, this tool reports several quality metrics such as coverage uniformity, gene and junction saturation, junction annotation and alignment statistics. This tool is based on the RSeQC package.)
 # INPUT alignment_file: "BAM file" TYPE GENERIC
 # INPUT reference_file: "BED file" TYPE GENERIC
-# OUTPUT OPTIONAL RSeQC.bamStat.txt
+# OUTPUT OPTIONAL RSeQC.txt
 # OUTPUT OPTIONAL RSeQC_report.pdf
 # PARAMETER OPTIONAL paired: "Generate inner distance plot" TYPE [yes, no] DEFAULT no (Calculate the inner distance (or insert size\) between two paired RNA reads. The distance is the mRNA length between two paired fragments.)
 
@@ -44,8 +44,18 @@ try(source("RSeQC.saturation.r"), silent=TRUE)
 system("mv RSeQC.saturation.pdf 05.pdf")
 
 # bam_stat
+system("echo 'bam_stats:' > RSeQC.txt")
+system("echo >> RSeQC.txt")
 binary <- c(file.path(chipster.tools.path, "RSeQC", "scripts", "bam_stat.py"))
-command <- paste(binary, "-i alignment_file 2> RSeQC.bamStat.txt")
+command <- paste(binary, "-i alignment_file 2>> RSeQC.txt")
+system(command)
+
+# read_distribution
+system("echo \"\n\n\" >> RSeQC.txt")
+system("echo 'read_distribution:' >> RSeQC.txt")
+system("echo >> RSeQC.txt")
+binary <- c(file.path(chipster.tools.path, "RSeQC", "scripts", "read_distribution.py"))
+command <- paste(binary, "-i alignment_file -r reference_file >> RSeQC.txt")
 system(command)
 
 # inner_distance
@@ -72,8 +82,8 @@ base <- strip_name(inputnames$alignment_file)
 
 # Make a matrix of output names
 outputnames <- matrix(NA, nrow=2, ncol=2)
-outputnames[1,] <- c("RSeQC.bamStat.txt", paste(base, ".bamStat.txt", sep=""))
-outputnames[2,] <- c("RSeQC_report.pdf", paste(base, ".pdf", sep=""))
+outputnames[1,] <- c("RSeQC.txt", paste(base, "_rseqc.txt", sep=""))
+outputnames[2,] <- c("RSeQC_report.pdf", paste(base, "_rseqc.pdf", sep=""))
 
 # Write output definitions file
 write_output_definitions(outputnames)
