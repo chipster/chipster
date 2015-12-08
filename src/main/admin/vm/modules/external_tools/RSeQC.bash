71e22af2d5ffc989f30ec68275eb3977e29b4e05
klemela <klemela>
2015-12-08 15:43:12 +0200

    Download everything from nic

--- src/main/admin/vm/modules/external_tools/RSeQC.bash
+++ src/main/admin/vm/modules/external_tools/RSeQC.bash
@@ -4,7 +4,8 @@ source ../installation_files/functions.bash
 
 # RSeQC, GPLv3
   cd ${TMPDIR_PATH}/
-  wget_retry -nv -O RSeQC-2.3.7.tar.gz http://sourceforge.net/projects/rseqc/files/RSeQC-2.3.7.tar.gz/download
+  #wget_retry -nv -O RSeQC-2.3.7.tar.gz http://sourceforge.net/projects/rseqc/files/RSeQC-2.3.7.tar.gz/download
+  wget_retry -nv -O RSeQC-2.3.7.tar.gz http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/RSeQC-2.3.7.tar.gz
   tar xf RSeQC-2.3.7.tar.gz
   mv RSeQC-2.3.7 ${TOOLS_PATH}/RSeQC-2.3.7
   rm -f RSeQC-2.3.7.tar.gz
--- src/main/admin/vm/modules/external_tools/tagcleaner.bash
+++ src/main/admin/vm/modules/external_tools/tagcleaner.bash
@@ -4,7 +4,8 @@ source ../installation_files/functions.bash
 
 # tagcleaner, GPLv3
   cd ${TMPDIR_PATH}/
-  wget_retry -nv -O tagcleaner-standalone-0.12.tar.gz http://downloads.sourceforge.net/project/tagcleaner/standalone/tagcleaner-standalone-0.12.tar.gz
+  #wget_retry -nv -O tagcleaner-standalone-0.12.tar.gz http://downloads.sourceforge.net/project/tagcleaner/standalone/tagcleaner-standalone-0.12.tar.gz
+  wget_retry -nv -O tagcleaner-standalone-0.12.tar.gz http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/tagcleaner-standalone-0.12.tar.gz
   tar xf tagcleaner-standalone-0.12.tar.gz -C ${TOOLS_PATH}/
   rm -f tagcleaner-standalone-0.12.tar.gz
   ln -s tagcleaner-standalone-0.12 ${TOOLS_PATH}/tagcleaner
