1a9f422d99462b7bf27447cb3d989b8c07093d98
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-04-05 13:49:18 +0300

    Organize imports

--- src/main/java/fi/csc/microarray/comp/OnDiskCompJobBase.java
+++ src/main/java/fi/csc/microarray/comp/OnDiskCompJobBase.java
@@ -3,7 +3,6 @@ package fi.csc.microarray.comp;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
