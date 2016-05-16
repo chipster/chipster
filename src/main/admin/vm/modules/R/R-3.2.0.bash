986e98b712aa5b343f4f037dbe322c384fad76d6
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-05-16 11:15:21 +0300

    Fix install-libs.R path

--- src/main/admin/vm/modules/R/R-3.2.0.bash
+++ src/main/admin/vm/modules/R/R-3.2.0.bash
@@ -31,6 +31,6 @@ R_VER=3.2.0
 
 curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.0-vmbin/R-3.2.0-08-07-2015.tar.gz | tar -xz -C ${TOOLS_PATH}/
 
-wget https://raw.githubusercontent.com/chipster/chipster-tools/master/modules/admin/R-3.2.0/install-libs.R
-wget https://raw.githubusercontent.com/chipster/chipster-tools/master/modules/admin/R/smip.R
+wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R-3.2.0/install-libs.R
+wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R/smip.R
 ${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla install-libs.R
