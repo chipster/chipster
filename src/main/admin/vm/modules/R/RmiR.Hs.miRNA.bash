ec7fb85218d252695091650273bb7045827b4942
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-06-16 12:50:42 +0300

    Remove obsolete RmiR.Hs.miRNA.bash

--- src/main/admin/vm/modules/R/RmiR.Hs.miRNA.bash
+++
@@ -1,5 +0,0 @@
-##depends:R/R-2.15.1-var.bash
-
-# Add RmiR.Hs.miRNA to R-2.15.1_bioc-2.11
-  #curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-2.15.1_bioc-2.11/library/RmiR.Hs.miRNA-vmbin.tar.gz | tar -xz -C ${TOOLS_PATH}/R-2.15.1_bioc-2.11/lib64/R/library/
-
