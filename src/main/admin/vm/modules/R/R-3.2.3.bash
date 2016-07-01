9ce431b07057844ccf05476cfaf08690aa83fc75
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-07-01 08:37:22 +0300

    Install R from scratch

--- src/main/admin/vm/modules/R/R-3.2.3.bash
+++ src/main/admin/vm/modules/R/R-3.2.3.bash
@@ -1,20 +1,20 @@
 ##depends:none
 
 R_VER=3.2.3  
-#cd ${TMPDIR_PATH}/
-#curl -s http://ftp.sunet.se/pub/lang/CRAN/src/base/R-3/R-${R_VER}.tar.gz | tar -xz
-#cd R-${R_VER}/
-#export MAKEFLAGS=-j
-#./configure --prefix=${TOOLS_PATH}/R-${R_VER}
-#make
-#make install
-#echo 'MAKEFLAGS=-j' > ${TOOLS_PATH}/R-${R_VER}/lib/R/etc/Makevars.site # (could also be $HOME/.R/Makevars)
+cd ${TMPDIR_PATH}/
+curl -s http://ftp.sunet.se/pub/lang/CRAN/src/base/R-3/R-${R_VER}.tar.gz | tar -xz
+cd R-${R_VER}/
+export MAKEFLAGS=-j
+./configure --prefix=${TOOLS_PATH}/R-${R_VER}
+make
+make install
+echo 'MAKEFLAGS=-j' > ${TOOLS_PATH}/R-${R_VER}/lib/R/etc/Makevars.site # (could also be $HOME/.R/Makevars)
 
 ## clean makeflags after R install
-#export MAKEFLAGS=
+export MAKEFLAGS=
 
-#cd ../
-#rm -rf R-${R_VER}/
+cd ../
+rm -rf R-${R_VER}/
 
 
 ## R libraries, if parallel processing is on, run libs installation with GNU parallel
@@ -24,12 +24,9 @@ R_VER=3.2.3
    #   #   ${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla ${CHIP_PATH}/comp/modules/admin/R/install-libs.R
 ##fi
 
-#wget https://raw.githubusercontent.com/chipster/chipster/master/src/main/modules/admin/R-3.2.0/install-libs.R
-#wget https://raw.githubusercontent.com/chipster/chipster/master/src/main/modules/admin/R/smip.R
-#${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla install-libs.R
 
-curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3-2016-03-10.tar.gz | tar -xz -C ${TOOLS_PATH}/
+#curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3-2016-03-10.tar.gz | tar -xz -C ${TOOLS_PATH}/
 
-#wget https://raw.githubusercontent.com/chipster/chipster-tools/master/modules/admin/R-3.2.3/install-libs.R
-#wget https://raw.githubusercontent.com/chipster/chipster-tools/master/modules/admin/R/smip.R
-#${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla install-libs.R
+wget https://raw.githubusercontent.com/chipster/chipster-tools/master/modules/admin/R-3.2.3/install-libs.R
+wget https://raw.githubusercontent.com/chipster/chipster-tools/master/modules/admin/R/smip.R
+${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla install-libs.R
