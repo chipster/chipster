a33a1b361337b6e2d583e20d2508d8ed5cc75ed6
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-07-06 13:15:42 +0300

    Update R-3.2.3 prepackaged url

--- src/main/admin/vm/modules/R/R-3.2.3.bash
+++ src/main/admin/vm/modules/R/R-3.2.3.bash
@@ -3,21 +3,20 @@
 R_VER=3.2.3  
 cd ${TMPDIR_PATH}/
 
-curl -s http://gemmei.acc.umu.se/mirror/CRAN/src/base/R-3/R-${R_VER}.tar.gz | tar -xz
-
-#curl -s http://ftp.sunet.se/pub/lang/CRAN/src/base/R-3/R-${R_VER}.tar.gz | tar -xz
-cd R-${R_VER}/
-export MAKEFLAGS=-j
-./configure --prefix=${TOOLS_PATH}/R-${R_VER}
-make
-make install
-echo 'MAKEFLAGS=-j' > ${TOOLS_PATH}/R-${R_VER}/lib/R/etc/Makevars.site # (could also be $HOME/.R/Makevars)
-
+#curl -s http://gemmei.acc.umu.se/mirror/CRAN/src/base/R-3/R-${R_VER}.tar.gz | tar -xz
+#
+#cd R-${R_VER}/
+#export MAKEFLAGS=-j
+#./configure --prefix=${TOOLS_PATH}/R-${R_VER}
+#make
+#make install
+#echo 'MAKEFLAGS=-j' > ${TOOLS_PATH}/R-${R_VER}/lib/R/etc/Makevars.site # (could also be $HOME/.R/Makevars)
+#
 ## clean makeflags after R install
-export MAKEFLAGS=
-
-cd ../
-rm -rf R-${R_VER}/
+#export MAKEFLAGS=
+#
+#cd ../
+#rm -rf R-${R_VER}/
 
 
 ## R libraries, if parallel processing is on, run libs installation with GNU parallel
@@ -29,7 +28,8 @@ rm -rf R-${R_VER}/
 
 
 #curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3-2016-03-10.tar.gz | tar -xz -C ${TOOLS_PATH}/
+curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3-2016-07-06.tar.gz | tar -xz -C ${TOOLS_PATH}/
 
-wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R-3.2.3/install-libs.R
-wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R/smip.R
-${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla install-libs.R
+#wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R-3.2.3/install-libs.R
+#wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R/smip.R
+#${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla install-libs.R
