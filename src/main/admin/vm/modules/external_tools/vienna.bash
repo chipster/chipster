8f5bf107dd0902529419a7bdfbf9904f631b6668
klemela <klemela>
2016-08-01 12:21:01 +0300

    Download Vienna from nic

--- src/main/admin/vm/modules/external_tools/vienna.bash
+++ src/main/admin/vm/modules/external_tools/vienna.bash
@@ -1,7 +1,9 @@
 ##depends:external_tools/EMBOSS.bash
 
 #vienna
-                                                                                                                                                                     	curl ftp://emboss.open-bio.org/pub/EMBOSS/VIENNA-1.7.2.650.tar.gz | tar -xz -C ${TMPDIR_PATH}/
+    
+    # curl ftp://emboss.open-bio.org/pub/EMBOSS/VIENNA-1.7.2.650.tar.gz | tar -xz -C ${TMPDIR_PATH}/
+    curl http://www.nic.funet.fi/pub/sci/molbio/chipster/dist/tools_extras/VIENNA-1.7.2.660.tar.gz | tar -xz -C ${TMPDIR_PATH}/
 	cd  ${TMPDIR_PATH}/VIENNA-1.7.2.650
 	./configure ${EMBOSS_OPTIONS}
 	make
