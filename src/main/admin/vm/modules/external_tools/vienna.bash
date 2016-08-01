3f007ae8af3133bad69c6a705eb1c634119d684c
klemela <klemela>
2016-08-01 12:28:55 +0300

    Use the nic mirror variable

--- src/main/admin/vm/modules/external_tools/vienna.bash
+++ src/main/admin/vm/modules/external_tools/vienna.bash
@@ -3,7 +3,7 @@
 #vienna
     
     # curl ftp://emboss.open-bio.org/pub/EMBOSS/VIENNA-1.7.2.650.tar.gz | tar -xz -C ${TMPDIR_PATH}/
-    curl http://www.nic.funet.fi/pub/sci/molbio/chipster/dist/tools_extras/VIENNA-1.7.2.660.tar.gz | tar -xz -C ${TMPDIR_PATH}/
+    curl http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/VIENNA-1.7.2.660.tar.gz | tar -xz -C ${TMPDIR_PATH}/
 	cd  ${TMPDIR_PATH}/VIENNA-1.7.2.650
 	./configure ${EMBOSS_OPTIONS}
 	make
