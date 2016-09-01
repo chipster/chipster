85fe9cfddc733f6dffffef37a6e3426ea7be4836
amsaren <ari-matti.saren@csc.fi>
2016-09-01 12:40:55 +0300

    Install Python and python-based tools (HTSeq, MACS, MACS2, RSeQC) to tools. Remove old installation scripts for the tools.

--- src/main/admin/vm/modules/external_tools/MACS.bash
+++
@@ -1,8 +0,0 @@
-##depends:none
-
-# MACS 1 & 2 
-  # part 2 - creating links
-  cd ${TMPDIR_PATH}/
-  mkdir ${TOOLS_PATH}/macs/
-  ln -s /usr/local/bin/macs14 ${TOOLS_PATH}/macs/macs14
-  ln -s /usr/local/bin/macs2 /mnt/tools/macs/macs2
