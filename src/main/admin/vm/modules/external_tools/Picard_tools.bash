daddfc15ef29ee53934bee5e1fed38c759ce8d68
amsaren <ari-matti.saren@csc.fi>
2016-10-10 14:04:54 +0300

    picard 2.6.0

--- src/main/admin/vm/modules/external_tools/Picard_tools.bash
+++ src/main/admin/vm/modules/external_tools/Picard_tools.bash
@@ -5,11 +5,9 @@ source ../installation_files/functions.bash
 # Picard tools, Apache License V2.0, MIT
   cd ${TMPDIR_PATH}/
   #wget_retry -nv -O picard-tools-1.138.zip https://github.com/broadinstitute/picard/releases/download/1.138/picard-tools-1.138.zip
-  wget_retry -nv -O picard-tools-1.138.zip http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/picard-tools-1.138.zip
-  unzip -q picard-tools-1.138.zip
-  rm picard-tools-1.138.zip
-  # remove this optional jar because it's in the root of the zip
-  #rm snappy-java-1.0.3-rc3.jar
-  mv picard-tools-1.138/ ${TOOLS_PATH}
+  #wget_retry -nv -O picard-tools-1.138.zip http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/picard-tools-1.138.zip
+  wget_retry -nv https://github.com/broadinstitute/picard/releases/download/2.6.0/picard.jar
+  mkdir ${TOOLS_PATH}/picard-tools-2.6.0
+  mv picard.jar ${TOOLS_PATH}/picard-tools-2.6.0
   cd ${TOOLS_PATH}
-  ln -s picard-tools-1.138 picard-tools
+  ln -s picard-tools-2.6.0 picard-tools
