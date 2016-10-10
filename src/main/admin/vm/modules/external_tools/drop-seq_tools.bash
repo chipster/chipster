3eb25e837781dec35088278f2c7ea40bed216e84
amsaren <ari-matti.saren@csc.fi>
2016-10-10 14:05:21 +0300

    Drop-seq tools 1.12

---
+++ src/main/admin/vm/modules/external_tools/drop-seq_tools.bash
@@ -0,0 +1,11 @@
+##depends:none
+
+source ../installation_files/functions.bash
+
+# Drop seq tools
+  cd ${TMPDIR_PATH}/
+  wget_retry -nv -O Drop-seq_tools-1.12.zip http://mccarrolllab.com/download/922/
+  unzip Drop-seq_tools-1.12.zip
+  mv Drop-seq_tools-1.12 ${TOOLS_PATH}
+  cd ${TOOLS_PATH}
+  ln -s drop-seq_tools-1.12 drop-seq_tools 
