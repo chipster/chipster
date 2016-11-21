1efbebb7afe2e94dd730f94fc67f62f6dfee30ff
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-11-21 19:19:18 +0200

    Use 16.04 prebuild virusdetect in build tools

--- src/main/admin/vm/modules/external_tools/VirusDetect.bash
+++ src/main/admin/vm/modules/external_tools/VirusDetect.bash
@@ -2,5 +2,5 @@
 
 # see ../docs/VirusDetect-compile.bash
 
-curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/bioperl_ubuntu-12.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
-curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/virusdetect_ubuntu-12.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
+curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/bioperl_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
+curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/virusdetect_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
