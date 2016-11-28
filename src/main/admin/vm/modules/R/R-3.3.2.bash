c8ff499d4b4f6153d38fbb25fd5290280388cc5e
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-11-28 08:40:07 +0200

    Update R-3.3.2 url

--- src/main/admin/vm/modules/R/R-3.3.2.bash
+++ src/main/admin/vm/modules/R/R-3.3.2.bash
@@ -1,3 +1,3 @@
 ##depends:none
 
-curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.3.2-vmbin/R-3.3.2_ubuntu-16.04_2016-11-04.tar.gz | tar -xz -C ${TOOLS_PATH}/
+curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.3.2-vmbin/R-3.3.2_ubuntu-16.04_2016-11-28.tar.gz | tar -xz -C ${TOOLS_PATH}/
