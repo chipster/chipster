7c2af62e9f983ab7e20b3b513a36cad1f4c7131f
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-12-09 14:27:29 +0200

    Fix edirect and sratoolkit installations

--- src/main/admin/vm/modules/external_tools/sratoolkit.bash
+++ src/main/admin/vm/modules/external_tools/sratoolkit.bash
@@ -2,10 +2,6 @@
 
 source ../installation_files/functions.bash
 
-cd ${TMPDIR_PATH}/
-#wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.4.2-4/sratoolkit.2.4.2-ubuntu64.tar.gz
-wget http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/sratoolkit.2.4.2-ubuntu64.tar.gz
-cd ${TOOLS_PATH}/
-tar zxvf ${TMPDIR_PATH}/sratoolkit.2.4.2-ubuntu64.tar.gz
-rm ${TMPDIR_PATH}/sratoolkit.2.4.2-ubuntu64.tar.gz
-ln -s sratoolkit.2.4.2-ubuntu64 sratoolkit
+#https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
+curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/sratoolkit.current-ubuntu64.tar.gz | | tar -xz -C ${TOOLS_PATH}/
+ln -s sratoolkit.2.8.0-ubuntu64 sratoolkit
