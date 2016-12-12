bf82292ebee64d32a964c28afe0832ac8c35d30b
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-12-12 10:06:36 +0200

    Fix sratoolkit link creation

--- src/main/admin/vm/modules/external_tools/sratoolkit.bash
+++ src/main/admin/vm/modules/external_tools/sratoolkit.bash
@@ -4,4 +4,5 @@ source ../installation_files/functions.bash
 
 #https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
 curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/sratoolkit.current-ubuntu64.tar.gz | tar -xz -C ${TOOLS_PATH}/
+cd ${TOOLS_PATH}
 ln -s sratoolkit.2.8.0-ubuntu64 sratoolkit
