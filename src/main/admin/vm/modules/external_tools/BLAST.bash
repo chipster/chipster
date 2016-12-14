24ffaa551c93b1201d38d89fe1c6a03c4793886f
Taavi Hupponen <taavi.hupponen@csc.fi>
2016-12-14 08:27:36 +0200

    Update blast download url

--- src/main/admin/vm/modules/external_tools/BLAST.bash
+++ src/main/admin/vm/modules/external_tools/BLAST.bash
@@ -2,5 +2,5 @@
 
 # BLAST, public domain
   #curl ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.29/ncbi-blast-2.2.29+-x64-linux.tar.gz | tar -xz -C ${TOOLS_PATH}/
-  curl http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/ncbi-blast-2.2.29+-x64-linux.tar.gz | tar -xz -C ${TOOLS_PATH}/
-  ln -s ncbi-blast-2.2.29+ ${TOOLS_PATH}/blast    
+  curl http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/ncbi-blast-2.5.0+-x64-linux.tar.gz | tar -xz -C ${TOOLS_PATH}/
+ ln -s ncbi-blast-2.5.0+ ${TOOLS_PATH}/blast    
