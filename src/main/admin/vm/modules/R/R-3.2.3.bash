697f61c6e0f52cdbceabc9a1888495e990c60014
aoikari <axel.oikari@csc.fi>
2017-08-02 13:33:44 +0300

    Replace piping between curl, lz4, and tar with tmp files to avoid EOF errors.

--- src/main/admin/vm/modules/R/R-3.2.3.bash
+++ src/main/admin/vm/modules/R/R-3.2.3.bash
@@ -29,7 +29,29 @@ cd ${TMPDIR_PATH}/
 
 #curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3-2016-03-10.tar.gz | tar -xz -C ${TOOLS_PATH}/
 #curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3_ubuntu-16.04_2017-02-27.tar.gz | tar -xz -C ${TOOLS_PATH}/
-curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3_ubuntu-16.04_2017-05-09.tar.lz4 | lz4 -d | tar -x -C ${TOOLS_PATH}/
+
+# TODO The following gives an error:
+#
+#curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3_ubuntu-16.04_2017-05-09.tar.lz4 | lz4 -d | tar -x -C ${TOOLS_PATH}/
+#
+# curl: (18) transfer closed with 48190853283 bytes remaining to read
+# Error 67 : Unfinished stream
+# tar: Unexpected EOF in archive
+#
+# Lets split it into a seperate commands:
+# 1. create a tmp dir
+# 2. curl the data
+# 3. lz4 decomptress the data
+# 4. extract the data with tar to TOOLS_PATH
+# 5. remove the tmp dir
+
+mkdir tmpR
+cd tmpR
+curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/R/R-3.2.3-vmbin/R-3.2.3_ubuntu-16.04_2017-05-09.tar.lz4 -o curl_out
+lz4 -d curl_out lz4_out
+tar -xf lz4_out -C ${TOOLS_PATH}/
+cd ..
+rm -rf tmpR
 
 #wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R-3.2.3/install-libs.R
 #wget https://raw.githubusercontent.com/chipster/chipster-tools/master/tools/admin/R/smip.R
