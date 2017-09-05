c2e884f6c2fe23f0cc0aa4f9c54903a1db93c537
amsaren <ari-matti.saren@csc.fi>
2017-09-05 13:55:03 +0300

    Added HISAT2 install script

---
+++ src/main/admin/vm/modules/external_tools/HISAT2.bash
@@ -0,0 +1,12 @@
+##depends:none
+
+source ../installation_files/functions.bash
+
+# HISAT2, GPLv3
+  cd ${TMPDIR_PATH}/
+  wget_retry -O hisat2-2.1.0-Linux_x86_64.zip ftp://ftp.ccb.jhu.edu/pub/infphilo/hisat2/downloads/hisat2-2.1.0-Linux_x86_64.zip
+ 
+  unzip hisat2-2.1.0-Linux_x86_64.zip -d ${TOOLS_PATH}/
+  rm -f hisat2-2.1.0-Linux_x86_64.zip
+  cd ${TOOLS_PATH}
+  ln -s hisat2-2.1.0-Linux_x86_64 hisat2
