4741889c9b5841bae9339063c5d7de2faaec7d25
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-10-16 21:40:43 +0300

    Create link for virusdetect

--- src/main/admin/vm/modules/external_tools/VirusDetect.bash
+++ src/main/admin/vm/modules/external_tools/VirusDetect.bash
@@ -4,3 +4,5 @@
 
 curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/bioperl_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
 curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/VirusDetect-v1.7_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
+cd ${TOOLS_PATH}
+ln -s VirusDetect-v1.7 virusdetect
