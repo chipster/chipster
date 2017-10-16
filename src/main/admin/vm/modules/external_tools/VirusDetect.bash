c7e0256217c15acd7f281d7d97f63b9dec3eea5a
Taavi Hupponen <taavi.hupponen@csc.fi>
2017-10-16 17:07:50 +0300

    Install VirusDetect 1.7

--- src/main/admin/vm/modules/external_tools/VirusDetect.bash
+++ src/main/admin/vm/modules/external_tools/VirusDetect.bash
@@ -1,6 +1,6 @@
-##depends:none                                                                                                                                                                                                 
+##depends:none
 
 # see ../docs/VirusDetect-compile.bash
 
 curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/bioperl_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
-curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/virusdetect_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
+curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/VirusDetect-v1.7_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
