419dcbc6c3ed611b0887048bbd416b9a0d19cc5c
klemela <klemela>
2018-05-30 15:42:09 +0300

    Install all Conda packages in the same file
    The dependency system seems to be broken.

--- src/main/admin/vm/modules/external_tools/conda.bash
+++ src/main/admin/vm/modules/external_tools/conda.bash
@@ -21,3 +21,10 @@ export PATH=\${PATH}:\${conda_path}/bin
 source activate \$c_env
 \$c_tool \$@
 EOF
+
+
+# minimap
+CONDA_PATH=("/opt/chipster/tools/miniconda3")
+export PATH=${PATH}:${CONDA_PATH}/bin
+source activate chipster_tools
+conda install minimap2=2.9 -y
--- src/main/admin/vm/modules/external_tools/minimap.bash
+++
@@ -1,6 +0,0 @@
-##depends:conda
-
-CONDA_PATH=("/opt/chipster/tools/miniconda3")
-export PATH=${PATH}:${CONDA_PATH}/bin
-source activate chipster_tools
-conda install minimap2=2.9 -y 
