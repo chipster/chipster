36421456d6d493835c9d8813ce302b409bf3a056
klemela <klemela>
2018-08-29 12:55:19 +0300

    Update Mothur, Silva and R-3.4.3

--- src/main/admin/vm/modules/R/R-3.4.3.bash
+++ src/main/admin/vm/modules/R/R-3.4.3.bash
@@ -1,3 +1,3 @@
 ##depends:none
 
-lz4 -d /mnt/artefacts/downloads/R-3.4.3_ubuntu-16.04_2018-05-30.tar.lz4 |tar x -C ${TOOLS_PATH}/
+lz4 -d /mnt/artefacts/downloads/R-3.4.3_ubuntu-16.04_2018-08-29.tar.lz4 | tar x -C ${TOOLS_PATH}/
--- src/main/admin/vm/modules/external_tools/mothur.bash
+++ src/main/admin/vm/modules/external_tools/mothur.bash
@@ -5,24 +5,22 @@ source ../installation_files/functions.bash
  # mothur GPLv3
   cd ${TMPDIR_PATH}/
   
-  #wget_retry -nv http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/Mothur-1.28.cen_64.noReadLine.zip
-  #unzip -q Mothur-1.28.cen_64.noReadLine.zip
-  #mv mothur ${TOOLS_PATH}/mothur-1.28
-  #ln -s mothur-1.28 ${TOOLS_PATH}/mothur
-  
-  #wget_retry -nv https://github.com/mothur/mothur/releases/download/v1.36.1/Mothur.linux_64.noReadline.zip
-  #unzip -q Mothur.linux_64.noReadline.zip
-  #mv mothur ${TOOLS_PATH}/mothur-1.36.1
-  #ln -s mothur-1.36.1 ${TOOLS_PATH}/mothur
-  
   # Mothur
   wget_retry -nv  https://github.com/mothur/mothur/releases/download/v1.39.5/Mothur.linux_64.noReadLine.zip
   unzip -q Mothur.linux_64.noReadLine.zip
   mv mothur ${TOOLS_PATH}/mothur-1.39.5
   ln -s mothur-1.39.5 ${TOOLS_PATH}/mothur
+  
+  wget_retry -nv  http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/Mothur-1.40.5.zip
+  unzip -q Mothur-1.40.5.zip
+  mv mothur ${TOOLS_PATH}/mothur-1.40.5
     
   curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/mothur-data.tar.gz | tar -xz -C ${TOOLS_PATH}/
 
   mkdir -p ${TOOLS_PATH}/mothur-silva-reference/
-  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/silva.bacteria.tar.gz  | tar -xz -C ${TOOLS_PATH}/mothur-silva-reference/
-  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/mothur-silva-reference-whole.tar.gz  | tar -xz -C ${TOOLS_PATH}/mothur-silva-reference/
+  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/silva/v102.tar.lz4  | lz4 -d | tar -x -C ${TOOLS_PATH}/mothur-silva-reference/
+  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/silva/v123.tar.lz4  | lz4 -d | tar -x -C ${TOOLS_PATH}/mothur-silva-reference/
+  curl -s http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/mothur/silva/v132.tar.lz4  | lz4 -d | tar -x -C ${TOOLS_PATH}/mothur-silva-reference/
+  
+  ln -s v132 ${TOOLS_PATH}/mothur-silva-reference/silva
+ 
