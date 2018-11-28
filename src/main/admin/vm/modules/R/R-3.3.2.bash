6af3997b4f1fc8c16bbece5cab99be445cc4856e
amsaren <ari-matti.saren@csc.fi>
2018-11-28 14:51:47 +0200

    Temporary fix for mirror

--- src/main/admin/vm/modules/R/R-3.3.2.bash
+++ src/main/admin/vm/modules/R/R-3.3.2.bash
@@ -7,4 +7,6 @@ lz4 -d /mnt/artefacts/downloads/R-3.3.2_ubuntu-16.04_2017-06-16.tar.lz4 |tar x -
 # For GATK
 R_VER=3.3.2
 wget https://raw.githubusercontent.com/broadinstitute/gatk/master/scripts/docker/gatkbase/install_R_packages.R
+# Temporary fix because cran.mtu.edu is down
+sed -i s_' "http://cran.mtu.edu",'__ install_R_packages.R
 ${TOOLS_PATH}/R-${R_VER}/bin/Rscript --vanilla install_R_packages.R
