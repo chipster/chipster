65f648f2085ff4cb4acf7b5df742a8db5bb1b325
amsaren <ari-matti.saren@csc.fi>
2018-12-19 13:20:29 +0200

    Update VirusDetect databases

--- src/main/admin/vm/modules/external_tools/VirusDetect.bash
+++ src/main/admin/vm/modules/external_tools/VirusDetect.bash
@@ -6,3 +6,10 @@ curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect
 curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/virusdetect/VirusDetect-v1.7_ubuntu-16.04.tar.gz | tar -xz -C ${TOOLS_PATH}/
 cd ${TOOLS_PATH}
 ln -s VirusDetect-v1.7 virusdetect
+
+#Update just database
+cd ${TMPDIR_PATH}
+wget_retry -nv https://chipster.object.pouta.csc.fi/virusdetect/databases.tgz
+tar xf databases.tgz
+rm -rf ${TOOLS_PATH}/virusdetect/databases
+mv databases ${TOOLS_PATH}/virusdetect
