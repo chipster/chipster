15f1b8334c1a3ca4aaa03dc3a95d42fb41322b6b
amsaren <ari-matti.saren@csc.fi>
2019-02-20 12:50:14 +0200

    SRA Toolkit 2.9.4

--- src/main/admin/vm/modules/external_tools/sratoolkit.bash
+++ src/main/admin/vm/modules/external_tools/sratoolkit.bash
@@ -3,6 +3,7 @@
 source ../installation_files/functions.bash
 
 #https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
-curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/sratoolkit.current-ubuntu64.tar.gz | tar -xz -C ${TOOLS_PATH}/
+#curl -L http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/sratoolkit.current-ubuntu64.tar.gz | tar -xz -C ${TOOLS_PATH}/
+curl -L http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.9.4/sratoolkit.2.9.4-ubuntu64.tar.gz | tar -xz -C ${TOOLS_PATH}/
 cd ${TOOLS_PATH}
 ln -s sratoolkit.2.8.0-ubuntu64 sratoolkit
