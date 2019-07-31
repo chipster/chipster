d281b235e0703b952598e69503a14a0112fe3e81
klemela <klemela>
2019-07-31 13:47:53 +0300

    Add R 3.6.1, revert 3.4.3

---
+++ src/main/admin/vm/modules/R/R-3.6.1-plain.bash
@@ -0,0 +1,3 @@
+##depends:none
+
+lz4 -d /mnt/artefacts/downloadsR-3.6.1-plain_ubuntu-16.04_2019-07-31.tar.lz4 | tar x -C ${TOOLS_PATH}/
---
+++ src/main/admin/vm/modules/R/R-3.6.1.bash
@@ -0,0 +1,3 @@
+##depends:none
+
+lz4 -d /mnt/artefacts/downloads/R-3.4.3_ubuntu-16.04_2019-07-30.tar.lz4 | tar x -C ${TOOLS_PATH}/
