837503b7bc8dab4c46b7fb5aca486484f32e6916
klemela <klemela>
2019-08-01 12:41:57 +0300

    Update Seurat files

--- src/main/admin/vm/modules/external_tools/seurat.bash
+++ src/main/admin/vm/modules/external_tools/seurat.bash
@@ -4,7 +4,8 @@
   cd ${TMPDIR_PATH}/
   mkdir seurat
   cd seurat
-  wget http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/seurat/cell_cycle_vignette_files.zip
+  #wget http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/seurat/cell_cycle_vignette_files.zip
+  wget http://vm0151.kaj.pouta.csc.fi/artefacts/downloads/seurat/2019-08-01_cell_cycle_vignette_files.zip
   unzip cell_cycle_vignette_files.zip
   cd ..
   mv seurat ${TOOLS_PATH}/
