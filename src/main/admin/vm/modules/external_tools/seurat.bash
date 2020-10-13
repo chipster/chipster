0191fda80183927ec03a6f628334b9e1c604d072
Petri Klemela <petri.klemela@csc.fi>
2020-10-13 09:23:17 +0300

    Fix a package name

--- src/main/admin/vm/modules/external_tools/seurat.bash
+++ src/main/admin/vm/modules/external_tools/seurat.bash
@@ -6,6 +6,6 @@
   cd seurat
   #wget http://$NIC_MIRROR/pub/sci/molbio/chipster/dist/tools_extras/seurat/cell_cycle_vignette_files.zip
   wget http://vm0151.kaj.pouta.csc.fi/artefacts/downloads/seurat/2019-08-01_cell_cycle_vignette_files.zip
-  unzip cell_cycle_vignette_files.zip
+  unzip 2019-08-01_cell_cycle_vignette_files.zip
   cd ..
   mv seurat ${TOOLS_PATH}/
