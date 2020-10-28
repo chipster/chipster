f571ef6f22a6ee07de6601807a6d78b04f2fc358
Petri Klemela <petri.klemela@csc.fi>
2020-10-28 15:10:27 +0200

    Add mothur unite reference

--- src/main/admin/vm/modules/R/R-3.6.1.bash
+++
@@ -1,3 +0,0 @@
-##depends:none
-
-lz4 -d /mnt/artefacts/downloads/R-3.6.1_ubuntu-16.04_2019-08-01.tar.lz4 | tar x -C ${TOOLS_PATH}/
--- src/main/admin/vm/modules/external_tools/data_for_tools.bash
+++
@@ -1,3 +0,0 @@
-##depends:none
-# Genome data for tools
-  ln -s /opt/chipster/genomes ${TOOLS_PATH}/genomes
